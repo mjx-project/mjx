e940b73f8e6cf4babc79607d57e092337cdbda97
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-06-11 12:25:25 +0900

    feat: add yaku_evaluator.h and cpp

---
+++ mahjong/yaku_evaluator.cpp
@@ -0,0 +1,17 @@
+
+#include "yaku_evaluator.h"
+
+namespace mj
+{
+
+    YakuEvaluator::YakuEvaluator(const WinningHandCache &win_cache) : win_cache_(win_cache){ }
+
+    std::vector<Yaku> YakuEvaluator::Apply(Hand hand) {
+        std::vector<Yaku> yakus;
+        return yakus;
+    }
+
+    std::uint64_t mj::YakuEvaluator::YakuBit(Hand hand) {
+        return 0;
+    }
+}
