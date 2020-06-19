5f37b2caa53dbf3974a6c0464e10fbfab00a10a3
keigo habara <34413567+habara-k@users.noreply.github.com>
2020-06-19 15:06:45 +0900

    [WIP] 上がり形の判定 (#36)
    * Create Outline
    
    * feat: update algorithm
    
    * feat: use boost/property_tree to save cache
    
    * feat: complete cache calculation
    
    * refactor
    
    * remove CMake option --pedantic-errors
    
    * create class WinningHandCacheGenerator
    
    * add test
    
    * add cache file
    
    * move cache directory
    
    * fix: cache directory
    
    * refactor
    
    * add entrypoint
    
    * use unordered_map
    
    * fix generator

--- mahjong/win_cache.h
+++ mahjong/win_cache.h
@@ -1,25 +1,28 @@
 #ifndef MAHJONG_WIN_CACHE_H
 #define MAHJONG_WIN_CACHE_H
 
-#include <memory>
 #include <unordered_map>
+#include <set>
+#include <vector>
+#include <string>
+
+#include "types.h"
 
 namespace mj
 {
+    using AbstructHand = std::string;
+    using SplitPattern = std::vector<std::vector<int>>;
+    using CacheType = std::unordered_map<AbstructHand, std::set<SplitPattern>>;
+
     class WinningHandCache
     {
     public:
         WinningHandCache();
-        [[nodiscard]] std::size_t Size() const noexcept ;
         [[nodiscard]] bool Has(const std::string &s) const noexcept ;
-        [[nodiscard]] std::uint64_t YakuBit(const std::string &s) const;
-        // utils
-        static void PrepareWinCache();
-        void ShowStats(std::uint64_t yaku_bit, const std::string &yaku_name);
     private:
-        std::unique_ptr<std::unordered_map<std::string, std::uint64_t>> cache_;
-        void Load();
+        CacheType cache_;
+        void LoadWinCache();
     };
-}
+}  // namespace mj
 
 #endif //MAHJONG_WIN_CACHE_H
