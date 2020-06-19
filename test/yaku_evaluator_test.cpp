bfc82f9d750a3ec5d3a8dd2ff8b215559b4a98e0
Keigo Habara <k.habara.aa@gmail.com>
2020-06-19 16:39:02 +0900

    fix test

--- test/yaku_evaluator_test.cpp
+++ test/yaku_evaluator_test.cpp
@@ -13,12 +13,6 @@ protected:
 };
 
 
-TEST_F(YakuTest, Empty)
-{
-    EXPECT_TRUE(evaluator.Has(mj::Hand(mj::HandParams("m1,m9,p1,p9,s1,s9,ew,sw,ww,nw,wd,gd,rd"))));
-    EXPECT_EQ(evaluator.Eval(mj::Hand(mj::HandParams("m1,m9,p1,p9,s1,s9,ew,sw,ww,nw,wd,gd,rd"))).size(), 0);
-}
-
 TEST_F(YakuTest, FullyConcealdHand)
 {
     auto yaku1 = evaluator.Eval(
