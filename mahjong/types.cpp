b2ff15a734d1ef5fcbcf1e9f2a947727af444a45
Keigo Habara <k.habara.aa@gmail.com>
2020-06-25 16:30:29 +0900

    fix

--- mahjong/types.cpp
+++ mahjong/types.cpp
@@ -3,7 +3,7 @@
 namespace mj
 {
     std::uint8_t Num(TileType type) noexcept {
-        assert(!Is(type, TileSetType::kHonours));
+        assert(type < TileType::kEW);
         return static_cast<uint8_t>(type) % 9 + 1;
     }
 } // namespace mj
