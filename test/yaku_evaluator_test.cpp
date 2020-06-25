ec1a766bc70698199b8396f303071b583cdb83bf
Keigo Habara <k.habara.aa@gmail.com>
2020-06-25 16:39:19 +0900

    implement tanyao

--- test/yaku_evaluator_test.cpp
+++ test/yaku_evaluator_test.cpp
@@ -27,3 +27,14 @@ TEST_F(YakuTest, FullyConcealdHand)
             mj::Hand(mj::HandParams("m1,m2,m3,m4,m5,rd,rd,m7,m8,m9").Pon("p1,p1,p1").Tsumo("m6")));
     EXPECT_EQ(std::count(yaku3.begin(), yaku3.end(), mj::Yaku::kFullyConcealedHand), 0);
 }
+
+TEST_F(YakuTest, AllSimples)
+{
+    auto yaku1 = evaluator.Eval(
+            mj::Hand(mj::HandParams("m2,m3,m4,m5,m7,s2,s2,s6,s7,s8,p2,p3,p4").Tsumo("m6")));
+    EXPECT_EQ(std::count(yaku1.begin(), yaku1.end(), mj::Yaku::kAllSimples), 1);
+
+    auto yaku2 = evaluator.Eval(
+            mj::Hand(mj::HandParams("m2,m3,m4,m5,m7,s2,s2,s6,s7,s8,p1,p2,p3").Tsumo("m6")));
+    EXPECT_EQ(std::count(yaku2.begin(), yaku2.end(), mj::Yaku::kAllSimples), 0);
+}
