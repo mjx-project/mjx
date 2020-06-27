91eeb6b2eca434f83bcbc21c2423024ff097d99d
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-06-28 05:26:18 +0900

    update action

--- mahjong/action.h
+++ mahjong/action.h
@@ -4,6 +4,8 @@
 #include <memory>
 #include <mahjong.pb.h>
 #include "types.h"
+#include "tile.h"
+#include "open.h"
 
 namespace mj
 {
@@ -13,6 +15,9 @@ namespace mj
         Action();
         AbsolutePos Who();
         ActionType Type();
+        bool Yes();
+        Tile Discard();
+        std::unique_ptr<Open> Open();
     private:
         std::unique_ptr<ActionResponse> action_response_;
     };
