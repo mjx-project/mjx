47dfcbb0a07f4333c70a1e83d31d2ea6f8a1d536
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-06-28 15:28:42 +0900

    update grpc usages

--- mahjong/agent_client.cpp
+++ mahjong/agent_client.cpp
@@ -26,31 +26,40 @@ int main(int argc, char** argv) {
             grpc::CreateChannel("127.0.0.1:9090", grpc::InsecureChannelCredentials())
     );
 
-    mj::ActionRequest request = mj::ActionRequest();
-    request.set_who(0);
-    auto score = request.mutable_score();
-    score->set_round(5);
-    score->set_honba(1);
-    score->set_riichi(2);
-    score->add_ten(250);
-    score->add_ten(250);
-    score->add_ten(250);
-    score->add_ten(250);
-    auto taken_actions = request.mutable_taken_actions();
-    auto taken_action = mj::ActionRequest_TakenAction();
-    taken_action.set_who(1);
-    taken_action.set_type(2);
-    taken_action.set_draw(1);
-    taken_actions->Add(std::move(taken_action));
-    auto initial_hand = request.mutable_initial_hand();
-    auto tiles = initial_hand->mutable_tiles();
-    tiles->Add(0);
-    tiles->Add(1);
-    tiles->Add(2);
-    tiles->Add(3);
-    auto observation = mj::Observation(request);
+    // Common observation over 4 players
+    auto common_observation = new mj::ActionRequest_CommonObservation();
 
-    auto action = agent.TakeAction(observation);
+    // action1 happens
+    auto taken_actions = common_observation->mutable_taken_actions();
+    auto taken_action1 = mj::ActionRequest_CommonObservation_TakenAction();
+    taken_actions->Add(std::move(taken_action1));
+
+    // take first action
+    auto request1 = mj::ActionRequest();
+    request1.set_who(1);
+    // use common observation
+    request1.set_allocated_common_observation(common_observation);
+    auto action = agent.TakeAction(mj::Observation(request1));
+    // return common observation
+    common_observation = request1.release_common_observation();
+
+    // action2 happens
+    taken_actions = common_observation->mutable_taken_actions();
+    auto taken_action2 = mj::ActionRequest_CommonObservation_TakenAction();
+    taken_actions->Add(std::move(taken_action2));
+
+    // take second action
+    auto request2 = mj::ActionRequest();
+    request2.set_who(2);
+    request2.set_allocated_common_observation(common_observation);
+    action = agent.TakeAction(mj::Observation(request2));
+    // return common observatoin
+    common_observation = request2.release_common_observation();
+
+    if (common_observation) {
+        std::cout << "deleted" << std::endl;
+        delete common_observation;
+    }
 
     std::cout << "  type: " << action.MutableActionResponse()->type() << std::endl;
     std::cout << "  action: " << action.MutableActionResponse()->discard() << std::endl;
