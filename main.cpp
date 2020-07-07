214134ceccb4fd31cb65b6a45931dd54554ead19
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-07-08 07:01:06 +0900

    fix: build

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 
-#include "win_cache_generator.h"
-#include "win_cache.h"
-
-
 int main() {
     std::cerr << "Hello" << std::endl;
-    //mj::WinningHandCacheGenerator::GenerateCache();
-    mj::WinningHandCache win_cache;
-
     return 0;
 }
