7d608eaac6bf933571f6777388dc1f78a396126c
Keigo Habara <k.habara.aa@gmail.com>
2020-07-23 18:16:55 +0900

    make Open real class

--- mahjong/open.h
+++ mahjong/open.h
@@ -10,122 +10,109 @@
 
 namespace mj
 {
-   class Open
-    {
-    public:
-        Open() = default;
-        explicit Open(std::uint16_t bits);
-        virtual ~Open() = default;
-        [[nodiscard]] virtual OpenType Type() const = 0;
-        virtual RelativePos From() = 0;  // In added kan, it's the opponent player from whom the pon was declared (not kan)
-        [[nodiscard]] virtual Tile At(std::size_t i) const = 0;  // sorted by tile id
-        virtual std::size_t Size() = 0;
-        [[nodiscard]] virtual std::vector<Tile> Tiles() const = 0;  // sorted by tile id
-        virtual std::vector<Tile> TilesFromHand() = 0;  // sorted by tile id. chi => 2 tiles, pon => 2, kan_opened => 3, kan_closed => 4, kan_added => 2
-        virtual Tile StolenTile() = 0; // kan_added => poned tile by others, kan_closed => tile id represented at left 8 bits
-        virtual Tile LastTile() = 0;  // Last tile added to this open tile sets. kan_added => lastly kaned tile, the others => stolen()
-        virtual std::vector<TileType> UndiscardableTileTypes() = 0;
-        virtual std::uint16_t GetBits();
-        [[nodiscard]] virtual std::string ToString(bool verbose = false) const;  // TODO(sotetsuk): put more information
-        static std::unique_ptr<Open> NewOpen(std::uint16_t bits);
-    protected:
+
+    class Open {
         std::uint16_t bits_;  // follows tenhou format (see https://github.com/NegativeMjark/tenhou-log)
-    };
-
-    // TODO(sotetsuk): delete this class (duplicate to NewOpen())
-    class OpenGenerator
-    {
     public:
-       std::unique_ptr<Open> generate(std::uint16_t bits);
+        explicit Open(std::uint16_t bits);
+        OpenType Type() const;
+
+        RelativePos From() const;  // In added kan, it's the opponent player from whom the pon was declared (not kan)
+        Tile At(std::size_t i) const;  // sorted by tile id
+        std::size_t Size() const;
+        std::vector<Tile> Tiles() const;  // sorted by tile id
+        std::vector<Tile> TilesFromHand() const;  // sorted by tile id. chi => 2 tiles, pon => 2, kan_opened => 3, kan_closed => 4, kan_added => 2
+        Tile StolenTile() const; // kan_added => poned tile by others, kan_closed => tile id represented at left 8 bits
+        Tile LastTile() const;  // Last tile added to this open tile sets. kan_added => lastly kaned tile, the others => stolen()
+        std::vector<TileType> UndiscardableTileTypes() const;
+
+        std::uint16_t GetBits() const;
+        std::string ToString(bool verbose = false) const;  // TODO(sotetsuk): put more information
     };
 
-    class Chi : public Open
-    {
+    class Chi {
     public:
         Chi() = delete;
-        explicit Chi(std::uint16_t bits);
-        Chi(std::vector<Tile> &tiles, Tile stolen);
-        [[nodiscard]] OpenType Type() const final;
-        RelativePos From() final;
-        [[nodiscard]] Tile At(std::size_t i) const final;
-        std::size_t Size() final;
-        [[nodiscard]] std::vector<Tile> Tiles() const final;
-        std::vector<Tile> TilesFromHand() final;
-        Tile StolenTile() final;
-        Tile LastTile() final;
-        std::vector<TileType> UndiscardableTileTypes() final;
+        static Open Create(std::vector<Tile> &tiles, Tile stolen);
+
+        static RelativePos From(std::uint16_t bits);
+        static Tile At(std::uint16_t bits, std::size_t i);
+        static std::size_t Size(std::uint16_t bits);
+        static std::vector<Tile> Tiles(std::uint16_t bits);
+        static std::vector<Tile> TilesFromHand(std::uint16_t bits);
+        static Tile StolenTile(std::uint16_t bits);
+        static Tile LastTile(std::uint16_t bits);
+        static std::vector<TileType> UndiscardableTileTypes(std::uint16_t bits);
     private:
-        [[nodiscard]] std::uint16_t min_type() const;
-        [[nodiscard]] Tile at(std::size_t i, std::uint16_t min_type) const;
+        static std::uint16_t min_type(std::uint16_t bits);
+        static Tile at(std::uint16_t bits, std::size_t i, std::uint16_t min_type);
     };
 
-    class Pon: public Open
-    {
+    class Pon {
+
     public:
         Pon() = delete;
-        explicit Pon(std::uint16_t bits);
-        Pon(Tile stolen, Tile unused, RelativePos from);
-        [[nodiscard]] OpenType Type() const final;
-        RelativePos From() final;
-        [[nodiscard]] Tile At(std::size_t i) const final;
-        std::size_t Size() final;
-        [[nodiscard]] std::vector<Tile> Tiles() const final;
-        std::vector<Tile> TilesFromHand() final;
-        Tile StolenTile() final;
-        Tile LastTile() final;
-        std::vector<TileType> UndiscardableTileTypes() final;
+        static Open Create(Tile stolen, Tile unused, RelativePos from);
+
+        static RelativePos From(std::uint16_t bits);
+        static Tile At(std::uint16_t bits, std::size_t i);
+        static std::size_t Size(std::uint16_t bits);
+        static std::vector<Tile> Tiles(std::uint16_t bits);
+        static std::vector<Tile> TilesFromHand(std::uint16_t bits);
+        static Tile StolenTile(std::uint16_t bits);
+        static Tile LastTile(std::uint16_t bits);
+        static std::vector<TileType> UndiscardableTileTypes(std::uint16_t bits);
     };
 
-    class KanOpened : public Open
+    class KanOpened
     {
     public:
         KanOpened() = delete;
-        explicit KanOpened(std::uint16_t bits);
-        KanOpened(Tile stolen, RelativePos from);
-        [[nodiscard]] OpenType Type() const final;
-        RelativePos From() final;
-        [[nodiscard]] Tile At(std::size_t i) const final;
-        std::size_t Size() final;
-        [[nodiscard]] std::vector<Tile> Tiles() const final;
-        std::vector<Tile> TilesFromHand() final;
-        Tile StolenTile() final;
-        Tile LastTile() final;
-        std::vector<TileType> UndiscardableTileTypes() final;
+        static Open Create(Tile stolen, RelativePos from);
+
+        static RelativePos From(std::uint16_t bits);
+        static Tile At(std::uint16_t bits, std::size_t i);
+        static std::size_t Size(std::uint16_t bits);
+        static std::vector<Tile> Tiles(std::uint16_t bits);
+        static std::vector<Tile> TilesFromHand(std::uint16_t bits);
+        static Tile StolenTile(std::uint16_t bits);
+        static Tile LastTile(std::uint16_t bits);
+        static std::vector<TileType> UndiscardableTileTypes(std::uint16_t bits);
     };
 
-    class KanClosed : public Open
+
+    class KanClosed
     {
     public:
         KanClosed() = delete;
-        explicit KanClosed(std::uint16_t bits);
-        explicit KanClosed(Tile tile);  // TODO: check which tile id does Tenhou use? 0? drawn tile? This should be aligned.
-        [[nodiscard]] OpenType Type() const final;
-        RelativePos From() final;
-        [[nodiscard]] Tile At(std::size_t i) const final;
-        std::size_t Size() final;
-        [[nodiscard]] std::vector<Tile> Tiles() const final;
-        std::vector<Tile> TilesFromHand() final;
-        Tile StolenTile() final;
-        Tile LastTile() final;
-        std::vector<TileType> UndiscardableTileTypes() final;
+        static Open Create(Tile tile);  // TODO: check which tile id does Tenhou use? 0? drawn tile? This should be aligned.
+
+        static RelativePos From(std::uint16_t bits);
+        static Tile At(std::uint16_t bits, std::size_t i);
+        static std::size_t Size(std::uint16_t bits);
+        static std::vector<Tile> Tiles(std::uint16_t bits);
+        static std::vector<Tile> TilesFromHand(std::uint16_t bits);
+        static Tile StolenTile(std::uint16_t bits);
+        static Tile LastTile(std::uint16_t bits);
+        static std::vector<TileType> UndiscardableTileTypes(std::uint16_t bits);
     };
 
-    class KanAdded : public Open
+    class KanAdded
     {
     public:
         KanAdded() = delete;
-        explicit KanAdded(std::uint16_t bits);
-        KanAdded(Open* pon);
-        [[nodiscard]] OpenType Type() const final;
-        RelativePos From() final;
-        [[nodiscard]] Tile At(std::size_t i) const final;
-        std::size_t Size() final;
-        [[nodiscard]] std::vector<Tile> Tiles() const final;
-        std::vector<Tile> TilesFromHand() final;
-        Tile StolenTile() final;
-        Tile LastTile() final;
-        std::vector<TileType> UndiscardableTileTypes() final;
+        static Open Create(Open pon);
+
+        static RelativePos From(std::uint16_t bits);
+        static Tile At(std::uint16_t bits, std::size_t i);
+        static std::size_t Size(std::uint16_t bits);
+        static std::vector<Tile> Tiles(std::uint16_t bits);
+        static std::vector<Tile> TilesFromHand(std::uint16_t bits);
+        static Tile StolenTile(std::uint16_t bits);
+        static Tile LastTile(std::uint16_t bits);
+        static std::vector<TileType> UndiscardableTileTypes(std::uint16_t bits);
     };
+
 }  // namespace mj
 
 #endif //MAHJONG_OPEN_H
