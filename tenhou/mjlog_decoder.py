ffb4fbfdba1e96acbd73da0f7d5d5a307412a9b0
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-07-23 19:21:25 +0900

    add wall to proto

--- tenhou/mjlog_decoder.py
+++ tenhou/mjlog_decoder.py
@@ -14,14 +14,14 @@ class MjlogParser:
     def __init__(self):
         self.state = None
 
-    def parse(self, path_to_mjlog: str) -> Iterator[mahjong_pb2.State]:
+    def parse(self, path_to_mjlog: str, wall_dices: List[Tuple[List[int], List[int]]]) -> Iterator[mahjong_pb2.State]:
         tree = ET.parse(path_to_mjlog)
         root = tree.getroot()
-        yield from self._parse_each_game(root)
+        yield from self._parse_each_game(root, wall_dices)
 
-    def _parse_each_game(self, root: Element) -> Iterator[mahjong_pb2.State]:
-        assert (root.tag == "mjloggm")
-        assert (root.attrib['ver'] == "2.3")
+    def _parse_each_game(self, root: Element, wall_dices: List[Tuple[List[int], List[int]]]) -> Iterator[mahjong_pb2.State]:
+        assert root.tag == "mjloggm"
+        assert root.attrib['ver'] == "2.3"
         shuffle = root.iter("SHUFFLE")
         go = root.iter("GO")
         un = root.iter("UN")  # TODO(sotetsuk): if there are > 2 "UN", some user became offline
@@ -30,19 +30,24 @@ class MjlogParser:
 
         self.state = mahjong_pb2.State()
         kv: List[Tuple[str, Dict[str, str]]] = []
+        i = 0
         for child in root:
             if child.tag in ["SHUFFLE", "GO", "UN", "TAIKYOKU"]:
                 continue
             if child.tag == "INIT":
                 if kv:
-                    yield from self._parse_each_round(kv)
+                    wall, dices = wall_dices[i]
+                    yield from self._parse_each_round(kv, wall, dices)
+                    i += 1
                 self.state = mahjong_pb2.State()
                 kv = []
             kv.append((child.tag, child.attrib))
         if kv:
-            yield from self._parse_each_round(kv)
+            wall, dices = wall_dices[i]
+            yield from self._parse_each_round(kv, wall, dices)
+            i += 1
 
-    def _parse_each_round(self, kv: List[Tuple[str, Dict[str, str]]]) -> Iterator[mahjong_pb2.State]:
+    def _parse_each_round(self, kv: List[Tuple[str, Dict[str, str]]], wall: List[int], dices: List[int]) -> Iterator[mahjong_pb2.State]:
         """Input examples
 
         - <INIT seed="0,0,0,2,2,112" ten="250,250,250,250" oya="0" hai0="48,16,19,34,2,76,13,7,128,1,39,121,87" hai1="17,62,79,52,56,57,82,98,32,103,24,70,54" hai2="55,30,12,26,31,90,3,4,80,125,66,102,78" hai3="120,130,42,67,114,93,5,61,20,108,41,100,84"/>
@@ -62,12 +67,8 @@ class MjlogParser:
         - <AGARI ba="1,3" hai="1,6,9,24,25,37,42,44,45,49,52,58,60,64" machi="44" ten="30,8000,1" yaku="1,1,7,1,52,1,54,1,53,1" doraHai="69" doraHaiUra="59" who="2" fromWho="3" sc="240,0,260,0,230,113,240,-83" />
           - key = "AGARI" val = {'ba': '1,3', 'hai': '1,6,9,24,25,37,42,44,45,49,52,58,60,64', 'machi': '44', 'ten': '30,8000,1', 'yaku': '1,1,7,1,52,1,54,1,53,1', 'doraHai': '69', 'doraHaiUra': '59', 'who': '2', 'fromWho': '3', 'sc': '240,0,260,0,230,113,240,-83'}
         """
-        assert (kv[0][0] == 'INIT')
-        last_drawer, last_draw = None, None
-        taken_action = None
-
         key, val = kv[0]
-        assert(key == "INIT")
+        assert key == "INIT"
         round_, honba, riichi, dice1, dice2, dora = [int(x) for x in val["seed"].split(",")]
         self.state.init_score.round = round_
         self.state.init_score.honba = honba
@@ -77,10 +78,18 @@ class MjlogParser:
         self.state.curr_score.honba = honba
         self.state.curr_score.riichi = riichi
         self.state.curr_score.ten[:] = [int(x) for x in val["ten"].split(",")]
+        self.state.wall[:] = wall
         self.state.dora.append(dora)
         for i in range(4):
             self.state.initial_hands.append(mahjong_pb2.InitialHand(who=i, tiles=[int(x) for x in val["hai" + str(i)].split(",")]))
+        for i in range(4 * 12):
+            assert wall[i] in self.state.initial_hands[((i // 4) + round_) % 4].tiles
+        for i in range(4 * 12, 4 * 13):
+            assert wall[i] in self.state.initial_hands[(i + round_ ) % 4].tiles
+        assert dora == wall[130]
 
+        last_drawer, last_draw = None, None
+        taken_action = None
         for key, val in kv[1:]:
             if key[0] in ["T", "U", "V", "W"]:  # draw
                 # TODO (sotetsuk): consider draw after kan case
@@ -189,7 +198,7 @@ class MjlogParser:
 
     @staticmethod
     def _to_wind(wind_str: str) -> mahjong_pb2.Wind:
-        assert (wind_str in ["T", "U", "V", "W", "D", "E", "F", "G"])
+        assert wind_str in ["T", "U", "V", "W", "D", "E", "F", "G"]
         if wind_str in ["T", "D"]:
             return mahjong_pb2.WIND_EAST
         elif wind_str in ["U", "E"]:
@@ -215,11 +224,37 @@ class MjlogParser:
                 return mahjong_pb2.ACTION_TYPE_KAN_OPENED
 
 
+# TODO: remove docker dependency
+def reproduce_wall(path_to_mjlog: str) -> List[Tuple[List[int], List[int]]]:
+    tree = ET.parse(path_to_mjlog)
+    root = tree.getroot()
+    shuffle = root.iter("SHUFFLE")
+    seed = ""
+    for i, child in enumerate(shuffle):
+        assert i == 0
+        x = child.attrib["seed"].split(",")
+        print(x[0])
+        assert x[0] == "mt19937ar-sha512-n288-base64"
+        assert len(x) == 2
+        seed = repr(x[1])[1:-1]
+    out = subprocess.run(["docker", "run", "sotetsuk/twr:v0.0.1", "/twr",  seed, "100"], capture_output=True)
+    wall_dices: List[Tuple[List[int], List[int]]] = []
+    wall, dices = [], []
+    for i, line in enumerate(out.stdout.decode('utf-8').strip('\n').split('\n')):
+        if i % 2 == 0:
+            wall = [int(x) for x in line.split(',')]
+            wall.reverse()
+            assert len(wall) == 136
+        else:
+            dices = [int(x) for x in line.split(',')]
+            assert len(dices) == 2
+            wall_dices.append((wall, dices))
+    return wall_dices
+
+
 if __name__ == "__main__":
-    out = subprocess.run(
-        "docker run sotetsuk/twr:v0.0.1 /twr zmsk28otF+PUz4E7hyyzUN0fvvn3BO6Ec3fZfvoKX1ATIhkPO8iNs9yH6pWp+lvKcYsXccz1oEJxJDbuPL6qFpPKrjOe/PCBMq1pQdW2c2JsWpNSRdOCA6NABD+6Ty4pUZkOKbWDrWtGxKPUGnKFH2NH5VRMqlbo463I6frEgWrCkW3lpazhuVT1ScqAI8/eCxUJrY095I56NKsw5bGgYPARsE4Sibrk44sAv3F42/Q3ohmb/iXFCilBdfE5tNSg55DMu512CoOwd2bwV7U0LctLgl9rj6Tv6K3hOtcysivTjiz+UGvJPT6R/VTRX/u1bw6rr/SuLqOAx0Dbl2CC1sjKFaLRAudKnr3NAS755ctPhGPIO5Olf9nJZiDCRpwlyzCdb8l7Jh3VddtqG9GjhSrqGE0MqlR2tyi+R3f1FkoVe8+ZIBNt1A1XigJeVT//FsdEQYQ2bi4kG8jwdlICgY2T0Uo2BakfFVIskFUKRNbFgTLqKXWPTB7KAAH/P4zBW1Qtqs9XuzZIrDrak9EXt/4nO0PYVTCjC1B+DE/ZlqgO8SoGeJRz/NbAp6gxe0H1G7UQ+tr2QfZUA1jDUInylosQDufKpr0gPQMQepVI6XjpWkNrVu6zFwedN1W8gUSd6uDKb83QS49/pXSBWmEXSDC8dWs0a1SopdbroqZxoVfg2QUuwdMa7LHQ71fg63yYMXErIa9mci58CEMQnqsgczMaVyNClb7uWdR3e4i5DRgaF2rENuM0wT8Ihm49Z1HLbmqkiHJLQ9t7RaQP+M51GMBc53ygBsgA2TCEsXCBYMM1nhO5IVuZ0+Xu2iJvl2TeBM5UZD7NYECo6WqfRlsy1+/pNCFOBucFuChWqITn9bwAsVu1Th+2r2DHoN+/JO1b2cRcr4vzG5ci5r0n6BObhPtSAYif4fhbqAsOiEAWHQWJRuAZfS2XbIu7Ormi0LxIhRoX5zZwU26MJud1yVsf6ZQD0GQF2TqZkHrqbr9ey2QojNHernYv0JA1pqIIfEuxddQwYh5FJgcmdwbKUzIubGUn/FnbWPQiJuAoGU/3qiC6Y5VbEUazRvRufbABgbmmJHZghyxO4yDuECfNWDYNyY7G+T6aGXLpysywgZxIdPxTbyYJ8DbyE9Ir5foQIBpXby+ULVTrOQNbuUlt4iYY0QcAzlK2HRm/ek46r8Sip+3axzebvXy43QJ/XqMF2FTph0qQyIQeqXrjGixjgYQ+gRiVRuS06TWBIMjToG4H5G5UebBNoAir7B0AQzDNgHJt8Jrr2k5AHkr7/nIoiYOwkav7Yo5+FCVWBhr8NT7++qgtqK8CFpHRD5wkWEYAUCFQysYf1F8SRYkeRPbIpYBjhQzGbqbJ6KlF1eETp8oAeXC672L5kiC4PMMmqo/wOINpB//pHNPEsVaMOKuYiEN3fGD6e38zAXeddchn2J9s6QSnjcl33ZHDO9vyoKKHfVYmW/skE2TljaxiS+1zuCjhCMT60QYqBRSUFsIh6aHXxSj2IEgmc64kqErgyOJKS80nDGz0HVVdCVHJXsQadZrrJB1+itIW4H7xlquVHW0/tnTibnRyzK5P6u15Z3JAk4ls86hUEC6lbGK7lJ+Haalcot9QuKRZ7iPMsYlODLOI93A1Tz1E4ahy7uInECaa8fSCLY0ccv1Wx0VM8E77yZbcDn55rH9zeYz7cg6S8a6aD3Pvx+8khN8fKCX5CJj4PBPJKbH71QIhfgjUATJROL144wr3KkeYnzt1ScqGAqfzDu/5bV1B1tkF6rm5SvsOBcdYZW7Tq4oPxYyExbiBMkXzRw0UbCDrV1cCblw43wLEpZtpIkR0P3pf/iD6IvU+hdplSfp62Qvj4HeyuVfZZMgM59O7sPqqHvIxPoJb9T2TSfE/B5/EYr9rDB8qCCWaJxfwmzv6n/xF3RfHqJbWDZY0iPMHczaminOFEjrcrTa2cpCUAc1qGxj+PnAbTppjwmsMkKFCIaL9GwY2W+I4Io3dp3YMoGqRoHAlWLPVL/jh3fvcm6SluMAeuXltXorczpglslG1YAudgyfhIcZF/LIevQgiAKdFln+yVApmObVJ3gSEj2u1T0f7Jy2/PVTGbZrt9RaLyd4u2gm6dTWJO6jADJKGe43Vk1ec5dpOsCfl8mwtpeHZ8DMoSf0L63iNqvETCZe6DQzIPjX57NKBYg2wDLzVObz+fJF3IJWOxvgF6q7J1q2Gnpwm7IXibAzUS3EohgFQy6x6gersbv72kvZAhRDiexovVP6euh3oAgJpMMN4vCrJvNbFOB5cEC2ZTWaYs+qqQZvsh6I36W2UBbbpCgRyNR2Jfm0ffZW76ybjqmyn8Tnmyam+shdSn5bS5z2ew86hImOhv9aqfRL3JQuKJZictnKfNY6195Gz6DD9EyvxVTN+qzzpjLTM3nYuH1zXN9bZz+jKvOc3DygPkGPRAcFRewfQY9v8jACCbojc9QYTKqACJXPvzIwwggAOxZTPwU8sKxM8nq8zpd9d+H3VXQ7hHjTaLlQP4ocKiu0sxRFUuuCWx5mGkTSFt9yOrvAinnZFckMZx2UQkzatZk5c5tKaZdDpkv4WB/wshRBAlJl4SzN+GVY0qdAjIwTLH15IJZxj+p1nUgTBd19SK4WHL2WC1KNIQ2YIqCFUe+baCTPIW9XZtEIQ4wJwpItkbD1i+cs6LPQejapmIcTY1EjMFL7OrwT82FB7ac7gWnv3QIGcUyn2GQoDuBftpxnYzKvKvEz1JBD64os3hjbkGLxpJAJzhft91bCyp/LjeVmCXjmj8X6cMGkJEALjBPuB6htqRXdWNmVbD9qVsOsmWyy3USqPMPTLXzqUNytMuGHaP4YAT0tsE5m5s/ANHnhaQK8rowD8fEuSI8VjQYaKt7YEDd5jT0ljwf3aC2mB+hCxK7W7myTTU6GsJnWy7wFbGHi7DQC+0OQyAVuBw26PmecxOsdMQ0mA7EEemFO46uFT0w8bM86NoebI9KC5FDQh7DiDDiUWYSbZa/E+AKW6C9ADaYlMIg2Fi9tfptqeL0euFQCTo/QDk/Dv2AqGs5xTIk2+I50UfIT7x1SEOXErodN6C+qxpcGMLH5C/7rLo1lgMLGHRNSPKCBmqrrKiOt1eGtWHbE42kcZStPtSvj+ElQ9vIrHEYKITiwXaPuu3JggpaJOqKbDHnDlmosuECzXeVlRDaJyhnQ0FlmtUYOwEJ/X+QRgp84c0MCK/ZwKOq4OWQYzT4/nh4kjJEL0Jqmzx3tDCcKGUruzi+bXVwNQVEZusjlIM+20ul0Ed/NQirkyiMPTiVAjTXNuYKg4hIFvQq+h 3".split(),
-        capture_output=True)
-    # print(out.stdout.decode('utf-8'))
     parser = MjlogParser()
-    for state in parser.parse("resources/2011020417gm-00a9-0000-b67fcaa3.mjlog"):
+    path_to_mjlog = "resources/2011020417gm-00a9-0000-b67fcaa3.mjlog"
+    wall_dices = reproduce_wall(path_to_mjlog)
+    for state in parser.parse(path_to_mjlog, wall_dices):
         print(json.dumps(json_format.MessageToDict(state, including_default_value_fields=False)))
