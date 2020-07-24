acb82335f053e7fe1e0d74c2ae9c0ddfb4f4345e
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-07-24 14:04:18 +0900

    add mjlog_encoder

---
+++ tenhou/mjlog_encoder.py
@@ -0,0 +1,17 @@
+class MjlogEncoder:
+    def __init__(self):
+        pass
+
+    def parse(self):
+        pass
+
+    def _parse_each_round(self):
+        pass
+
+    def to_string(self):
+        pass
+
+
+if __name__ == "__main__":
+    path_to_json = "resources/json/2011020417gm-00a9-0000-b67fcaa3.json"
+    print("hello world")
