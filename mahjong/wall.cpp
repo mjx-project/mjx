9a072d12fc087874c15bace7802ad457b04b774e
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-07-26 17:36:32 +0900

    Align wall implementation to that of Tenhou (#171)
    * fix: align initial hand impementation to Tenhou's wall implementation
    
    * fix: bugs of mjlog_decoder.py
    
    * fix: dora position of wall following tenhou
    
    * fix: kan draw
    
    * fix wall docs and make HasDrawLeft const
    
    * fix: typo

--- mahjong/wall.cpp
+++ mahjong/wall.cpp
@@ -5,10 +5,17 @@
 
 namespace mj
 {
-    Wall::Wall(std::uint32_t seed)
-            : seed_(seed),
+    Wall::Wall(std::uint32_t round, std::uint32_t seed)
+            : round_(round), seed_(seed),
               tiles_(Tile::CreateAllShuffled(seed)),
-              itr_curr_draw_(draw_begin()), itr_curr_kan_draw_(kan_draw_begin())
+              itr_curr_draw_(draw_begin())
+    {}
+
+
+    Wall::Wall(std::uint32_t round, std::vector<Tile> tiles)
+            : round_(round), seed_(-1),
+              tiles_(std::move(tiles)),
+              itr_curr_draw_(draw_begin())
     {}
 
     Tile Wall::Draw() {
@@ -20,16 +27,20 @@ namespace mj
     }
 
     Hand Wall::initial_hand(AbsolutePos pos) const {
-        auto i = ToUType(pos);
-        return Hand(initial_hand_begin(i), initial_hand_end(i));
-    }
-
-    std::vector<Tile>::const_iterator Wall::initial_hand_begin(int pos) const {
-        return tiles_.cbegin() + (13 * pos);
-    }
-
-    std::vector<Tile>::const_iterator Wall::initial_hand_end(int pos) const {
-        return tiles_.cbegin() + (13 * (pos + 1));
+        auto pos_ix = ToUType(pos);
+        auto ix = ((pos_ix % 4 - round_ % 4 + 4) % 4) * 4;
+        std::vector<Tile> tiles;
+        tiles.reserve(13);
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                tiles.emplace_back(tiles_.at(ix++));
+            }
+            ix += 12;
+        }
+        ix = (pos_ix % 4 - round_ % 4 + 4) % 4 + 48;
+        tiles.emplace_back(tiles_.at(ix));
+        assert(tiles.size() == 13);
+        return Hand(tiles);
     }
 
     std::vector<Tile>::const_iterator Wall::draw_begin() const {
@@ -40,62 +51,11 @@ namespace mj
         return tiles_.cbegin() + 122;
     }
 
-    std::vector<Tile>::const_iterator Wall::kan_draw_begin() const {
-        return tiles_.cbegin() + 122;
-    }
-
-    std::vector<Tile>::const_iterator Wall::kan_draw_end() const {
-        return tiles_.cbegin() + 126;
-    }
-
-    std::vector<Tile>::const_iterator Wall::dora_begin() const {
-        return tiles_.cbegin() + 126;
-    }
-
-    std::vector<Tile>::const_iterator Wall::dora_end() const {
-        return tiles_.cbegin() + 131;
-    }
-
-    std::vector<Tile>::const_iterator Wall::ura_dora_begin() const {
-        return tiles_.cbegin() + 131;
-    }
-
-    std::vector<Tile>::const_iterator Wall::ura_dora_end() const {
-        return tiles_.cbegin() + 136;
-    }
-
-    std::string Wall::ToString(bool verbose) const {
-        std::string s;
-        for (auto it = initial_hand_begin(0); it != initial_hand_end(0); ++it) s += it->ToString(verbose) + ",";
-        s.pop_back(); s += "\n";
-        for (auto it = initial_hand_begin(1); it != initial_hand_end(1); ++it) s += it->ToString(verbose) + ",";
-        s.pop_back(); s += "\n";
-        for (auto it = initial_hand_begin(2); it != initial_hand_end(2); ++it) s += it->ToString(verbose) + ",";
-        s.pop_back(); s += "\n";
-        for (auto it = initial_hand_begin(3); it != initial_hand_end(3); ++it) s += it->ToString(verbose) + ",";
-        s.pop_back(); s += "\n";
-        for (auto it = draw_begin(); it != draw_end();) {
-            for (int i = 0; i < 6; ++i) {
-                s += it->ToString(verbose) + ",";
-                ++it;
-                if (it == draw_end()) break;
-            }
-            s.pop_back(); s += "\n";
-        }
-        for (auto it = kan_draw_begin(); it != kan_draw_end(); ++it) s += it->ToString(verbose) + ",";
-        s.pop_back(); s += "\n";
-        for (auto it = dora_begin(); it != dora_end(); ++it) s += it->ToString(verbose) + ",";
-        s.pop_back(); s += "\n";
-        for (auto it = ura_dora_begin(); it != ura_dora_end(); ++it) s += it->ToString(verbose) + ",";
-        s.pop_back(); s += "\n";
-        return s;
-    }
-
     Tile Wall::KanDraw() {
         assert(abs(num_kan_draw_ - num_kan_dora_) <= 1);
         assert(num_kan_draw_ <= 3);
-        auto drawn_tile = *(kan_draw_begin() + num_kan_draw_);
-        num_kan_draw_++;
+        auto kan_ixs = std::vector<int>{134, 135, 132, 133};
+        auto drawn_tile = tiles_[kan_ixs[num_kan_draw_++]];
         return drawn_tile;
     }
 
@@ -105,22 +65,22 @@ namespace mj
         num_kan_dora_++;
     }
 
-    bool Wall::HasDrawLeft() {
+    bool Wall::HasDrawLeft() const {
         assert(abs(num_kan_draw_ - num_kan_dora_) <= 1);
         return itr_curr_draw_ + num_kan_draw_ != draw_end();
     }
 
     std::vector<Tile> Wall::doras() const {
         assert(abs(num_kan_draw_ - num_kan_dora_) <= 1);
-        std::vector<Tile> ret;
-        for (auto it = dora_begin(); it != dora_begin() + num_kan_dora_ + 1; ++it) ret.emplace_back(*it);
+        std::vector<Tile> ret = {tiles_[130]};
+        for (int i = 0; i < num_kan_dora_; ++i) ret.emplace_back(tiles_[128 - 2 * i]);
         return ret;
     }
 
     std::vector<Tile> Wall::ura_doras() const {
         assert(abs(num_kan_draw_ - num_kan_dora_) <= 1);
-        std::vector<Tile> ret;
-        for (auto it = ura_dora_begin(); it != ura_dora_begin() + num_kan_dora_ + 1; ++it) ret.emplace_back(*it);
+        std::vector<Tile> ret = {tiles_[131]};
+        for (int i = 0; i < num_kan_dora_; ++i) ret.emplace_back(tiles_[129 - 2 * i]);
         return ret;
     }
 }  // namespace mj
--- mahjong/wall.h
+++ mahjong/wall.h
@@ -10,56 +10,37 @@ namespace mj
     class Wall
     {
         /*
-         * 136 tiles, indexed [0, 135]
-         *  - [0, 51] (13*4=52): initial hands of 4 players 配牌
-         *  - [52, 121] (70): draws ツモ
-         *  - [122, 125] (4): kan draws 嶺上牌
-         *  - [126] (1):  dora ドラ
-         *  - [127, 130] (4): kan doras カンドラ
-         *  - [131] (1): ura dora 裏ドラ
-         *  - [132, 135] (4): kan ura doras カンドラ裏
-         */
-
-        /*
-         * Tenhou's wall
+         * This wall class implementation follows Tenhou's wall implementation:
          *
-         *  136 tiles, indexed [0, 135]
-         *  - [0, 51] (13*4=52): initial hands of 4 players 配牌
-         *    - Depends on round.
-         *  - [52, 121] (70): draws ツモ
+         *  136 tiles, indexed [0, ..., 135]
+         *  - [0, ..., 51] (13*4=52): initial hands of 4 players 配牌
+         *    - **Initial hands depend on round info**
+         *  - [52, ..., 121] (70): draws ツモ
          *  - [122, 124, 126, 128] Kan dora 3, 2, 1, 0
          *  - [123, 125, 127, 129] Kan ura Dora 3, 2, 1, 0
          *  - [130] Dora
          *  - [131] Ura dora
-         *  - [132, ..., 135]  Kan draw 3, 2, 1, 0
+         *  - [132, ..., 135]  Kan draw 2, 3, 0, 1  TODO (sotetsuk) check and test this.
          */
     public:
-        explicit Wall(std::uint32_t seed = 9999);
+        explicit Wall(std::uint32_t round, std::uint32_t seed = 9999);  // round info is necessary due to Tenhou's implementation
+        Wall(std::uint32_t round, std::vector<Tile> tiles);
         [[nodiscard]] Hand initial_hand(AbsolutePos pos) const;
         [[nodiscard]] std::vector<Tile> doras() const;
         [[nodiscard]] std::vector<Tile> ura_doras() const;
+        [[nodiscard]] bool HasDrawLeft() const;
         Tile Draw();
         Tile KanDraw();
         void AddKanDora();
-        bool HasDrawLeft();
-        [[nodiscard]] std::string ToString(bool verbose = false) const;
     private:
+        std::uint32_t round_;
         std::uint32_t seed_;
         std::vector<Tile> tiles_;
         std::vector<Tile>::const_iterator itr_curr_draw_;
-        std::vector<Tile>::const_iterator itr_curr_kan_draw_;
         int num_kan_draw_ = 0;
         int num_kan_dora_ = 0;
-        [[nodiscard]] std::vector<Tile>::const_iterator initial_hand_begin(int pos) const;
-        [[nodiscard]] std::vector<Tile>::const_iterator initial_hand_end(int pos) const;
         [[nodiscard]] std::vector<Tile>::const_iterator draw_begin() const;
         [[nodiscard]] std::vector<Tile>::const_iterator draw_end() const;
-        [[nodiscard]] std::vector<Tile>::const_iterator kan_draw_begin() const;
-        [[nodiscard]] std::vector<Tile>::const_iterator kan_draw_end() const;
-        [[nodiscard]] std::vector<Tile>::const_iterator dora_begin() const;
-        [[nodiscard]] std::vector<Tile>::const_iterator dora_end() const;
-        [[nodiscard]] std::vector<Tile>::const_iterator ura_dora_begin() const;
-        [[nodiscard]] std::vector<Tile>::const_iterator ura_dora_end() const;
     };
 }  // namespace mj
 
