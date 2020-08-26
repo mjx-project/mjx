a9fa2f6c2696e00df9e6a44e685e73b664224234
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-08-26 11:12:00 +0900

    feat: impelemnt state encoding/decoding test

--- test/state_test.cpp
+++ test/state_test.cpp
@@ -1,3 +1,5 @@
+#include <fstream>
+#include <filesystem>
 #include "gtest/gtest.h"
 #include "state.h"
 #include "utils.h"
@@ -477,4 +479,21 @@ TEST(state, Update) {
     actions = { Action::CreateRon(AbsolutePos::kInitWest) };
     state_before.Update(std::move(actions));
     EXPECT_EQ(state_before.ToJson(), state_after.ToJson());
+}
+
+TEST(state, tenhou) {
+    auto check = [](const auto &filename) {
+        std::ifstream reading_file;
+        reading_file.open(filename, std::ios::in);
+        std::string original_json, restored_json;
+        while (!reading_file.eof()) {
+            std::getline(reading_file, original_json);
+            restored_json = State(original_json).ToJson();
+            EXPECT_EQ(original_json, restored_json);
+        }
+    };
+
+    std::string json_path;
+    // json_path = "";
+    if (!json_path.empty()) for (const auto &filename : std::filesystem::directory_iterator(json_path)) check(filename);
 }
