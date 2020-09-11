7dca7f09db2a3bbd858190f45dea59666ddc4b73
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-09-11 10:48:32 +0900

    天和時にunordered_mapのキーエラーで実行時に落ちるのを修正 (#309)
    * feat: add test cases for oor error
    
    * feat: implement blessing of heaven
    
    * fix: test example
    
    * fix: wrong ext

--- mahjong/state.h
+++ mahjong/state.h
@@ -63,7 +63,7 @@ namespace mj
         bool require_riichi_score_change_ = false;
         bool require_kan_draw_ = false;
         int require_kan_dora_ = 0;  // 加槓 => 暗槓が続いたときに2回連続でカンドラを開く場合がある https://github.com/sotetsuk/mahjong/issues/199
-        std::unordered_map<AbsolutePos, bool> is_ippatsu_;
+        std::unordered_map<AbsolutePos, bool> is_ippatsu_ = {{AbsolutePos::kInitEast, false}, {AbsolutePos::kInitSouth, false}, {AbsolutePos::kInitWest, false}, {AbsolutePos::kInitNorth, false}};
 
         // accessors
         [[nodiscard]] const Player& player(AbsolutePos pos) const;
