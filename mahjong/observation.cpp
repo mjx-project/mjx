44fc6e6a91128a14b577bda03a2bf69f1b30bea2
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-09-15 20:39:18 +0900

    fix: observation constructor

--- mahjong/observation.cpp
+++ mahjong/observation.cpp
@@ -20,12 +20,15 @@ namespace mj
     }
 
     Observation::Observation(AbsolutePos who, const mjproto::State &state) {
-        // proto_.mutable_player_ids()->CopyFrom(state.player_ids());
+        proto_.mutable_player_ids()->CopyFrom(state.player_ids());
         proto_.mutable_init_score()->CopyFrom(state.init_score());
-        // proto_.mutable_doras()->CopyFrom(state.doras());
+        proto_.mutable_doras()->CopyFrom(state.doras());
+        // TODO: avoid copy by
+        // proto_.set_allocated_event_history(&state.mutable_event_history());
+        // proto_.release_event_history(); // in deconstructor
         proto_.mutable_event_history()->CopyFrom(state.event_history());
         proto_.set_who(mjproto::AbsolutePos(who));
-        // proto_.mutable_private_info()->CopyFrom(state.private_infos(ToUType(who)));
+        proto_.mutable_private_info()->CopyFrom(state.private_infos(ToUType(who)));
     }
 
     bool Observation::has_possible_action() const {
