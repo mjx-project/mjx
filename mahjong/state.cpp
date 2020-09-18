1e0562ad3e465f000d7a293b644163c28542deaf
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-09-18 14:58:20 +0900

    fix: when to reset furiten

--- mahjong/state.cpp
+++ mahjong/state.cpp
@@ -228,7 +228,6 @@ namespace mj
                 missed_tiles[AbsolutePos(i)].set(ix);
             }
         }
-        if (!player(who).IsUnderRiichi()) missed_tiles[who].reset();  // フリテン解除
 
         auto draw = require_kan_draw_ ? wall_.KanDraw() : wall_.Draw();
         require_kan_draw_ = false;
@@ -248,6 +247,7 @@ namespace mj
         auto [discarded, tsumogiri] = mutable_player(who).Discard(discard);
         assert(discard == discarded);
 
+        if (!player(who).IsUnderRiichi()) missed_tiles[who].reset();  // フリテン解除
         last_ronable_tile = discard; // ロンされうる牌を更新
 
         is_ippatsu_[who] = false;
