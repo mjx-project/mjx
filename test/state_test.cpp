c3c684d03cb19f1a3de854efc5c3c9e2f42937f0
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-09-21 03:56:35 +0900

    Show filename if test fails (#355)
    * feat: show filename if failed
    
    * refactor: add ()
    
    * fix to show filename
    
    * refactor: use stuctured bindings

--- test/state_test.cpp
+++ test/state_test.cpp
@@ -135,13 +135,16 @@ bool ParallelTest(F&& f) {
         // }
         int curr = begin;
         while (curr < end) {
-            const std::string &json = jsons[curr];
+            const auto &[json, filename] = jsons[curr];
             bool ok = f(json);
             {
                 std::lock_guard<std::mutex> lock(mtx_);
                 total_cnt++;
-                if (!ok) failure_cnt++;
-                if (total_cnt % 1000 == 0) std::cerr << "# failure = " << failure_cnt  << "/" << total_cnt << " " << 100.0 * failure_cnt / total_cnt << " %" << std::endl;
+                if (!ok) {
+                    failure_cnt++;
+                    std::cerr << filename << std::endl;
+                }
+                if (total_cnt % 1000 == 0) std::cerr << "# failure = " << failure_cnt  << "/" << total_cnt << " (" << 100.0 * failure_cnt / total_cnt << " %)" << std::endl;
             }
             curr++;
         }
@@ -149,7 +152,7 @@ bool ParallelTest(F&& f) {
 
     const auto thread_count = std::thread::hardware_concurrency();
     std::vector<std::thread> threads;
-    std::vector<std::string> jsons;
+    std::vector<std::pair<std::string, std::string>> jsons;
     std::string json_path = std::string(TEST_RESOURCES_DIR) + "/json";
 
     auto Run = [&]() {
@@ -171,13 +174,13 @@ bool ParallelTest(F&& f) {
             std::string json;
             std::getline(ifs, json);
             if (json.empty()) continue;
-            jsons.emplace_back(std::move(json));
+            jsons.emplace_back(std::move(json), filename.path().string());
         }
         if (jsons.size() > 1000) Run();
     }
     Run();
 
-    std::cerr << "# failure = " << failure_cnt  << "/" << total_cnt << " " << 100.0 * failure_cnt / total_cnt << " %" << std::endl;
+    std::cerr << "# failure = " << failure_cnt  << "/" << total_cnt << " (" << 100.0 * failure_cnt / total_cnt << " %)" << std::endl;
     return failure_cnt == 0;
 }
 
