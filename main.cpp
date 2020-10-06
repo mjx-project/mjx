efc967649165734a114162b9738157d8956eb996
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-10-07 03:26:55 +0900

    fix: remove unnecessary main function in agent_server_mock.cpp (#389)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "mahjong/agent_server_mock.h"
 
 int main() {
+    std::unique_ptr<mj::AgentServer> mock_agent =  std::make_unique<mj::MockAgentServer>();
+    mock_agent->RunServer("127.0.0.1:9090");
     std::cerr << "Hello" << std::endl;
     return 0;
 }
