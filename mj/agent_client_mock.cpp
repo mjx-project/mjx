3b24a8132159daf1e7df5f931752d69017cd37b5
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-10-09 18:58:02 +0900

    feat: implement tsumo for agent client mock

--- mj/agent_client_mock.cpp
+++ mj/agent_client_mock.cpp
@@ -6,7 +6,13 @@ namespace mj
     Action AgentClientMock::TakeAction(Observation &&observation) const {
         // Currently this method only implements discard
         mjproto::Action response;
+        response.set_who(mjproto::AbsolutePos(observation.who()));
         for (const auto &possible_action: observation.possible_actions()) {
+            if (possible_action.type() == ActionType::kTsumo) {
+                response.set_type(mjproto::ActionType::ACTION_TYPE_TSUMO);
+                break;
+            }
+
             if (possible_action.type() == ActionType::kDiscard) {
                 // random action
                 const auto &discard_candidates = possible_action.discard_candidates();
@@ -16,8 +22,6 @@ namespace mj
                 break;
             }
         }
-        response.set_who(mjproto::AbsolutePos(observation.who()));
-        auto action = Action(std::move(response));
-        return action;
+        return Action(std::move(response));
     }
 }
