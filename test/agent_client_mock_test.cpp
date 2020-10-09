d89ebb0b6849069c68c802a7a416ee1ec089da94
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-10-09 11:50:56 +0900

    test: add test utility

--- test/agent_client_mock_test.cpp
+++ test/agent_client_mock_test.cpp
@@ -4,11 +4,22 @@
 
 using namespace mj;
 
+// Test utilities
+std::string GetLastJsonLine(const std::string &filename) {
+    auto json_path = std::string(TEST_RESOURCES_DIR) + "/json/" + filename;
+    std::ifstream ifs(json_path, std::ios::in);
+    std::string buf, json_line;
+    while (!ifs.eof()) {
+        std::getline(ifs, buf);
+        if (buf.empty()) break;
+        json_line = buf;
+    }
+    return json_line;
+}
+
 TEST(agent_client_mock, TakeAction) {
-    // // random discard
-    // auto state = State(9999);
-    // std::unique_ptr<AgentClient> agent = std::make_unique<AgentClientMock>();
-    // auto drawer = state.UpdateStateByDraw();
+    State state; Action action;
+    std::unique_ptr<AgentClient> agent = std::make_unique<AgentClientMock>();
     // auto action = agent->TakeAction(state.CreateObservation(drawer));
     // EXPECT_EQ(action.type(), ActionType::kDiscard);
 }
