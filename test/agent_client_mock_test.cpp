d2f8a4360954f6b9e88a41b879f0e77ef78cc4e1
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-10-09 12:01:03 +0900

    Revert "test: add test utility"
    This reverts commit d89ebb0b6849069c68c802a7a416ee1ec089da94.

--- test/agent_client_mock_test.cpp
+++ test/agent_client_mock_test.cpp
@@ -4,22 +4,11 @@
 
 using namespace mj;
 
-// Test utilities
-std::string GetLastJsonLine(const std::string &filename) {
-    auto json_path = std::string(TEST_RESOURCES_DIR) + "/json/" + filename;
-    std::ifstream ifs(json_path, std::ios::in);
-    std::string buf, json_line;
-    while (!ifs.eof()) {
-        std::getline(ifs, buf);
-        if (buf.empty()) break;
-        json_line = buf;
-    }
-    return json_line;
-}
-
 TEST(agent_client_mock, TakeAction) {
-    State state; Action action;
-    std::unique_ptr<AgentClient> agent = std::make_unique<AgentClientMock>();
+    // // random discard
+    // auto state = State(9999);
+    // std::unique_ptr<AgentClient> agent = std::make_unique<AgentClientMock>();
+    // auto drawer = state.UpdateStateByDraw();
     // auto action = agent->TakeAction(state.CreateObservation(drawer));
     // EXPECT_EQ(action.type(), ActionType::kDiscard);
 }
