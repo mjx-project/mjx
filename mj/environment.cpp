ae8151792cd2df44a48c9979d28ef26b12fc9f2d
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-10-11 20:10:28 +0900

    refactor: remove unnecesary coments

--- mj/environment.cpp
+++ mj/environment.cpp
@@ -21,31 +21,6 @@ namespace mj
     }
 
     void Environment::RunOneRound() {
-        // state_ = State();
-        // while (!state_.IsRoundOver()) {
-        //     auto drawer = state_.UpdateStateByDraw();
-        //     // discard, riichi_and_discard, tsumo, kan_closed or kan_added. (At the first draw, 9種9牌）
-        //     auto action = agent(drawer).TakeAction(state_.CreateObservation(drawer));
-        //     state_.UpdateStateByAction(action);
-        //     // TODO(sotetsuk): assert that possbile_actions are empty
-        //     if (auto winners = state_.RonCheck(); winners) {
-        //         std::vector<Action> action_candidates;
-        //         for (AbsolutePos winner: winners.value()) {
-        //             // only ron
-        //             action_candidates.emplace_back(agent(winner).TakeAction(state_.CreateObservation(winner)));
-        //         }
-        //         state_.UpdateStateByActionCandidates(action_candidates);
-        //     }
-        //     if (auto stealers = state_.StealCheck(); stealers) {
-        //         std::vector<Action> action_candidates;
-        //         // TODO (sotetsuk): make gRPC async
-        //         for (auto &[stealer, possible_opens]: stealers.value()) {
-        //             // chi, pon and kan_opened
-        //             action_candidates.emplace_back(agent(stealer).TakeAction(state_.CreateObservation(stealer)));
-        //         }
-        //         state_.UpdateStateByActionCandidates(action_candidates);
-        //     }
-        // }
     }
 
     std::shared_ptr<AgentClient> Environment::agent(AbsolutePos pos) const {
