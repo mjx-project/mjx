4dde08ff870382697b1c5c063313952f19f4d238
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-10-16 19:40:48 +0900

    refactor: to handle with non-ended game

--- mjconvert/src/main.py
+++ mjconvert/src/main.py
@@ -4,51 +4,90 @@ import sys
 import json
 import argparse
 from argparse import RawTextHelpFormatter
+from google.protobuf import json_format
+from . import mj_pb2
 from .mjlog_encoder import MjlogEncoder
 from .mjlog_decoder import MjlogDecoder
 
 
-class Converter:
-    def __init__(self, fmt_to: str):
-        self.fmt_from: str = ""
-        self.fmt_to: str = fmt_to
-        self.converter = None
+class LineBuffer:
+    """Split lines of inputs by game end."""
+    def __init__(self, fmt: str):
+        self.fmt_: str = fmt
+        self.buffer_: List[List[str]] = []
+        self.is_game_end = False
 
     @staticmethod
-    def _detect_format(line: str) -> str:
-        try:
-            json.loads(line)
-            return "mjproto"
-        except:
-            return "mjlog"
+    def mjproto_game_end_(line):
+        d = json.loads(line)
+        state = json_format.ParseDict(d, mj_pb2.State())
+        return state.terminal.is_game_over
 
-    def _init_converter(self, line: str):
-        self.fmt_from = Converter._detect_format(line)
+    def put(self, line) -> None:
+        assert not self.is_game_end  # please call get() before put new line
+        if self.fmt_.startswith("mjproto"):
+            if len(self.buffer_) == 0:
+                self.buffer_.append([])
+            self.buffer_[-1].append(line)
+            self.is_game_end = False
+            if LineBuffer.mjproto_game_end_(line):
+                self.is_game_end = True
+        elif self.fmt_ == "mjlog":
+            self.buffer_.append([line])  # each line corresponds to each game
+            self.is_game_end = True  # mjlogは各行が1試合に対応
+
+    def get(self) -> List[List[str]]:  # each List[str] corresponds to each game.
+        tmp = self.buffer_
+        self.buffer_ = []
+        self.is_game_end = False
+        return tmp
+
+    def empty(self) -> bool:
+        return len(self.buffer_) == 0
+
+
+def detect_format(line: str) -> str:
+    try:
+        json.loads(line)
+        return "mjproto"
+    except:
+        return "mjlog"
+
+
+class Converter:
+    def __init__(self, fmt_from: str, fmt_to: str):
+        self.fmt_from: str = fmt_from
+        self.fmt_to: str = fmt_to
+        self.mjlog2mjproto = None
+        self.mjproto2mjlog = None
+
         if self.fmt_from == "mjproto" and self.fmt_to == "mjlog":
-            self.converter = MjlogEncoder()
+            self.mjproto2mjlog = MjlogEncoder()
         elif self.fmt_from == "mjlog" and self.fmt_to == "mjproto":
-            self.converter = MjlogDecoder(modify=True)
+            self.mjlog2mjproto = MjlogDecoder(modify=True)
         elif self.fmt_from == "mjlog" and self.fmt_to == "mjproto_raw":
-            self.converter = MjlogDecoder(modify=False)
+            self.mjlog2mjproto = MjlogDecoder(modify=False)
         else:
             sys.stderr.write(f"Input format = {self.fmt_from}\n")
-            sys.stderr.write(f"Output format = {self.fmt_from}\n")
+            sys.stderr.write(f"Output format = {self.fmt_to}\n")
             raise ValueError("Input format and output format should be different")
 
-    def convert(self, line: str) -> List[str]:
-        if self.converter is None:
-            self._init_converter(line)
+    def convert(self, lines: List[str]) -> List[str]:
+        """Transform a log of completed (or non-completed) one game into another format.
 
+        :param lines: must correspond to completed (or non-completed) one game
+        :return: also correspond to completed (or non-completed) one game
+        """
         if self.fmt_from == "mjproto" and self.fmt_to == "mjlog":
-            self.converter.put(line)
-            if self.converter.is_completed():
-                return [self.converter.get()]
-            else:
-                return []
+            for line in lines:
+                self.mjproto2mjlog.put(line)
+            return [self.mjproto2mjlog.get()]  # a mjlog line corresponds to one game
         if self.fmt_from == "mjlog" and self.fmt_to == "mjproto":
-            return self.converter.decode(line)
+            assert len(lines) == 1  # each line has each game
+            return self.mjlog2mjproto.decode(lines[0])
         if self.fmt_from == "mjlog" and self.fmt_to == "mjproto_raw":
-            return self.converter.decode(line)
+            assert len(lines) == 1  # each line has each game
+            return self.mjlog2mjproto.decode(lines[0])
         else:
             raise NotImplementedError
 
@@ -87,7 +126,9 @@ Example (using stdin)
   $ cat test.mjlog | mjconvert --to-mjproto-raw
   $ cat test.json  | mjconvert --to-mjlog
       
-Example (using file inputs)
+Example (using file inputs) 
+
+[NOTE] File inputs assume that each file corresponds to each game in any format.
     
   $ mjconvert ./mjlog_dir ./mjproto_dir --to-mjproto
   $ mjconvert ./mjlog_dir ./mjproto_dir --to-mjproto-raw
@@ -109,18 +150,34 @@ Difference between mjproto and mjproto-raw:
     assert (args.dir_from and args.dir_to) or (not args.dir_from and not args.dir_to)
     assert args.to_mjproto or args.to_mjproto_raw or args.to_mjlog
 
-    converter = Converter(to(args))
+    fmt_from: str = ""
+    converter: Converter = None
+    buffer: LineBuffer = None
 
-    if not args.dir_from and not args.dir_to:
+    if not args.dir_from and not args.dir_to:  # From stdin
         if args.verbose:
             sys.stderr.write(f"Converting to {to(args)}. stdin => stdout\n")
-        # From stdin
+
         itr = StdinIterator()
         for line in itr:
-            for transformed in converter.convert(line):
-                sys.stdout.write(transformed)
-    else:
-        # From files
+            if fmt_from == "":
+                fmt_from = detect_format(line)
+                converter = Converter(fmt_from, to(args))
+                buffer = LineBuffer(fmt_from)
+
+            buffer.put(line)
+            if buffer.is_game_end:  # 入力がmjprotoの場合は各lineが終局に対応していないため、終局のときだけ変換する
+                for lines in buffer.get():
+                    for transformed_line in converter.convert(lines):
+                        sys.stdout.write(transformed_line)
+
+        # stdin の最終行が終局になっていない場合のため
+        if not buffer.empty():
+            for lines in buffer.get():
+                for transformed_line in converter.convert(lines):
+                    sys.stdout.write(transformed_line)
+
+    else:  # From files
         if args.verbose:
             sys.stderr.write(f"Converting to {to(args)}. {args.dir_from} => {args.dir_to}\n")
 
@@ -129,19 +186,32 @@ Difference between mjproto and mjproto-raw:
         for file_from in os.listdir(args.dir_from):
             if not file_from.endswith("json") and not file_from.endswith("mjlog"):
                 continue
+
             path_from = os.path.join(args.dir_from, file_from)
             path_to = os.path.join(args.dir_to, os.path.splitext(os.path.basename(path_from))[0] + '.' + to_ext)
 
             if args.verbose:
                 sys.stderr.write(f"Converting {path_from} to {path_to}\n")
 
-            with open(path_from , 'r') as f:
-                transformed_lines = []
+            # 読み込み（全てのフォーマットで、１ファイル１半荘を想定）
+            transformed_lines = []
+            with open(path_from, 'r') as f:
                 for line in f:
                     if not line:
                         continue
-                    transformed_lines += converter.convert(line)
+                    if fmt_from == "":
+                        fmt_from = detect_format(line)
+                        converter = Converter(fmt_from, to(args))
+                        buffer = LineBuffer(fmt_from)
 
+                    buffer.put(line)
+
+            # 変換
+            list_lines: List[List[str]] = buffer.get()
+            assert len(list_lines) == 1  # each file has one game
+            transformed_lines += converter.convert(list_lines[0])
+
+            # 書き込み
             with open(path_to, 'w') as f:
                 for line in transformed_lines:
                     f.write(line)
