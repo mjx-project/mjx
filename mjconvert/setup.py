20c7c64d54059446c042e31b7af2a3f2e0577e84
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-10-22 17:31:13 +0900

    Fix to rm docker container (#443)
    * fix: rm docker container
    
    * fix: update version

--- mjconvert/setup.py
+++ mjconvert/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mjconvert',
-    version="0.0.1",
+    version="0.0.2",
     description='Converter for Mahjong log files',
     author='sotetsuk',
     author_email='koyamada-s@sys.i.kyoto-u.ac.jp',
