99cd56a198dddd6480b579fdb42d0e511ce50d2e
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-10-28 20:37:34 +0900

    refactor: rename test

--- test/game_summarizer_test.cpp
+++ test/game_summarizer_test.cpp
@@ -2,7 +2,7 @@
 #include <mj/game_result_summarizer.h>
 using namespace mj;
 
-TEST(game_summalizer, Add) {
+TEST(GameResultSummarizer, Add) {
     GameResultSummarizer game_summalizer;
     EXPECT_EQ(game_summalizer.n_game(), 0);
     game_summalizer.Add(GameResult());
