e3152a25e9c35a2c281b91fe4da93596e0ce7e4a
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-10-29 02:28:57 +0900

    feat: implement summarizer

--- mj/game_result_summarizer.h
+++ mj/game_result_summarizer.h
@@ -8,11 +8,25 @@ namespace mj
     class GameResultSummarizer
     {
     public:
-        [[nodiscard]] const int& n_game() const;
-        void Add(GameResult game_result);
-        void show() const;
+        struct PlayerPerformance
+        {
+            int num_games = 0;
+            std::map<int, int> num_ranking = {{1, 0}, {2, 0}, {3, 0}, {4, 0}};
+            std::optional<double> avg_ranking = std::nullopt;
+            // For the definition of stable dan (安定段位, see these materials:
+            //   - Appendix C. https://arxiv.org/abs/2003.13590
+            //   - https://tenhou.net/man/#RANKING
+            std::optional<double> stable_dan = std::nullopt;
+        };
+        [[nodiscard]] int num_games() const;
+        [[nodiscard]] const PlayerPerformance& player_performance(const PlayerId& player_id) const;
+        std::string string() const;
+        void Add(GameResult&& game_result);
     private:
-        int n_game_ = 0;
+        int num_games_ = 0;
+        std::map<PlayerId, PlayerPerformance> player_performances_;
+        static std::optional<double> avg_ranking(const std::map<int, int>& num_ranking);
+        static std::optional<double> stable_dan(const std::map<int, int>& num_ranking);
     };
 }
 #endif //MAHJONG_GAME_RESULT_SUMMARIZER_H
--- test/game_summarizer_test.cpp
+++ test/game_summarizer_test.cpp
@@ -2,9 +2,25 @@
 #include <mj/game_result_summarizer.h>
 using namespace mj;
 
-TEST(GameResultSummarizer, Add) {
-    GameResultSummarizer game_summalizer;
-    EXPECT_EQ(game_summalizer.n_game(), 0);
-    game_summalizer.Add(GameResult());
-    EXPECT_EQ(game_summalizer.n_game(), 1);
+TEST(GameResultSummarizer, Add)
+{
+    GameResultSummarizer summarizer;
+    EXPECT_EQ(summarizer.num_games(), 0);
+    summarizer.Add(GameResult{0, {{"A", 1}, {"B", 2}, {"C", 3}, {"D", 4}}});
+    EXPECT_EQ(summarizer.num_games(), 1);
+}
+
+TEST(GameResultSummarizer, player_performance)
+{
+    // avg ranking, stable dan
+    GameResultSummarizer summarizer;
+    summarizer.Add(GameResult{0, {{"A", 1}, {"B", 2}, {"C", 3}, {"D", 4}}});
+    summarizer.Add(GameResult{0, {{"A", 2}, {"B", 3}, {"C", 4}, {"D", 1}}});
+    summarizer.Add(GameResult{0, {{"A", 3}, {"B", 4}, {"C", 1}, {"D", 2}}});
+    summarizer.Add(GameResult{0, {{"A", 4}, {"B", 1}, {"C", 2}, {"D", 3}}});
+    for (const auto& player_id: {"A", "B", "C", "D"}) {
+        EXPECT_EQ(summarizer.player_performance(player_id).avg_ranking, 2.5);
+        EXPECT_EQ(summarizer.player_performance(player_id).stable_dan, 5.0);
+    }
+    std::cout << summarizer.string() << std::endl;
 }
