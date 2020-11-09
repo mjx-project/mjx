b082123f2f8941aa41ff6da641a78f8f4e2c0eb5
Keigo Habara <k.habara.aa@gmail.com>
2020-11-09 15:49:42 +0900

    Generate KanClosed and KanAdded train data

--- dataset/train_data_generator.h
+++ dataset/train_data_generator.h
@@ -7,5 +7,6 @@ namespace mj {
     public:
         static void generateDiscard(const std::string& src_path, const std::string& dst_path);
         static void generateOpen(const std::string& src_path, const std::string& dst_path, mjproto::ActionType open_type);
+        static void generateOpenYesNo(const std::string& src_path, const std::string& dst_path, mjproto::ActionType open_type);
     };
 }
