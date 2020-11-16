8cf68225ba17659fc1c6abaa6ff787c02a0fec20
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-11-16 21:06:25 +0900

    test: implement encode/decode test

---
+++ mjconvert/mjconvert/tests/test_mjlog.py
@@ -0,0 +1,20 @@
+import os
+from typing import List
+
+from mjconvert import mjlog_decoder, mjlog_encoder, check_equality
+
+
+def test_encode_decode():
+    decoder = mjlog_decoder.MjlogDecoder(modify=False)
+    encoder = mjlog_encoder.MjlogEncoder()
+    mjlog_dir = "resources/mjlog"
+    for mjlog in os.listdir(mjlog_dir):
+        mjlog = os.path.join(mjlog_dir, mjlog)
+        with open(mjlog, "r") as f:
+            lines = f.readlines()
+            assert len(lines) == 1
+            original = lines[0]
+            decoded: List[str] = decoder.decode(original, store_cache=True)
+            restored = encoder.encode(decoded)
+            print(mjlog)
+            assert check_equality(original, restored)
