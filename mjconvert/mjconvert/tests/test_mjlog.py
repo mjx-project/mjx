c33ef6f1eb05b5b6e8a7603f973bba74fb35934d
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-11-16 21:29:57 +0900

    fix test resource path specification

--- mjconvert/mjconvert/tests/test_mjlog.py
+++ mjconvert/mjconvert/tests/test_mjlog.py
@@ -7,7 +7,7 @@ from mjconvert import mjlog_decoder, mjlog_encoder, check_equality
 def test_encode_decode():
     decoder = mjlog_decoder.MjlogDecoder(modify=False)
     encoder = mjlog_encoder.MjlogEncoder()
-    mjlog_dir = "resources/mjlog"
+    mjlog_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources/mjlog")
     for mjlog in os.listdir(mjlog_dir):
         mjlog = os.path.join(mjlog_dir, mjlog)
         with open(mjlog, "r") as f:
