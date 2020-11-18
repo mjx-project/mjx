4e0a3504a4f2da1e1361e97d5ee481406db06942
n0n0a <n0n0a6262@gmail.com>
2020-11-18 14:26:03 +0900

    reafactor: member name

--- mj/wall_seed.h
+++ mj/wall_seed.h
@@ -9,10 +9,10 @@ namespace mj {
         WallSeed();
         WallSeed(std::uint64_t seed);
         std::uint64_t Get(int round, int honba) const;
-        [[nodiscard]] const std::uint64_t Seed() const;
+        [[nodiscard]] std::uint64_t seed() const;
 
     private:
-        std::uint64_t seed, round_base = 1000, honba_base = 1; // fix base-value
+        std::uint64_t seed_, round_base_ = 128, honba_base_ = 1; // fix base-value
         std::mt19937_64& mt();
     };
 } // namespace mj
