bddc2bf9ee475be03ea26193b6727c0cd920f688
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-11-25 19:36:45 +0900

    extract select discard

--- mj/agent_example_rule_based.h
+++ mj/agent_example_rule_based.h
@@ -13,6 +13,9 @@ namespace mj
         explicit AgentExampleRuleBased(PlayerId player_id);
         ~AgentExampleRuleBased() final = default;
         [[nodiscard]] mjproto::Action TakeAction(Observation &&observation) const final ;
+    private:
+        template<typename RandomGenerator>
+        Tile SelectDiscard(std::vector<Tile> &discard_candidates, const Hand &curr_hand, RandomGenerator& g) const;
     };
 }
 
