402a78f1879fda53010d5a404597ebd192585e2c
n0n0a <n0n0a6262@gmail.com>
2020-12-02 12:06:58 +0900

    add: member method

--- mj/strategy_rule_based.cpp
+++ mj/strategy_rule_based.cpp
@@ -1,5 +1,1 @@
-//
-// Created by gotup on 2020/12/02.
-//
-
 #include "strategy_rule_based.h"
--- mj/strategy_rule_based.h
+++ mj/strategy_rule_based.h
@@ -1,16 +1,28 @@
-//
-// Created by gotup on 2020/12/02.
-//
-
 #ifndef MAHJONG_STRATEGY_RULE_BASED_H
 #define MAHJONG_STRATEGY_RULE_BASED_H
 
+#include "agent.h"
 
+namespace mj
+{
+    // rule-based strategy (only methods)
+    class StrategyRuleBased:
+    {
+    public:
+        StrategyRuleBased(const StrategyRuleBased&) = delete;
+        StrategyRuleBased& operator=(StrategyRuleBased&) = delete;
+        StrategyRuleBased(StrategyRuleBased&&) = delete;
+        StrategyRuleBased& operator=(StrategyRuleBased&&) = delete;
 
-class strategy_rule_based {
+        [[nodiscard]] static mjproto::Action TakeAction(Observation &&observation);
+    private:
+        // インスタンス化禁止
+        StrategyRuleBased() = default;
+        ~StrategyRuleBased() = default;
 
-};
-
-
+        template<typename RandomGenerator>
+        static Tile SelectDiscard(std::vector<Tile> &discard_candidates, const Hand &curr_hand, RandomGenerator& g);
+    };
+}
 
 #endif //MAHJONG_STRATEGY_RULE_BASED_H
