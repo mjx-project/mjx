5c86c22f87ba093afa8202906d77eeefab2b317c
n0n0a <n0n0a6262@gmail.com>
2020-12-02 12:03:10 +0900

    add: class files

---
+++ mj/strategy_rule_based.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gotup on 2020/12/02.
+//
+
+#include "strategy_rule_based.h"
---
+++ mj/strategy_rule_based.h
@@ -0,0 +1,16 @@
+//
+// Created by gotup on 2020/12/02.
+//
+
+#ifndef MAHJONG_STRATEGY_RULE_BASED_H
+#define MAHJONG_STRATEGY_RULE_BASED_H
+
+
+
+class strategy_rule_based {
+
+};
+
+
+
+#endif //MAHJONG_STRATEGY_RULE_BASED_H
