68a26596bcf9868ca45782e3eaea1eab40132f70
n0n0a <n0n0a6262@gmail.com>
2020-12-02 13:26:03 +0900

    change: include agent.h -> observation.h and delete constructor

--- mj/strategy_rule_based.h
+++ mj/strategy_rule_based.h
@@ -1,7 +1,7 @@
 #ifndef MAHJONG_STRATEGY_RULE_BASED_H
 #define MAHJONG_STRATEGY_RULE_BASED_H
 
-#include "agent.h"
+#include "observation.h"
 
 namespace mj
 {
@@ -9,17 +9,8 @@ namespace mj
     class StrategyRuleBased
     {
     public:
-        StrategyRuleBased(const StrategyRuleBased&) = delete;
-        StrategyRuleBased& operator=(StrategyRuleBased&) = delete;
-        StrategyRuleBased(StrategyRuleBased&&) = delete;
-        StrategyRuleBased& operator=(StrategyRuleBased&&) = delete;
-
         [[nodiscard]] static mjproto::Action SelectAction(Observation &&observation);
     private:
-        // インスタンス化禁止
-        StrategyRuleBased() = default;
-        ~StrategyRuleBased() = default;
-
         template<typename RandomGenerator>
         static Tile SelectDiscard(std::vector<Tile> &discard_candidates, const Hand &curr_hand, RandomGenerator& g);
     };
