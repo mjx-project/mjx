93a2a6041b0b2bd59276a2a4514f0705c21a65f8
Nishimori Soichiro <55940613+nissymori@users.noreply.github.com>
2020-12-03 17:37:11 +0900

    mjlogからmjscoreへの変換を実装する (#458)
    * add score information
    
    * create function to sort init_hand
    
    * define function to change format, and sort inithand the last one was not correct
    
    * define function to return discards in mjscore
    
    * refactor functions
    
    * Implement open format transformation (#481)
    
    * feat: prepare open convertion function input/output
    
    * fix: rm inappropriate comment line
    
    * fix: rm unnecessary spaces
    
    * Rename mjconvert/src => mjconvert/mjconvert (#482)
    
    * refactor: rename dir src => mjconvert
    
    * fix: assertion
    
    * create function ecxept for kan
    
    * create function except for kan
    
    * change open_from and test passed
    
    * change open_tile_types and stolen_tile_type and tests are passed
    
    * change open_stolen_type and open_tile_types and tests were passed
    
    * change open_from stolen_tile_type and open_tile_types and tests were passed
    
    * change stolen_tile_type and open_tile_types for kan_closed and kan_opend
    
    * fix for kan_opend and kan_closed
    
    * create function for transforming red stolen tile not tested
    
    * create function for transforming red open tile not tested
    
    * add comments
    
    * add _min_tile_chi function
    
    * integrate transformation of red_stolen_tile in pon and kan_added because the process was the same
    
    * refactor open_stolen_tile_type and open_tile_types
    
    * create subfunction of transform_red_open_tile()
    
    * refactor subfunctions of transform_red_open_tile()
    
    * delete transform_red_open_tiles and open_tile_types pass through tests
    
    * add function to change_action_format
    
    * refactor parse_draws and change_action_format the same one to test was created regarding chi
    
    * create function to reformat_tile_in_open_tomjscore
    
    * refactor change_open_tiles_fmt
    
    * refactor change_action_fmt
    
    * add is stolen_red
    
    * add is_unused_red
    
    * add has_red_chi has_red_pon_kan_added
    
    * add has_red
    
    * refactor stolen_tile_type
    
    * refactor: simplify relative pos info
    
    * refactor rename to_do => TODO
    
    * move non-red functions above
    
    * fix typo
    
    * rename mj_pb2 => mjproto
    
    * Add basic test for mjscore conversion (#555)
    
    * extract to_states
    
    * apply formats
    
    * refactor: add test for log
    
    * feat: add log info in mjproto_to_mjscore
    
    * refactor: change test do not judge the result of the game
    
    * add mjscore test data #556
    
    * refactor: functionalize the judge part
    
    * feat: create function for result when there is no winner
    
    * feat: add yaku_list
    
    * feat: create function for corresponding yakus test passed
    
    * feat: create function winner point
    
    * refactor: refactoring parse_terminal
    
    * refactor: refacter doctests winner_point, winner_yakus, fan_fu all are passed
    
    * feat: add funtcion check_uradoras test_passed
    
    * refactor: reformat file and refacter doctest of winner_yakus
    
    * refactor: refactor test
    
    * apply formats
    
    * rename shadowed variables
    
    * refactor: refactor in order make checks to pass
    
    * refactor:reformat file
    
    * refactor: change & 3 into % 4
    
    * refactor: refactored about file changes
    
    * refactor:
    
    * fix merge mistake
    
    * apply formats
    
    * refactor: refactor for file changed
    
    * refactor: refactor for 382
    
    * reformat: reformat  mjconvert/mjscore_encoder.py
    
    Co-authored-by: Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>

--- mjconvert/mjconvert/mjscore_encoder.py
+++ mjconvert/mjconvert/mjscore_encoder.py
@@ -1,20 +1,385 @@
 from __future__ import annotations  # postpone type hint evaluation or doctest fails
 
 import json
-from typing import Dict
+from typing import Dict, List
 
 from google.protobuf import json_format
 
 import mjproto
+from mjconvert import open_converter
+
+
+def change_tile_fmt(tile_id: int) -> int:
+    reds_in_mjproto = [16, 52, 88]
+    reds_in_mjscore = [51, 52, 53]
+    reds_dict = dict(zip(reds_in_mjproto, reds_in_mjscore))
+    if tile_id in reds_in_mjproto:
+        tile_in_mjscore = reds_dict[tile_id]
+    else:
+        tile_in_mjscore = ((tile_id // 36) + 1) * 10 + ((tile_id % 36) // 4) + 1
+    return tile_in_mjscore
+
+
+# mjproto形式の牌のリストを引数にとり、表現をmjscore形式の表現に変える関数
+def change_tiles_fmt(tile_ids):
+    scores = list(map(change_tile_fmt, tile_ids))  # mjproto形式の表現ををmjscore形式に変換
+    return scores
+
+
+# openの情報を受け取ってmjscore形式に変更する関数
+def change_action_format(bits: int) -> str:
+    event_type = open_converter.open_event_type(bits)
+    open_from = open_converter.open_from(bits)
+    stolen_tile = open_converter.change_open_tile_fmt(open_converter.open_stolen_tile_type(bits))
+    open_tiles = open_converter.change_open_tiles_fmt(open_converter.open_tile_types(bits))
+    open_tiles.remove(stolen_tile)
+    if event_type == mjproto.EVENT_TYPE_CHI:  # 現状書き方があまりにも冗長。なんとかしたい。
+        return "c" + str(stolen_tile) + str(open_tiles[0]) + str(open_tiles[1])
+    elif event_type == mjproto.EVENT_TYPE_PON:
+        if open_from == mjproto.RELATIVE_POS_LEFT:
+            return "p" + str(stolen_tile) + str(open_tiles[0]) + str(open_tiles[1])
+        elif open_from == mjproto.RELATIVE_POS_LEFT:
+            return str(stolen_tile) + "p" + str(open_tiles[0]) + str(open_tiles[1])
+        else:
+            return str(stolen_tile) + str(open_tiles[0]) + "p" + str(open_tiles[1])
+    else:
+        return " "  # カンはまだmjscoreのformatがわからない。
+
+
+# mjscore形式の配牌をソートする関数。
+def sort_init_hand(init_hand: List) -> List:
+    # 11~19マンズ　21~29ピンズ　31~39ソウズ　#51~53赤マンピンソウ
+    reds_score = [51, 52, 53]  # 赤
+    init_key = [
+        int(str(i)[::-1]) + 0.1 if i in reds_score else i for i in init_hand
+    ]  # ソートする辞書のキー。赤は文字を反転させて、同じ種類の牌の中で最後尾になるようにソートする値に0.1を足す。
+    init_hand_dict = [[k, v] for k, v in zip(init_key, init_hand)]
+    sorted_hand = sorted(init_hand_dict, key=lambda x: x[0])
+    sorted_hand = [i[1] for i in sorted_hand]
+    return sorted_hand
+
+
+# mjproto形式のeventを受け取り、あるプレイヤーの捨て牌をmjscore形式で出力する関数。
+def parse_discards(events, abs_pos: int):
+    discards: List[object] = []
+    is_reach: bool = False  # リーチの有無
+    riichi_tile_list: List[int] = []  # リーチは一人一回なので論理的におかしいが、リーチ宣言牌をスコープを跨いで保存してmypyでエラーを出さないため。
+    for i, event in enumerate(events):
+        if event.type == mjproto.EVENT_TYPE_DISCARD_FROM_HAND and event.who == abs_pos:  # 手出し
+            discards.append(change_tile_fmt(event.tile))
+        elif event.type == mjproto.EVENT_TYPE_DISCARD_DRAWN_TILE and event.who == abs_pos:  # ツモギリ
+            discards.append(60)
+        elif event.type == mjproto.EVENT_TYPE_RIICHI and event.who == abs_pos:  # リーチ
+            is_reach = True
+            riichi_tile_list.append(
+                change_tile_fmt(events[i + 1].tile)
+            )  # riichiの次のeventに宣言牌が記録されているのでそのtileを取得して後にindexを取得変更
+    if is_reach:
+        riichi_tile = riichi_tile_list[0]
+        discards = [i if i != riichi_tile else "r" + str(i) for i in discards]  # リーチ宣言牌の形式変更
+    return discards
+
+
+# プレイヤーごとにmjscore形式のdrawsを返す。
+def parse_draws(draws, events, abs_pos):
+    """
+    - mjscoreでは引いた牌のリストにチーやポンなどのアクションが含まれている
+    - mjprotoの　drawsでは単に飛ばされていて、eventの方に情報がある
+
+    方針
+    1. チーポンも含めたdiscardsを作成
+    2. drawsの方で直前の捨て牌の直後にアクションを挿入
+    """
+    draws = change_tiles_fmt(draws)
+    discards = []
+    actions = []  #
+    for i, event in enumerate(events):
+        if event.type == mjproto.EVENT_TYPE_DISCARD_FROM_HAND and event.who == abs_pos:  # 手出し
+            discards.append(event.tile)
+        elif event.type == mjproto.EVENT_TYPE_DISCARD_DRAWN_TILE and event.who == abs_pos:  # ツモギリ
+            discards.append(60)
+        elif event.type == mjproto.EVENT_TYPE_CHI and event.who == abs_pos:  # チー
+            discards.append(event.open)
+            actions.append(event.open)
+        elif event.type == mjproto.EVENT_TYPE_PON and event.who == abs_pos:  # ポン
+            discards.append(event.open)
+            actions.append(event.open)
+    for i, action in enumerate(actions):
+        action_index = discards.index(action) - i  # 捨て牌でのactionのindex同じ順にdrawにアクションを挿入すれば良い
+        draws.insert(action_index, change_action_format(action))
+    return draws
+
+
+yaku_list_tumo = [
+    "門前清自摸和(1飜)",
+    "立直(1飜)",
+    "一発(1飜)",
+    "槍槓(1飜)",
+    "嶺上開花(1飜)",
+    "海底摸月(1飜)",
+    "河底撈魚(1飜)",
+    "平和(1飜)",
+    "断幺九(1飜)",
+    "一盃口(1飜)",
+    "自風 東(1飜)",
+    "自風 南(1飜)",
+    "自風 西(1飜)",
+    "自風 北(1飜)",
+    "場風 東(1飜)",
+    "場風 南(1飜)",
+    "場風 西(1飜)",
+    "場風 北(1飜)",
+    "役牌 白(1飜)",
+    "役牌 發(1飜)",
+    "役牌 中(1飜)",
+    "両立直(2飜)",
+    "七対子(2飜)",
+    "混全帯幺九(2飜)",
+    "一気通貫(2飜)",
+    "三色同順(2飜)",
+    "三色同刻(2飜)",
+    "三槓子(2飜)",
+    "対々和(2飜)",
+    "三暗刻(2飜)",
+    "小三元(2飜)",
+    "混老頭(2飜)",
+    "二盃口(3飜)",
+    "純全帯幺九(3飜)",
+    "混一色(3飜)",
+    "清一色(6飜)",
+    "人和",  # 天鳳は人和なし
+    "天和(役満)",
+    "地和(役満)",
+    "大三元(役満)",
+    "四暗刻(役満)",
+    "四暗刻単騎(役満)",
+    "字一色(役満)",
+    "緑一色(役満)",
+    "清老頭(役満)",
+    "九蓮宝燈(役満)",
+    "純正九蓮宝燈(役満)",
+    "国士無双(役満)",
+    "国士無双１３面(役満)",
+    "大四喜(役満)",
+    "小四喜(役満)",
+    "四槓子(役満)",
+    "ドラ(1飜)",
+    "裏ドラ(1飜)",
+    "赤ドラ(1飜)",
+]
+
+yaku_list_ron = [
+    "門前清自摸和(1飜)",
+    "立直(1飜)",
+    "一発(1飜)",
+    "槍槓(1飜)",
+    "嶺上開花(1飜)",
+    "海底摸月(1飜)",
+    "河底撈魚(1飜)",
+    "平和(1飜)",
+    "断幺九(1飜)",
+    "一盃口(1飜)",
+    "自風 東(1飜)",
+    "自風 南(1飜)",
+    "自風 西(1飜)",
+    "自風 北(1飜)",
+    "場風 東(1飜)",
+    "場風 南(1飜)",
+    "場風 西(1飜)",
+    "場風 北(1飜)",
+    "役牌 白(1飜)",
+    "役牌 發(1飜)",
+    "役牌 中(1飜)",
+    "両立直(2飜)",
+    "七対子(2飜)",
+    "混全帯幺九(1飜)",
+    "一気通貫(1飜)",
+    "三色同順(1飜)",
+    "三色同刻(2飜)",
+    "三槓子(2飜)",
+    "対々和(2飜)",
+    "三暗刻(2飜)",
+    "小三元(2飜)",
+    "混老頭(2飜)",
+    "二盃口(3飜)",
+    "純全帯幺九(2飜)",
+    "混一色(2飜)",
+    "清一色(5飜)",
+    "人和",  # 天鳳は人和なし
+    "天和(役満)",
+    "地和(役満)",
+    "大三元(役満)",
+    "四暗刻(役満)",
+    "四暗刻単騎(役満)",
+    "字一色(役満)",
+    "緑一色(役満)",
+    "清老頭(役満)",
+    "九蓮宝燈(役満)",
+    "純正九蓮宝燈(役満)",
+    "国士無双(役満)",
+    "国士無双１３面(役満)",
+    "大四喜(役満)",
+    "小四喜(役満)",
+    "四槓子(役満)",
+    "ドラ(1飜)",
+    "裏ドラ(1飜)",
+    "赤ドラ(1飜)",
+]
+yaku_list_keys = [i for i in range(55)]
+yaku_dict_tumo = {k: v for k, v in zip(yaku_list_keys, yaku_list_tumo)}
+yaku_dict_ron = {k: v for k, v in zip(yaku_list_keys, yaku_list_ron)}
+
+non_dealer_tsumo_dict = {
+    1100: "300-500",
+    1500: "400-700",
+    1600: "800-1600",
+    2000: "1000-2000",
+    2400: "600-1200",
+    2700: "700-1300",
+    3100: "800-1500",
+    3200: "800-1600",
+    3600: "900-1800",
+    4000: "1000-2000",
+    4700: "1200-2300",
+    5200: "1300-2600",
+    5900: "1500-2900",
+    6400: "1600-3200",
+    7200: "1800-3600",
+    7900: "2000-3900",
+    8000: "2000-4000",
+    12000: "3000-6000",
+    16000: "4000-8000",
+    24000: "6000-12000",
+    32000: "8000-16000",
+}
+
+dealer_point_dict = {12000: "満貫", 18000: "跳満", 24000: "倍満", 36000: "三倍満", 48000: "役満"}
+no_dealer_point_dict = {8000: "満貫", 12000: "跳満", 16000: "倍満", 24000: "三倍満", 32000: "役満"}
+
+
+def fan_fu(who, fans: List[int], fu: int, ten) -> str:
+    """
+    >>> fan_fu(0, [3, 1], 40, 12000)
+    '満貫'
+    >>> fan_fu(1, [2, 1], 40, 5200)
+    '40符3飜'
+    """
+    fan = sum(fans)
+    if who == mjproto.ABSOLUTE_POS_INIT_EAST:
+        if ten < 12000:
+            return str(fu) + "符" + str(fan) + "飜"
+        else:
+            return dealer_point_dict[ten]
+    else:
+        if int(ten) < 8000:
+            return str(fu) + "符" + str(fan) + "飜"
+        else:
+            return no_dealer_point_dict[ten]
+
+
+def winner_point(who: int, from_who: int, fans: List[int], fu: int, ten: int) -> str:
+    """
+    >>> winner_point(0, 0, [3, 0], 30, 6000)
+    '30符3飜2000点∀'
+    >>> winner_point(2, 0, [5, 0], 40, 8000)
+    '満貫8000点'
+    """
+    is_tsumo = who == from_who  # ツモあがりかどうかを判定
+    if is_tsumo:
+        if who == mjproto.ABSOLUTE_POS_INIT_EAST:  # 親かどうか
+            return fan_fu(who, fans, fu, ten) + str(int(ten / 3)) + "点∀"
+        else:
+            return fan_fu(who, fans, fu, ten) + non_dealer_tsumo_dict[ten] + "点"
+    else:
+        if who == mjproto.ABSOLUTE_POS_INIT_EAST:
+            return fan_fu(who, fans, fu, ten) + str(ten) + "点"
+        else:
+            return fan_fu(who, fans, fu, ten) + str(ten) + "点"
+
+
+def check_uradoras(fans: List[int], yakus: List[int]) -> List[int]:  # リーチがかかるとprotoではyakus
+    # に強制的にウラドラの情報が入るが、乗っているかどうかを確認する必要がある
+    """
+    >>> check_uradoras([1, 1, 1, 0], [1, 0, 7, 53])
+    [1, 0, 7]
+    """
+    if sum(fans) < len(yakus):
+        return [i for i in yakus if i != 53]
+    else:
+        return yakus
+
+
+def winner_yakus(yakus: List[int]) -> List[str]:
+    """
+    >>> winner_yakus([0, 1, 23])
+    ['門前清自摸和(1飜)', '立直(1飜)', '混全帯幺九(2飜)']
+    >>> winner_yakus([23])
+    ['混全帯幺九(1飜)']
+    """
+    if 0 in yakus:  # ツモの有無によって役の飜数がかわる。
+        return list(map(lambda x: yaku_dict_tumo[x], yakus))
+    else:
+        return list(map(lambda x: yaku_dict_ron[x], yakus))
+
+
+def parse_terminal(state: mjproto.State):
+    if len(state.terminal.wins) == 0:  # あがった人がいない場合,# state.terminal.winsの長さは0
+        ten_changes = [i for i in state.terminal.no_winner.ten_changes]
+        return ["流局", ten_changes]
+    else:
+        who = state.terminal.wins[0].who
+        from_who = state.terminal.wins[0].from_who
+        ten_changes = [i for i in state.terminal.wins[0].ten_changes]
+        fans = [i for i in state.terminal.wins[0].fans]  # [役での飜数, ドラの数]
+        yakus = check_uradoras(fans, [i for i in state.terminal.wins[0].yakus])
+        fu = state.terminal.wins[0].fu
+        ten = state.terminal.wins[0].ten
+        """
+        情報
+        fnas:[役での飜数, ドラでの飜数]
+        yakus: [役とドラの種類]
+        ten: 純粋に上がり点が表示される。ツモ上がりの際の対応が必要
+        """
+        yaku_point_infos = [who, from_who, who, winner_point(who, from_who, fans, fu, ten)]
+        yaku_point_infos.extend(winner_yakus(yakus))
+        return [
+            "和了",
+            ten_changes,
+            yaku_point_infos,
+        ]
+
+
+def determine_ura_doras_list(state: mjproto.State) -> List:
+    if len(state.terminal.wins) == 0:  # あがり者の有無でウラどらが表示されるかどうかが決まる
+        return []
+    else:
+        return [change_tile_fmt(i) for i in state.ura_doras]
 
 
 # ここを実装
 def mjproto_to_mjscore(state: mjproto.State) -> str:
-    # print(state.init_score.round)
-    # print(state.init_score.riichi)
-    # print(state.init_score.honba)
-    # print(state.init_score.ten)
-    d: Dict = {}
+    round: int = state.init_score.round
+    honba: int = state.init_score.honba
+    riichi: int = state.init_score.riichi
+    doras: List[int] = [change_tile_fmt(i) for i in state.doras]
+    ura_doras = determine_ura_doras_list(state)
+    init_score: List[int] = [i for i in state.init_score.ten]
+    log = [[round, honba, riichi], init_score, doras, ura_doras]
+    absolute_pos = [
+        mjproto.ABSOLUTE_POS_INIT_EAST,
+        mjproto.ABSOLUTE_POS_INIT_SOUTH,
+        mjproto.ABSOLUTE_POS_INIT_WEST,
+        mjproto.ABSOLUTE_POS_INIT_NORTH,
+    ]
+    for abs_pos in absolute_pos:
+        log.append(sort_init_hand(change_tiles_fmt(state.private_infos[abs_pos].init_hand)))
+        log.append(
+            parse_draws(state.private_infos[abs_pos].draws, state.event_history.events, abs_pos)
+        )
+        log.append(parse_discards(state.event_history.events, abs_pos))
+
+    log.append(parse_terminal(state))
+    d: Dict = {"title": [], "name": [], "rule": [], "log": [log]}
     return json.dumps(d)
 
 
@@ -36,7 +401,4 @@ if __name__ == "__main__":
     mjscore_str = mjproto_to_mjscore(state)
     mjscore_dict = json.loads(mjscore_str)
 
-    # 比較
-    print(mjscore_expected_dict)
-    print(mjscore_dict)
-    print(mjscore_expected_dict == mjscore_dict)
+    print(mjscore_expected_dict["log"] == mjscore_dict["log"])
