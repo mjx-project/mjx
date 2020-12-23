99ec6b80b2c5756dc4c6fcd9ec7a78274df4d305
Nishimori Soichiro <55940613+nissymori@users.noreply.github.com>
2020-12-23 16:30:38 +0900

    mjprotoからmjscoreへの変換テストケースを追加する3 (#603)
    * feat: create correspond yakus in order to return doras together
    
    * refactor: change determin_ura_doras_list
    
    * refactor: refactor the test logic the previous case was passed
    
    * refactor:refactor parse_discards to deal with tumogiri_riichi
    
    * refactor: refactor check_uradoras , correspond_yakus, winner_yakus to deal with dora representation
    
    * refactor: rename the fanctions to emphasize the hierarchy
    
    * feat: create transform_red_stolen and transform_red_open
    
    * Add files via upload
    
    * Add files via upload
    
    * add test for has_red is_red_stolen
    
    * feat: create transform_red_stolen transform_red_open
    
    * refactor transform_red_open
    
    * refactor : incorporated transform_red_stolen and transform_red_open to open_stolen_tile_type and open_tile_types
    
    * Rename test12010102910gm-00a9-0000-cdb9804c.json to 2010102910gm-00a9-0000-cdb9804c.json
    
    * Rename test12010102910gm-00a9-0000-cdb9804c.mjlog to 2010102910gm-00a9-0000-cdb9804c.mjlog
    
    * refactor: refactor to pass checks
    
    * refactor: refactor doctest
    
    * refactor: refactor doctest
    
    * refactor: reformat mjconvert/mjscore_encoder.py
    
    * feat: create function is_oya to determin if winner is oya
    
    * refactor: refactor __change_action_fmt typo
    
    * feat: add test for kan_closed and fix to pass test
    
    * feat: change _change_action_format to respond kan_closed but not suceed
    
    * change_func_nm
    
    * Add files via upload
    
    * Add files via upload
    
    * refactor refactor parse_discards
    
    * remove dupulication
    
    * reformat file
    
    * refactor: refactor transform_red_orpen
    
    * refactor _change_action_format for kan_added
    
    * refactor: fix is_stolen_red
    
    * reformat
    
    * reformat
    
    * Add files via upload
    
    * Add files via upload
    
    * refactor open_tile_types, _change_action_format
    
    Co-authored-by: Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>

--- mjconvert/mjconvert/mjscore_encoder.py
+++ mjconvert/mjconvert/mjscore_encoder.py
@@ -40,6 +40,10 @@ def _change_action_format(bits: int) -> str:  # TODO カン
             return str(stolen_tile) + "p" + str(open_tiles[0]) + str(open_tiles[1])
         else:
             return str(stolen_tile) + str(open_tiles[0]) + "p" + str(open_tiles[1])
+    elif event_type == mjproto.EVENT_TYPE_KAN_ADDED:
+        return (
+            "k" + str(stolen_tile) + str(open_tiles[0]) + str(open_tiles[1]) + str(open_tiles[2])
+        )
     elif event_type == mjproto.EVENT_TYPE_KAN_CLOSED:
         return str(stolen_tile) + str(stolen_tile) + str(stolen_tile) + "a" + str(stolen_tile)
     else:
@@ -86,6 +90,8 @@ def parse_discards(events, abs_pos: int):
                 riichi_tile_list.append(_change_tile_fmt(events[i + 1].tile))
             # riichiの次のeventに宣言牌が記録されているのでそのtileを取得して後にindexを取得変更
         elif event.type == mjproto.EVENT_TYPE_KAN_CLOSED and event.who == abs_pos:
+            discards.append(_change_action_format(event.open))
+        elif event.type == mjproto.EVENT_TYPE_KAN_ADDED and event.who == abs_pos:
             discards.append(_change_action_format(event.open))
     if is_reach:
         riichi_tile = riichi_tile_list[0]
@@ -272,15 +278,15 @@ dealer_point_dict = {12000: "満貫", 18000: "跳満", 24000: "倍満", 36000: "
 no_dealer_point_dict = {8000: "満貫", 12000: "跳満", 16000: "倍満", 24000: "三倍満", 32000: "役満"}
 
 
-def _fan_fu(who, fans: List[int], fu: int, ten) -> str:
+def _fan_fu(who, fans: List[int], fu: int, ten, round: int) -> str:
     """
-    >>> _fan_fu(0, [3, 1], 40, 12000)
+    >>> _fan_fu(0, [3, 1], 40, 12000, 0)
     '満貫'
-    >>> _fan_fu(1, [2, 1], 40, 5200)
+    >>> _fan_fu(1, [2, 1], 40, 5200, 0)
     '40符3飜'
     """
     fan = sum(fans)
-    if who == mjproto.ABSOLUTE_POS_INIT_EAST:
+    if _is_oya(who, round):  # 親かどうかを判定
         if ten < 12000:
             return str(fu) + "符" + str(fan) + "飜"
         else:
@@ -315,14 +321,14 @@ def _winner_point(who: int, from_who: int, fans: List[int], fu: int, ten: int, r
     is_tsumo = who == from_who  # ツモあがりかどうかを判定
     if is_tsumo:
         if _is_oya(who, round):  # 親かどうか
-            return _fan_fu(who, fans, fu, ten) + str(int(ten / 3)) + "点∀"
+            return _fan_fu(who, fans, fu, ten, round) + str(int(ten / 3)) + "点∀"
         else:
-            return _fan_fu(who, fans, fu, ten) + non_dealer_tsumo_dict[ten] + "点"
+            return _fan_fu(who, fans, fu, ten, round) + non_dealer_tsumo_dict[ten] + "点"
     else:
         if who == mjproto.ABSOLUTE_POS_INIT_EAST:
-            return _fan_fu(who, fans, fu, ten) + str(ten) + "点"
+            return _fan_fu(who, fans, fu, ten, round) + str(ten) + "点"
         else:
-            return _fan_fu(who, fans, fu, ten) + str(ten) + "点"
+            return _fan_fu(who, fans, fu, ten, round) + str(ten) + "点"
 
 
 def _check_uradoras(fans: List[int], yakus: List[int]) -> List[int]:  # リーチがかかるとprotoではyakus
