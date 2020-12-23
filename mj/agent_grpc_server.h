50cffd35c8341d477df6f7e264140d9ec0f42295
Nao Goto <n0n0a6262@gmail.com>
2020-12-24 04:11:41 +0900

    Agentサーバーのバッチ推論部分をまとめた親クラスをつくる (#602)
    * initialize
    
    * initialize
    
    * feat: super class
    
    * Revert "feat: super class"
    
    This reverts commit a9838b3ab3ab2deb7ec821abfacffa99a047b524.
    
    * re-add: super class
    
    * fix: make final => override
    
    * feat: add strategy super class
    
    * refactor: remove strategy dependency inside the impl class
    
    * use AgentGrpcServerImplRuleBased as AgentGrpcServerImpl
    
    * feat: implement RunServer in AgentGrpcServerImpl
    
    * refactor: remove unused class
    
    * refactor: make AgentGrpcServer class static w/ RunServer method
    
    * refactor: make InferenceAction private
    
    * rename
    
    Co-authored-by: Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>

--- mj/agent_grpc_server.h
+++ mj/agent_grpc_server.h
@@ -1,30 +1,54 @@
 #ifndef MAHJONG_AGENT_GRPC_SERVER_H
 #define MAHJONG_AGENT_GRPC_SERVER_H
 
-#include <grpcpp/grpcpp.h>
-#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <queue>
+#include <thread>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/functional/hash.hpp>
 #include "mj.grpc.pb.h"
+#include "strategy_rule_based.h"
+#include "observation.h"
 
 namespace mj
 {
     class AgentGrpcServer
     {
     public:
-        AgentGrpcServer() = default;  // invalid constructor
-        explicit AgentGrpcServer(std::unique_ptr<grpc::Service> agent_impl): agent_impl_(std::move(agent_impl)) {}
-        void RunServer(const std::string &socket_address) {
-            std::cout << socket_address << std::endl;
-            grpc::EnableDefaultHealthCheckService(true);
-            grpc::reflection::InitProtoReflectionServerBuilderPlugin();
-            grpc::ServerBuilder builder;
-            builder.AddListeningPort(socket_address, grpc::InsecureServerCredentials());
-            builder.RegisterService(agent_impl_.get());
-            std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-            std::cout << "Agent server listening on " << socket_address << std::endl;
-            server->Wait();
-        }
+        static void RunServer(std::unique_ptr<Strategy> strategy, const std::string &socket_address,
+                              int batch_size = 8, int wait_ms = 0);
+    };
+
+    class AgentGrpcServerImpl final : public mjproto::Agent::Service
+    {
+    public:
+        explicit AgentGrpcServerImpl(std::unique_ptr<Strategy> strategy, int batch_size = 8, int wait_ms = 0);
+        ~AgentGrpcServerImpl() final;
+        grpc::Status TakeAction(grpc::ServerContext* context, const mjproto::Observation* request, mjproto::Action* reply) final ;
     private:
-        std::unique_ptr<grpc::Service> agent_impl_;
+        struct ObservationInfo{
+            boost::uuids::uuid id;
+            Observation obs;
+        };
+
+        void InferAction();
+
+        // Agent logic
+        std::unique_ptr<Strategy> strategy_;
+
+        // 推論を始めるデータ数の閾値
+        int batch_size_;
+        // 推論を始める時間間隔
+        int wait_ms_;
+
+        std::mutex mtx_que_, mtx_map_;
+        std::queue<ObservationInfo> obs_que_;
+        std::unordered_map<boost::uuids::uuid, mjproto::Action, boost::hash<boost::uuids::uuid>> act_map_;
+        // 常駐する推論スレッド
+        std::thread thread_inference_;
+        bool stop_flag_ = false;
+
     };
 }  // namespace mj
 
