a46143ce95c0dbdc0d17f1440110155718da9037
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-02-28 11:28:27 +0900

    rename mjproto.mjx_pb2 => mjxproto.mjx_pb2

--- mjconvert/mjproto/__init__.py
+++ mjconvert/mjproto/__init__.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent))
 
-from mjproto.mjx_pb2_grpc import *
-from mjproto.mjx_pb2 import *
+from mjxproto.mjx_pb2_grpc import *
+from mjxproto.mjx_pb2 import *
