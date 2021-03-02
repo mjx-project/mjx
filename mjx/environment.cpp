e423111268e71db2c056eed93fba76ae189f2c48
Nao Goto <n0n0a6262@gmail.com>
2021-03-03 07:48:33 +0900

    ParallelRunGameで指定されたゲーム数をすべて行うように改善する (#640)
    * initialize
    
    * fix: assign the remainder games
    
    * add: example comment

--- mjx/environment.cpp
+++ mjx/environment.cpp
@@ -18,16 +18,29 @@ namespace mjx
         auto gen = mjx::GameSeed::CreateRandomGameSeedGenerator();
         auto results = std::vector<GameResult>();
         std::mutex results_mtx;
-        // スレッド生成
-        for(int i = 0; i < num_thread; i++){
+        // スレッド生成 (thread-safe)
+        // 100ゲーム16スレッドの場合、
+        // 前半-> (16-100%16)×(100/16)=12×6=72 game を割り当て
+        // 後半-> (100%16)×(100/16+1)=4×7=28 game を割り当て
+        // 前半+後半=100 game
+        for(int i = 0; i < num_thread-num_game%num_thread; i++){
             // TODO: シード生成を外部で行う（現在: 内部でGameSeed::CreateRandomGameSeedGeneratorにより生成）
             threads.emplace_back(std::thread([&]{
                 Environment env(agents);
-                for(int j = 0; j < num_game/num_thread; j++){
+                for(int j = 0; j < num_game/num_thread ; j++){
                     auto result = env.RunOneGame(gen());
                     std::lock_guard<std::mutex> lock(results_mtx);
                     results.emplace_back(result);
-
+                }
+            }));
+        }
+        for(int i = 0; i < num_game%num_thread; i++){
+            threads.emplace_back(std::thread([&]{
+                Environment env(agents);
+                for(int j = 0; j < num_game/num_thread + 1 ; j++){
+                    auto result = env.RunOneGame(gen());
+                    std::lock_guard<std::mutex> lock(results_mtx);
+                    results.emplace_back(result);
                 }
             }));
         }
