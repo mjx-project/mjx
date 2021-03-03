e810e280a64ecd5dcf77c5ad550f10fead4f84da
n0n0a <n0n0a6262@gmail.com>
2021-03-03 12:25:15 +0900

    add: nonbatch grpc

--- scripts/speed_benchmark.cpp
+++ scripts/speed_benchmark.cpp
@@ -3,14 +3,21 @@
 #include <mjx/mjx.h>
 #include "mjx/agent_grpc_client.h"
 #include "mjx/agent_batch_grpc_server.h"
+#include "mjx/agent_grpc_server.h"
 #include "mjx/strategy_rule_based.h"
 
 using namespace mjx;
 
-// grpc無:
-// ./speed_benchmark.out #game #thread
-// grpc有:
-// ./speed_benchmark.out {#batch_size #wait_ms host | #game #thread client}
+// command help
+// ./speed_benchmark  {-server [-B #batch_size #wait_ms] |-client #game #thread| #game #thread}
+
+// example)
+// grpc無,　バッチ無, 128ゲーム, 16スレッド:
+// ./speed_benchmark  128 16
+// grpc有, クライアント側, 256ゲーム, 32スレッド:
+// ./speed_benchmark -client 256 32
+// grpc有, バッチ有,　サーバー側, バッチサイズ16, 待機時間10ms:
+// ./speed_benchmark -server -B 16 10
 
 bool cmdOptionExists(char** begin, char** end, const std::string& option)
 {
@@ -19,21 +26,30 @@ bool cmdOptionExists(char** begin, char** end, const std::string& option)
 
 int main(int argc, char* argv[]) {
     std::cout << "cnt_args: " <<  argc << std::endl;
-    if(cmdOptionExists(argv, argv+argc, "host")){
-        AgentBatchGrpcServer::RunServer(
-                std::make_unique<StrategyRuleBased>(),"0.0.0.0:50051", std::atoi(argv[1]), std::atoi(argv[2])
-        );
+    if(cmdOptionExists(argv, argv+argc, "-server")){
+        if(cmdOptionExists(argv, argv+argc, "-B")){
+            AgentBatchGrpcServer::RunServer(
+                    std::make_unique<StrategyRuleBased>(),"0.0.0.0:50051", std::atoi(argv[3]), std::atoi(argv[4])
+            );
+        }
+        else{
+            AgentGrpcServer::RunServer(
+                    std::make_unique<StrategyRuleBased>(),"0.0.0.0:50051"
+            );
+        }
     }
     else{
         std::vector<std::shared_ptr<Agent>> agents;
-        if(cmdOptionExists(argv, argv+argc, "client")){
+        int num_game, num_thread;
+        if(cmdOptionExists(argv, argv+argc, "-client")){
             auto channel_rulebased = grpc::CreateChannel("localhost:50051",grpc::InsecureChannelCredentials());
             agents = {
                     std::make_shared<AgentGrpcClient>("rule-based-0", channel_rulebased),
                     std::make_shared<AgentGrpcClient>("rule-based-1", channel_rulebased),
                     std::make_shared<AgentGrpcClient>("rule-based-2", channel_rulebased),
-                    std::make_shared<AgentGrpcClient>("rule-based-3", channel_rulebased),
-                    };
+                    std::make_shared<AgentGrpcClient>("rule-based-3", channel_rulebased)
+            };
+            num_game = std::atoi(argv[2]), num_thread = std::atoi(argv[3]);
         }
         else {
             agents = {
@@ -42,9 +58,10 @@ int main(int argc, char* argv[]) {
                     std::make_shared<AgentExampleRuleBased>("rule-based-2"),
                     std::make_shared<AgentExampleRuleBased>("rule-based-3")
             };
+            num_game = std::atoi(argv[1]), num_thread = std::atoi(argv[2]);
         }
         auto start = std::chrono::system_clock::now();
-        auto results = Environment::ParallelRunGame(std::atoi(argv[1]), std::atoi(argv[2]), agents);
+        auto results = Environment::ParallelRunGame(num_game, num_thread, agents);
         auto end = std::chrono::system_clock::now();
         auto &summarizer = GameResultSummarizer::instance();
         summarizer.Initialize();
@@ -52,11 +69,11 @@ int main(int argc, char* argv[]) {
             summarizer.Add(std::move(result));
         }
         std::cout << summarizer.string() << std::endl;
-        std::cout << "# games: " << std::atoi(argv[1]) << std::endl;
-        std::cout << "# threads: " << std::atoi(argv[2]) << std::endl;
+        std::cout << "# games: " << num_game << std::endl;
+        std::cout << "# threads: " << num_thread << std::endl;
         auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
         std::cout << "total [sec]: " << ms / 1000.0 << std::endl;
-        std::cout << "sec/game: " << ms / 1000.0 / std::atoi(argv[1]) << std::endl;
+        std::cout << "sec/game: " << ms / 1000.0 / num_game << std::endl;
     }
     return 0;
 }
