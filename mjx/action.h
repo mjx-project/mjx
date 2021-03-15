2252897b83275bb7e85250deae469401f4719b7c
keigo habara <34413567+habara-k@users.noreply.github.com>
2021-03-15 17:18:06 +0900

    Actionを1つの整数値に対応させる (#661)
    * Add Action::Encode
    
    * fix
    
    * Add Action::Decode
    
    * Add test
    
    * Add test KanOpened and KanAdded
    
    * add test case
    
    * fix

--- mjx/action.h
+++ mjx/action.h
@@ -24,6 +24,22 @@ class Action {
   static mjxproto::Action CreateNo(AbsolutePos who);
   static mjxproto::Action CreateNineTiles(AbsolutePos who);
   static bool Equal(const mjxproto::Action& lhs, const mjxproto::Action& rhs);
+  static std::uint8_t Encode(const mjxproto::Action &action);
+  static mjxproto::Action Decode(std::uint8_t code, const std::vector<mjxproto::Action>& possible_action);
+  // 0~33: Discard m1~rd
+  // 34,35,36: Discard m5(red), p5(red), s5(red)
+  // 37~57: Chi m1m2m3 ~ s7s8s9
+  // 58,59,60: Chi m3m4m5(red), m4m5(red)m6, m5(red)m6m7
+  // 61,62,63: Chi p3p4p5(red), p4p5(red)p6, p5(red)p6p7
+  // 64,65,66: Chi s3s4s5(red), s4s5(red)s6, s5(red)s6s7
+  // 67~100: Pon m1~rd
+  // 101,102,103: Pon m5(w/ red), s5(w/ red), p5(w/ red)
+  // 104~137: Kan m1~rd
+  // 138: Tsumo
+  // 139: Ron
+  // 140: Riichi
+  // 141: Kyuushu
+  // 142: No
 };
 }  // namespace mjx
 
---
+++ tests/action_test.cpp
@@ -0,0 +1,46 @@
+#include <mjx/action.h>
+
+#include "gtest/gtest.h"
+
+using namespace mjx;
+
+TEST(action, Encode) {
+  // Discard
+  EXPECT_EQ(Action::Encode(Action::CreateDiscard(AbsolutePos::kInitEast, Tile(17))), 4);
+  // Discard red
+  EXPECT_EQ(Action::Encode(Action::CreateDiscard(AbsolutePos::kInitEast, Tile(16))), 34);
+  // Chi
+  auto tiles = std::vector<Tile>{Tile(12), Tile(17), Tile(20)};
+  EXPECT_EQ(Action::Encode(Action::CreateOpen(AbsolutePos::kInitEast,
+    Chi::Create(tiles, Tile(12)))), 40);
+  tiles = std::vector<Tile>{Tile(48), Tile(53), Tile(56)};
+  EXPECT_EQ(Action::Encode(Action::CreateOpen(AbsolutePos::kInitEast,
+                                              Chi::Create(tiles, Tile(48)))), 47);
+  // Chi w/ red
+  tiles = std::vector<Tile>{Tile(12), Tile(16), Tile(20)};
+  EXPECT_EQ(Action::Encode(Action::CreateOpen(AbsolutePos::kInitEast,
+                                              Chi::Create(tiles, Tile(12)))), 59);
+  // Pon
+  EXPECT_EQ(Action::Encode(Action::CreateOpen(AbsolutePos::kInitEast,
+                                              Pon::Create(Tile(17), Tile(16), RelativePos::kLeft))), 71);
+  // Pon w/ red
+  EXPECT_EQ(Action::Encode(Action::CreateOpen(AbsolutePos::kInitEast,
+                                              Pon::Create(Tile(16), Tile(17), RelativePos::kLeft))), 101);
+  // Kan
+  EXPECT_EQ(Action::Encode(Action::CreateOpen(AbsolutePos::kInitEast,
+                                              KanClosed::Create(Tile(16)))), 108);
+  EXPECT_EQ(Action::Encode(Action::CreateOpen(AbsolutePos::kInitEast,
+                                              KanOpened::Create(Tile(16), RelativePos::kLeft))), 108);
+  EXPECT_EQ(Action::Encode(Action::CreateOpen(AbsolutePos::kInitEast,
+                                              KanAdded::Create(Pon::Create(Tile(17), Tile(16), RelativePos::kLeft)))), 108);
+  // Tsumo
+  EXPECT_EQ(Action::Encode(Action::CreateTsumo(AbsolutePos::kInitEast)), 138);
+  // Ron
+  EXPECT_EQ(Action::Encode(Action::CreateRon(AbsolutePos::kInitEast)), 139);
+  // Riichi
+  EXPECT_EQ(Action::Encode(Action::CreateRiichi(AbsolutePos::kInitEast)), 140);
+  // Kyusyu
+  EXPECT_EQ(Action::Encode(Action::CreateNineTiles(AbsolutePos::kInitEast)), 141);
+  // No
+  EXPECT_EQ(Action::Encode(Action::CreateNo(AbsolutePos::kInitEast)), 142);
+}
