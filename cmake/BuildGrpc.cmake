38b9dd6f2b56332ef62930ad37b4f7cb1bbff084
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-03-27 19:53:32 +0900

    use fetch content to install grpc

---
+++ cmake/BuildGrpc.cmake
@@ -0,0 +1,26 @@
+include(FetchContent)
+
+set(CMAKE_BUILD_TYPE Release)
+set(gRPC_BUILD_TESTS OFF)
+# set(gRPC_SSL_PROVIDER package)
+# set(GRPC_FETCHCONTENT ON)
+
+FetchContent_Declare(
+        protobuf
+        GIT_REPOSITORY https://github.com/protocolbuffers/protobuf.git
+        GIT_TAG        v3.13.0
+)
+
+FetchContent_MakeAvailable(protobuf)
+
+find_package(Protobuf CONFIG REQUIRED)
+
+FetchContent_Declare(
+        gRPC
+        GIT_REPOSITORY https://github.com/grpc/grpc
+        GIT_TAG        v1.28.0
+)
+
+set(FETCHCONTENT_QUIET OFF)
+
+FetchContent_MakeAvailable(gRPC)
