dd0b84dbd1cfc54f65b3af4a242777184aa6b767
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-03-31 21:28:20 +0900

    download entire boost

---
+++ cmake/boost.cmake
@@ -0,0 +1,15 @@
+include(FetchContent)
+set(FETCHCONTENT_BASE_DIR ${EXTERNALDIR})
+set(FETCHCONTENT_UPDATES_DISCONNECTED ON)
+
+FetchContent_Declare(
+    boost
+    GIT_REPOSITORY https://github.com/boostorg/boost.git
+    GIT_TAG        boost-1.75.0
+)
+
+FetchContent_GetProperties(boost)
+if(NOT boost_POPULATED)
+    FetchContent_Populate(boost)
+    include_directories(${boost_SOURCE_DIR}/include)
+endif()
