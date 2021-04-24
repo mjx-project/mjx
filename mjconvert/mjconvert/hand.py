1a6b090ca491a416f36285dffbd6eb61348a32d8
n0n0a <n0n0a6262@gmail.com>
2021-04-24 14:47:48 +0900

    refactor

--- mjconvert/mjconvert/hand.py
+++ mjconvert/mjconvert/hand.py
@@ -5,17 +5,17 @@ from mjconvert.open_tile_ids_converter import open_additional_tile_ids, open_rem
 
 class Hand:
     def __init__(self, closed_tiles: List[int] = [], open_tiles: List[int] = []):
-        self.closed_tiles_: List[int] = closed_tiles  # each item is tile id (0 ~ 135)
-        self.opens_: List[int] = open_tiles
-        self.closed_tiles_.sort()
-        self.opens_.sort()
+        self.closed_tiles: List[int] = closed_tiles  # each item is tile id (0 ~ 135)
+        self.open_tiles: List[int] = open_tiles
+        self.closed_tiles.sort()
+        self.open_tiles.sort()
 
     def draw(self, tile_id: int):
-        self.closed_tiles_.append(tile_id)
-        self.closed_tiles_.sort()
+        self.closed_tiles.append(tile_id)
+        self.closed_tiles.sort()
 
     def discard(self, tile_id: int):
-        self.closed_tiles_.remove(tile_id)
+        self.closed_tiles.remove(tile_id)
 
     def apply_open(self, open: int):
         """
@@ -26,29 +26,29 @@ class Hand:
         added_kan
         >>> hand = Hand([2, 4, 6, 5, 11, 10, 8, 9, 86, 90, 125, 126], [0, 3, 1])
         >>> hand.apply_open(49495)
-        >>> hand.closed_tiles_
+        >>> hand.closed_tiles
         [2, 4, 5, 6, 8, 9, 10, 11, 125, 126]
-        >>> hand.opens_
+        >>> hand.open_tiles
         [0, 1, 3, 82, 86, 90]
         >>> hand.apply_open(47723)
-        >>> hand.closed_tiles_
+        >>> hand.closed_tiles
         [2, 4, 5, 6, 8, 9, 10, 11]
-        >>> hand.opens_
+        >>> hand.open_tiles
         [0, 1, 3, 82, 86, 90, 124, 125, 126]
         >>> hand.apply_open(1793)
-        >>> hand.closed_tiles_
+        >>> hand.closed_tiles
         [2, 8, 9, 10, 11]
-        >>> hand.opens_
+        >>> hand.open_tiles
         [0, 1, 3, 4, 5, 6, 7, 82, 86, 90, 124, 125, 126]
         >>> hand.apply_open(2048)
-        >>> hand.closed_tiles_
+        >>> hand.closed_tiles
         [2]
-        >>> hand.opens_
+        >>> hand.open_tiles
         [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 82, 86, 90, 124, 125, 126]
         >>> hand.apply_open(530)
-        >>> hand.closed_tiles_
+        >>> hand.closed_tiles
         []
-        >>> hand.opens_
+        >>> hand.open_tiles
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 82, 86, 90, 124, 125, 126]
         """
         # convert into t  ile_ids
@@ -57,9 +57,9 @@ class Hand:
         # move the tiles from close to open
         # remove from close
         for id in removable_ids:
-            self.closed_tiles_.remove(id)
+            self.closed_tiles.remove(id)
         # add to open
         for id in addtional_ids:
-            self.opens_.append(id)
+            self.open_tiles.append(id)
 
-        self.opens_.sort()
+        self.open_tiles.sort()
--- mjconvert/mjconvert/open_tile_ids_converter.py
+++ mjconvert/mjconvert/open_tile_ids_converter.py
@@ -7,75 +7,91 @@ MASK_CHI_OFFSET = [0b0000000000011000, 0b0000000001100000, 0b0000000110000000]
 MASK_PON_UNUSED_OFFSET = 0b0000000001100000
 
 
-def open_additional_tile_ids(bits: int) -> List[int]:
+def open_tile_ids(bits: int) -> List[int]:
     """
-    chi,
-    pon,
-    open_kan,
-    closed_kan,
-    added_kan
-    >>> open_additional_tile_ids(49495)
+    各鳴きで使われた牌のidを返す関数
+    以下、各鳴きに対するdoctest
+    >>> open_tile_ids(49495)  # chi
     [82, 86, 90]
-    >>> open_additional_tile_ids(47723)
+    >>> open_tile_ids(47723)  # pon
     [124, 125, 126]
-    >>> open_additional_tile_ids(1793)
+    >>> open_tile_ids(1793)  # open_kan
     [4, 5, 6, 7]
-    >>> open_additional_tile_ids(2048)
+    >>> open_tile_ids(2048)  # closed_kan
     [8, 9, 10, 11]
-    >>> open_additional_tile_ids(530)
-    [2]
+    >>> open_tile_ids(530)  # added_kan
+    [0, 1, 2, 3]
     """
     event_type = open_event_type(bits)
     if event_type == mjxproto.EVENT_TYPE_CHI:
-        return chi_ids(bits)
+        return chi_ids_(bits)
     elif event_type == mjxproto.EVENT_TYPE_PON:
-        return pon_ids(bits)
+        return pon_ids_(bits)
     elif event_type == mjxproto.EVENT_TYPE_OPEN_KAN:
-        return oepn_kan_ids(bits)
+        return oepn_kan_ids_(bits)
     elif event_type == mjxproto.EVENT_TYPE_CLOSED_KAN:
-        return closed_kan_ids(bits)
+        return closed_kan_ids_(bits)
     else:
+        return added_kan_ids_(bits)
+
+
+def open_additional_tile_ids(bits: int) -> List[int]:
+    """
+    各鳴きに対してopensに加えるべき牌のidを返す関数
+    以下、各鳴きに対するdoctest
+    >>> open_additional_tile_ids(49495)  # chi
+    [82, 86, 90]
+    >>> open_additional_tile_ids(47723)  # pon
+    [124, 125, 126]
+    >>> open_additional_tile_ids(1793)  # open_kan
+    [4, 5, 6, 7]
+    >>> open_additional_tile_ids(2048)  # closed_kan
+    [8, 9, 10, 11]
+    >>> open_additional_tile_ids(530)  # added_kan
+    [2]
+    """
+    event_type = open_event_type(bits)
+    if event_type == mjxproto.EVENT_TYPE_ADDED_KAN:
         type = (bits >> 9) // 3
         stolen_ix = (bits >> 9) % 3
         unused_offset = (bits & MASK_PON_UNUSED_OFFSET) >> 5
         if stolen_ix >= unused_offset:
             stolen_ix = stolen_ix + 1
         return [type * 4 + stolen_ix]
+    else:
+        return open_tile_ids(bits)
 
 
 def open_removable_tile_ids(bits: int) -> List[int]:
     """
-    chi,
-    pon,
-    open_kan,
-    closed_kan,
-    added_kan
-    >>> open_removable_tile_ids(49495)
+    各鳴きに対してclosed_tilesから取り除くべき牌のidを返す関数
+    以下、各鳴きに対するdoctest
+    >>> open_removable_tile_ids(49495)  # chi
     [86, 90]
-    >>> open_removable_tile_ids(47723)
+    >>> open_removable_tile_ids(47723)  # pon
     [125, 126]
-    >>> open_removable_tile_ids(1793)
+    >>> open_removable_tile_ids(1793)  # open_kan
     [4, 5, 6]
-    >>> open_removable_tile_ids(2048)
+    >>> open_removable_tile_ids(2048)  # closed_kan
     [8, 9, 10, 11]
-    >>> open_removable_tile_ids(530)
+    >>> open_removable_tile_ids(530)  # added_kan
     [2]
     """
     event_type = open_event_type(bits)
     if event_type == mjxproto.EVENT_TYPE_CHI:
-        tiles = chi_ids(bits)
+        tiles = chi_ids_(bits)
         tiles.pop((bits >> 10) % 3)
         return tiles
     elif event_type == mjxproto.EVENT_TYPE_PON:
-        tiles = pon_ids(bits)
+        tiles = pon_ids_(bits)
         tiles.pop((bits >> 9) % 3)
         return tiles
     elif event_type == mjxproto.EVENT_TYPE_OPEN_KAN:
-        tiles = oepn_kan_ids(bits)
+        tiles = oepn_kan_ids_(bits)
         tiles.pop((bits >> 8) % 4)
         return tiles
     elif event_type == mjxproto.EVENT_TYPE_CLOSED_KAN:
-        return oepn_kan_ids(bits)
+        return oepn_kan_ids_(bits)
     else:
         type = (bits >> 9) // 3
         stolen_ix = (bits >> 9) % 3
@@ -85,13 +101,19 @@ def open_removable_tile_ids(bits: int) -> List[int]:
         return [type * 4 + stolen_ix]
 
 
-def chi_ids(bits: int) -> List[int]:
+def chi_ids_(bits: int) -> List[int]:
+    """
+    チーに使われた牌のidを返す関数
+    """
     min_type_base = (bits >> 10) // 3
     min_type = (min_type_base // 7) * 9 + min_type_base % 7
     return [(min_type + i) * 4 + ((bits & MASK_CHI_OFFSET[i]) >> (2 * i + 3)) for i in range(3)]
 
 
-def pon_ids(bits: int) -> List[int]:
+def pon_ids_(bits: int) -> List[int]:
+    """
+    ポンに使われた牌のidを返す関数
+    """
     ids = []
     for i in range(3):
         type = (bits >> 9) // 3
@@ -102,13 +124,22 @@ def pon_ids(bits: int) -> List[int]:
     return ids
 
 
-def oepn_kan_ids(bits: int) -> List[int]:
+def oepn_kan_ids_(bits: int) -> List[int]:
+    """
+    大明槓に使われた牌のidを返す関数
+    """
     return [((bits >> 8) // 4) * 4 + i for i in range(4)]
 
 
-def closed_kan_ids(bits: int) -> List[int]:
+def closed_kan_ids_(bits: int) -> List[int]:
+    """
+    暗槓に使われた牌のidを返す関数
+    """
     return [((bits >> 8) // 4) * 4 + i for i in range(4)]
 
 
-def added_kan_ids(bits: int) -> List[int]:
+def added_kan_ids_(bits: int) -> List[int]:
+    """
+    加槓に使われた牌のidを返す関数
+    """
     return [((bits >> 9) // 3) * 4 + i for i in range(4)]
