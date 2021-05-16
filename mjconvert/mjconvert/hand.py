f332169236b3598a59878050db1cc72b0a59c758
n0n0a <n0n0a6262@gmail.com>
2021-05-16 13:07:30 +0900

    fix: keep open_tiles sorted in hand.py

--- mjconvert/mjconvert/hand.py
+++ mjconvert/mjconvert/hand.py
@@ -8,6 +8,7 @@ class Hand:
         self.closed_tiles: List[int] = closed_tiles  # each item is tile id (0 ~ 135)
         self.open_tiles: List[int] = open_tiles
         self.closed_tiles.sort()
+        self.open_tiles.sort()
 
     def draw(self, tile_id: int):
         self.closed_tiles.append(tile_id)
@@ -24,27 +25,27 @@ class Hand:
         >>> hand.closed_tiles
         [2, 4, 5, 6, 8, 9, 10, 11, 125, 126]
         >>> hand.open_tiles
-        [0, 3, 1, 82, 86, 90]
+        [0, 1, 3, 82, 86, 90]
         >>> hand.apply_open(47723)  # pon
         >>> hand.closed_tiles
         [2, 4, 5, 6, 8, 9, 10, 11]
         >>> hand.open_tiles
-        [0, 3, 1, 82, 86, 90, 124, 125, 126]
+        [0, 1, 3, 82, 86, 90, 124, 125, 126]
         >>> hand.apply_open(1793)  # open_kan
         >>> hand.closed_tiles
         [2, 8, 9, 10, 11]
         >>> hand.open_tiles
-        [0, 3, 1, 82, 86, 90, 124, 125, 126, 4, 5, 6, 7]
+        [0, 1, 3, 4, 5, 6, 7, 82, 86, 90, 124, 125, 126]
         >>> hand.apply_open(2048)  # closed_kan
         >>> hand.closed_tiles
         [2]
         >>> hand.open_tiles
-        [0, 3, 1, 82, 86, 90, 124, 125, 126, 4, 5, 6, 7, 8, 9, 10, 11]
+        [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 82, 86, 90, 124, 125, 126]
         >>> hand.apply_open(530)  # added_kan
         >>> hand.closed_tiles
         []
         >>> hand.open_tiles
-        [0, 3, 1, 82, 86, 90, 124, 125, 126, 4, 5, 6, 7, 8, 9, 10, 11, 2]
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 82, 86, 90, 124, 125, 126]
         """
         # convert into t  ile_ids
         ids = open_tile_ids(open)
@@ -54,3 +55,5 @@ class Hand:
                 self.closed_tiles.remove(id)
             if not self.open_tiles.count(id):
                 self.open_tiles.append(id)
+
+        self.open_tiles.sort()
