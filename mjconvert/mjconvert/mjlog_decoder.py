a2214a3556d2e1a18c45754422130769fc864a00
n0n0a <n0n0a6262@gmail.com>
2021-05-16 12:43:55 +0900

    feat: curr_hand update in mjconvert

--- mjconvert/mjconvert/mjlog_decoder.py
+++ mjconvert/mjconvert/mjlog_decoder.py
@@ -13,6 +13,7 @@ from google.protobuf import json_format
 
 import mjxproto
 from mjconvert.const import AbsolutePos, RelativePos
+from mjconvert.hand import Hand
 
 
 class MjlogDecoder:
@@ -148,6 +149,9 @@ class MjlogDecoder:
         for i in range(4 * 12, 4 * 13):
             assert wall[i] in self.state.private_observations[(i + round_) % 4].init_hand
 
+        curr_hands = []
+        for i in range(4):
+            curr_hands.append(Hand([int(x) for x in val["hai" + str(i)].split(",")], []))
         event = None
         num_kan_dora = 0
         self.last_drawer = None
@@ -158,12 +162,14 @@ class MjlogDecoder:
                 who, draw = MjlogDecoder.parse_draw(key)
                 self.state.private_observations[int(who)].draw_history.append(draw)
                 event = MjlogDecoder.make_draw_event(who)
+                curr_hands[int(who)].draw(draw)
                 self.last_drawer, self.last_draw = who, draw
             elif key != "DORA" and key[0] in ["D", "E", "F", "G"]:  # discard
                 who, discard = MjlogDecoder.parse_discard(key)
                 event = MjlogDecoder.make_discard_event(
                     who, discard, self.last_drawer, self.last_draw
                 )
+                curr_hands[int(who)].discard(discard)
                 self.last_drawer, self.last_draw = None, None
             elif key == "N":  # open
                 who = int(val["who"])
@@ -173,6 +179,7 @@ class MjlogDecoder:
                     type=MjlogDecoder._open_type(open),
                     open=open,
                 )
+                curr_hands[int(who)].apply_open(open)
             elif key == "REACH":
                 who = int(val["who"])
                 if int(val["step"]) == 1:
@@ -244,6 +251,12 @@ class MjlogDecoder:
                 + self.state.terminal.final_score.riichi * 1000
                 == 100000
             )
+
+        for i in range(4):
+            for tile in curr_hands[i].closed_tiles:
+                self.state.private_observations[i].curr_hand.closed_tiles.append(tile)
+            for tile in curr_hands[i].open_tiles:
+                self.state.private_observations[i].curr_hand.opens.append(tile)
 
         yield copy.deepcopy(self.state)
 
