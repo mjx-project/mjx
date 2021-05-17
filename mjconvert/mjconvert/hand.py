d3bf2854d6b7c5c6ac1b0e784f52fb06ad706f19
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-05-17 09:33:18 +0900

    feat: add opens to Hand instead of open_tiles

--- mjconvert/mjconvert/hand.py
+++ mjconvert/mjconvert/hand.py
@@ -4,56 +4,42 @@ from mjconvert.open_tile_ids_converter import open_event_type, open_tile_ids
 
 
 class Hand:
-    def __init__(self, closed_tiles: List[int] = [], open_tiles: List[int] = []):
-        self.closed_tiles: List[int] = closed_tiles  # each item is tile id (0 ~ 135)
-        self.open_tiles: List[int] = open_tiles
+    def __init__(self, closed_tiles: List[int] = [], opens: List[int] = []):
+        self._closed_tiles: List[int] = closed_tiles  # each item is tile id (0 ~ 135)
+        self.opens: List[int] = opens  # e.g., 49495. Follow Tenhou format.
         self.closed_tiles.sort()
-        self.open_tiles.sort()
+
+    @property
+    def closed_tiles(self):
+        self._closed_tiles.sort()
+        return self._closed_tiles
 
     def draw(self, tile_id: int):
+        assert len(self.closed_tiles) in [1, 4, 7, 10, 13]
         self.closed_tiles.append(tile_id)
-        self.closed_tiles.sort()
 
     def discard(self, tile_id: int):
+        assert len(self.closed_tiles) in [2, 5, 8, 11, 14]
         self.closed_tiles.remove(tile_id)
 
     def apply_open(self, open: int):
         """
-        各鳴きをHandに適用し、closed_tilesとopen_tilesを変更する
-        >>> hand = Hand([2, 4, 6, 5, 11, 10, 8, 9, 86, 90, 125, 126], [0, 3, 1])
+        Update closed_tiles and opens by open action.
+
+        TODO: fix test cases
+
+        >>> hand = Hand([2, 4, 6, 5, 11, 10, 8, 9, 86, 90, 125, 126, 130], [])
         >>> hand.apply_open(49495)  # chi
         >>> hand.closed_tiles
-        [2, 4, 5, 6, 8, 9, 10, 11, 125, 126]
-        >>> hand.open_tiles
-        [0, 1, 3, 82, 86, 90]
-        >>> hand.apply_open(47723)  # pon
-        >>> hand.closed_tiles
-        [2, 4, 5, 6, 8, 9, 10, 11]
-        >>> hand.open_tiles
-        [0, 1, 3, 82, 86, 90, 124, 125, 126]
-        >>> hand.apply_open(1793)  # open_kan
-        >>> hand.closed_tiles
-        [2, 8, 9, 10, 11]
-        >>> hand.open_tiles
-        [0, 1, 3, 4, 5, 6, 7, 82, 86, 90, 124, 125, 126]
-        >>> hand.apply_open(2048)  # closed_kan
-        >>> hand.closed_tiles
-        [2]
-        >>> hand.open_tiles
-        [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 82, 86, 90, 124, 125, 126]
-        >>> hand.apply_open(530)  # added_kan
-        >>> hand.closed_tiles
-        []
-        >>> hand.open_tiles
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 82, 86, 90, 124, 125, 126]
+        [2, 4, 5, 6, 8, 9, 10, 11, 125, 126, 130]
+        >>> hand.opens
+        [49495]
         """
-        # convert into t  ile_ids
+        # convert into tile_ids
         ids = open_tile_ids(open)
-        # move the tiles from close to open
         for id in ids:
-            if self.closed_tiles.count(id):
-                self.closed_tiles.remove(id)
-            if not self.open_tiles.count(id):
-                self.open_tiles.append(id)
+            if id not in self.closed_tiles:
+                continue
+            self.closed_tiles.remove(id)
 
-        self.open_tiles.sort()
+        self.opens.append(open)
