cdaacadfadb2db5944d784b2c0d42f095b747695
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-05-29 13:36:56 +0900

    update dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,7 @@
 set -eu
 
 ver=${1}
-cp ../mjconvert/requirements.txt ./mjconvert_requirements.txt
+cp ../pymjx/requirements.txt ./mjx_requirements.txt
 docker build -t sotetsuk/ubuntu-gcc-grpc:${ver} -m 6g .
 docker build -t sotetsuk/ubuntu-gcc-grpc:latest -m 6g .
-rm ./mjconvert_requirements.txt
+rm ./mjx_requirements.txt
