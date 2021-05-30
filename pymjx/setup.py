4d8ead294249962e867b82e8e720ebfb21592130
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-05-30 13:46:24 +0900

    [py] Add `mjx convert` subcommand to python CLI (#792)
    * use click instead of argparse
    
    * fix pymjx install
    
    * fix make test-cli command
    
    * rm unnecesary comments

--- pymjx/setup.py
+++ pymjx/setup.py
@@ -1,20 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='mjx',
+    name="mjx",
     version="0.0.1",
-    description='mjx',
-    author='Sotetsu KOYAMADA',
-    author_email='koyamada-s@sys.i.kyoto-u.ac.jp',
-    license='MIT',
+    description="mjx",
+    author="Sotetsu KOYAMADA",
+    author_email="koyamada-s@sys.i.kyoto-u.ac.jp",
+    license="MIT",
     install_requires=[],
     packages=find_packages(),
     include_package_data=True,
-    entry_points={
-        'console_scripts': 'mjx = mjx.main:main'
-    },
+    entry_points={"console_scripts": "mjx = mjx.main:main"},
     classifiers=[
         "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License"
-    ]
+        "License :: OSI Approved :: MIT License",
+    ],
 )
