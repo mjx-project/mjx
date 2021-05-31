7a1da4a4bb11e50662334e7de8709e2b5c456424
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-05-31 12:02:14 +0900

    fix imports

--- pymjx/mjx/converter/open_tile_ids_converter.py
+++ pymjx/mjx/converter/open_tile_ids_converter.py
@@ -1,7 +1,7 @@
 from typing import List
 
 import mjxproto
-from mjx.open_converter import open_event_type
+from mjx.converter.open_converter import open_event_type
 
 MASK_CHI_OFFSET = [0b0000000000011000, 0b0000000001100000, 0b0000000110000000]
 MASK_PON_UNUSED_OFFSET = 0b0000000001100000
