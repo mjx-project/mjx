dea4e5c37de8867dfda0976bf5ab55d8d3f962e2
n0n0a <n0n0a6262@gmail.com>
2021-06-01 18:10:32 +0900

    feat: Env Class (in progress)

---
+++ mjx/env.cpp
@@ -0,0 +1,71 @@
+#include "env.h"
+
+namespace mjx{
+Env::Env()
+    : player_ids_(std::vector<internal::PlayerId>(4))
+{
+  for (int i = 0; i < 4; ++i){
+    // TODO: PlayerIds Argument
+    internal::PlayerId player_id = "RuleBased" + std::to_string(i);
+    player_ids_[i] = player_id;
+    map_idxs_[player_id] = i;
+  }
+  // TODO: GameSeed Argument?
+//  auto gen = internal::GameSeed::CreateRandomGameSeedGenerator();
+//  state_ = internal::State(internal::State::ScoreInfo{player_ids_, gen()});
+    state_ = internal::State();
+}
+
+void Env::reset() {
+  for(auto id: player_ids_){
+    rewards_[player_ids_] = 0;
+    dones_[player_ids_] = 0;
+  }
+  auto gen = internal::GameSeed::CreateRandomGameSeedGenerator();
+  state_ = internal::State(internal::State::ScoreInfo{player_ids_, gen()});
+  observations_ = state_.CreateObservations();
+  set_next_idx();
+}
+
+void Env::step(std::string json) {
+  mjxproto::Action action;
+  google::protobuf::util::JsonStringToMessage(json, &action);
+  assert(current_palyer_idx_ == action.who());
+  assert(observations_.count(player_ids_[current_palyer_idx_]) > 0);
+  actions_.emplace_back(action);
+  if(current_palyer_idx_ == player_num_){
+    assert(actions_.size() == observations_.size());
+    if(state_.IsRoundOver()){
+      if(state_.IsGameOver()){
+        for(auto [id, score]: state_.result().rankings){
+          rewards_[id] =
+        }
+      }
+    }
+    else{
+      state_.Update(std::move(actions_));
+      observations_ = state_.CreateObservations();
+      actions_.clear();
+    }
+  }
+  set_next_idx();
+}
+
+std::tuple<std::string, int, bool, std::string> Env::last(){
+  std::string json;
+  google::protobuf::util::MessageToJsonString(observations_[player_ids_[current_palyer_idx_]].proto(), &json);
+  return {json, rewards_[current_palyer_idx_], dones_[current_palyer_idx_], ""};
+}
+
+void Env::set_next_idx(){
+  while (observations_.count(player_ids_[++current_palyer_idx_]) == 0
+         && current_palyer_idx_ < player_num_){}
+  if(current_palyer_idx_ < player_num_) return;
+  current_palyer_idx_ = 0;
+  while (observations_.count(player_ids_[++current_palyer_idx_]) == 0
+         && current_palyer_idx_ < player_num_){}
+  if(current_palyer_idx_ < player_num_){
+    // Nobody can take action ?
+  }
+}
+}
---
+++ mjx/env.h
@@ -0,0 +1,31 @@
+#ifndef MJX_PROJECT_ENV_H
+#define MJX_PROJECT_ENV_H
+
+#include <utility>
+#include "internal/state.h"
+#include "internal/utils.h"
+
+namespace mjx{
+class Env {
+ public:
+  Env();
+  void reset();
+  void step(std::string action);
+  std::tuple<std::string, int, bool, std::string> last();
+
+ private:
+  void set_next_idx();
+
+  const int player_num_ = 4;
+  std::vector<internal::PlayerId> player_ids_;
+  std::unordered_map<internal::PlayerId, int> map_idxs_;
+  internal::State state_;
+  std::unordered_map<internal::PlayerId, internal::Observation> observations_;
+  std::vector<mjxproto::Action> actions_;
+  std::unordered_map<internal::PlayerId, int> rewards_;
+  std::unordered_map<internal::PlayerId, bool> dones_;
+  int current_palyer_idx_;
+};
+}
+
+#endif  // MJX_PROJECT_ENV_H
