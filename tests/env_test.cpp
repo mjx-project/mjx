4679d121c7cb4e1910c2ca8ebe4f6e421b1a9261
n0n0a <n0n0a6262@gmail.com>
2021-06-05 14:43:48 +0900

    fix: env_test

--- tests/env_test.cpp
+++ tests/env_test.cpp
@@ -7,31 +7,26 @@
 using namespace mjx::internal;
 
 TEST(env, play_one_game) {
-  const std::vector<std::shared_ptr<Agent>> agents = {
-      std::make_shared<AgentLocal>("agent01",
-                                   std::make_unique<StrategyRuleBased>()),
-      std::make_shared<AgentLocal>("agent02",
-                                   std::make_unique<StrategyRuleBased>()),
-      std::make_shared<AgentLocal>("agent03",
-                                   std::make_unique<StrategyRuleBased>()),
-      std::make_shared<AgentLocal>("agent04",
-                                   std::make_unique<StrategyRuleBased>())};
+  // so large number that the game ends
   int max_cycles = 10000;
   auto env = mjx::Env();
-  auto observations = env.reset();
+  std::unordered_map<PlayerId, std::string> observations;
   std::unordered_map<PlayerId, int> rewards;
   std::unordered_map<PlayerId, bool> dones;
   std::unordered_map<PlayerId, std::string> infos;
+
+  observations = env.reset();
+  StrategyRuleBased strategy;
   for(int i = 0; i < max_cycles; i++){
     std::unordered_map<PlayerId, std::string> act_dict;
-    int idx = 0;
     for(const auto &agent : env.Agents()){
       if(observations.count(agent) == 0) continue;
       mjxproto::Observation observation;
+      // json -> Message
       google::protobuf::util::JsonStringToMessage(observations[agent], &observation);
-      std::cout << Observation(observation).current_hand().SizeClosed() << std::endl;
-      auto action = agents[idx++]->TakeAction(Observation(observation));
+      auto action = strategy.TakeAction(Observation(observation));
       std::string json;
+      // Message -> json
       google::protobuf::util::MessageToJsonString(action, &json);
       act_dict[agent] = json;
     }
