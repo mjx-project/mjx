0af00c7d4bd82115b08d73db75b5e5be1427c40d
n0n0a <n0n0a6262@gmail.com>
2021-06-12 10:56:18 +0900

    feat: pyenv class

--- mjx/pyenv.cpp
+++ mjx/pyenv.cpp
@@ -1,1 +1,43 @@
 #include "pyenv.h"
+
+mjx::env::RLlibMahjongPyEnv::RLlibMahjongPyEnv(): env_() {}
+
+std::unordered_map<mjx::internal::PlayerId, std::string>
+mjx::env::RLlibMahjongPyEnv::reset() noexcept {
+  auto proto_obs_dict = env_.reset();
+  std::unordered_map<mjx::internal::PlayerId, std::string> json_obs_dict;
+  for(const auto &[id, obs] : proto_obs_dict){
+    std::string json;
+    google::protobuf::util::MessageToJsonString(obs, &json);
+    json_obs_dict[id] = json;
+  }
+  return json_obs_dict;
+}
+
+std::tuple<std::unordered_map<mjx::internal::PlayerId, std::string>,
+    std::unordered_map<mjx::internal::PlayerId, int>,
+    std::unordered_map<mjx::internal::PlayerId, bool>,
+    std::unordered_map<mjx::internal::PlayerId, std::string>>
+mjx::env::RLlibMahjongPyEnv::step(
+    const std::unordered_map<internal::PlayerId, std::string>&
+    action_dict) noexcept {
+  std::unordered_map<internal::PlayerId, mjxproto::Action> proto_action_dict;
+  for(const auto &[id, action] : action_dict){
+    mjxproto::Action proto_action;
+    google::protobuf::util::JsonStringToMessage(action, &proto_action);
+    proto_action_dict[id] = proto_action;
+  }
+  auto step_tpl = env_.step(proto_action_dict);
+  auto proto_obs_dict = std::get<0>(step_tpl);
+  std::unordered_map<mjx::internal::PlayerId, std::string> json_obs_dict;
+  for(const auto &[id, obs] : proto_obs_dict){
+    std::string json;
+    google::protobuf::util::MessageToJsonString(obs, &json);
+    json_obs_dict[id] = json;
+  }
+  return std::make_tuple(json_obs_dict, std::get<1>(step_tpl), std::get<2>(step_tpl), std::get<3>(step_tpl));
+}
+
+void mjx::env::RLlibMahjongPyEnv::seed(std::uint64_t game_seed) noexcept {
+  env_.seed(game_seed)
+}
--- mjx/pyenv.h
+++ mjx/pyenv.h
@@ -1,6 +1,31 @@
+#include "env.h"
+
 #ifndef MJX_PROJECT_PYENV_H
 #define MJX_PROJECT_PYENV_H
 
+namespace mjx::env {
+class RLlibMahjongPyEnv {
+ public:
+  RLlibMahjongPyEnv();
+
+  // RLlib MultiAgentEnv requires step and reset as public API
+  // https://github.com/ray-project/ray/blob/master/rllib/env/multi_agent_env.py
+  std::unordered_map<mjx::internal::PlayerId, std::string>
+  reset() noexcept;
+  std::tuple<std::unordered_map<mjx::internal::PlayerId,
+      std::string>,     // observations
+  std::unordered_map<internal::PlayerId, int>,   // rewards
+  std::unordered_map<internal::PlayerId, bool>,  // dones
+  std::unordered_map<internal::PlayerId, std::string>>  // infos
+  step(const std::unordered_map<internal::PlayerId, std::string>&
+  action_dict) noexcept;
+
+  // extra methods
+  void seed(std::uint64_t game_seed) noexcept;  // TODO: make it compatible
+ private:
+  RLlibMahjongEnv env_;
+};
+}  // namespace mjx::env
 class pyenv {};
 
 #endif  // MJX_PROJECT_PYENV_H
