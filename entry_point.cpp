521d840a0791bc3876196748a7cd781f18d08fe8
n0n0a <n0n0a6262@gmail.com>
2021-06-12 15:25:23 +0900

    fix: internal State->PyEnv

--- entry_point.cpp
+++ entry_point.cpp
@@ -9,7 +9,7 @@ PYBIND11_MODULE(pyenv, m) {
   m.doc() = "mahjong environment for python";
 
   // クラス定義
-  py::class_<mjx::internal::State>(m, "env")
+  py::class_<mjx::env::RLlibMahjongPyEnv>(m, "pyenv")
       .def(py::init<>())
       .def("reset", &mjx::env::RLlibMahjongPyEnv::reset)
       .def("step", &mjx::env::RLlibMahjongPyEnv::step)
