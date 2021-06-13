a8ee050da72150a76d0996017786d9d1796cdee0
Nao Goto <n0n0a6262@gmail.com>
2021-06-13 11:50:43 +0900

    [env] pybindで`RLlibMahjongPyEnv`のエントリーポイントを作成する (#817)
    * add: pyenv files
    
    * add files to Cmake
    
    * feat: pyenv class
    
    * refactor
    
    * Apply clang-format
    
    * add: status assertion
    
    * remove: observation copy
    
    * fix: copy -> ref
    
    * initialize
    
    * feat: entry_point
    
    * add: compiler option & pybind build
    
    * Apply clang-format
    
    * fix: internal State->PyEnv
    
    * use fetchcontent in cmake to download pybind11
    
    * rename
    
    * use cache
    
    * rename mjxpy => _mjx
    
    * refactor cmake
    
    * Apply clang-format
    
    * fix build
    
    * fix cache path
    
    * rm comment
    
    * fix cache setting
    
    Co-authored-by: GitHub Actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>

---
+++ pymjx/_mjx/src/entry_point.cpp
@@ -0,0 +1,15 @@
+#include <mjx/pyenv.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_mjx, m) {
+  m.doc() = "";
+
+  py::class_<mjx::env::RLlibMahjongPyEnv>(m, "RLlibMahjongEnv")
+      .def(py::init<>())
+      .def("reset", &mjx::env::RLlibMahjongPyEnv::reset)
+      .def("step", &mjx::env::RLlibMahjongPyEnv::step)
+      .def("seed", &mjx::env::RLlibMahjongPyEnv::seed);
+}
