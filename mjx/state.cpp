1fc0380e5659606f8d96bd345170ef5e216ab25b
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-07-03 08:12:31 +0900

    add hand/observation/state

---
+++ mjx/state.cpp
@@ -0,0 +1,34 @@
+#include "state.h"
+
+#include <google/protobuf/util/json_util.h>
+#include <google/protobuf/util/message_differencer.h>
+
+#include <utility>
+
+namespace mjx {
+mjx::State::State(mjxproto::State proto) : proto_(std::move(proto)) {}
+
+State::State(const std::string& json) {
+  auto status = google::protobuf::util::JsonStringToMessage(json, &proto_);
+  assert(status.ok());
+}
+
+const mjxproto::State& mjx::State::ToProto() const noexcept { return proto_; }
+
+std::string mjx::State::ToJson() const noexcept {
+  std::string serialized;
+  auto status =
+      google::protobuf::util::MessageToJsonString(proto_, &serialized);
+  assert(status.ok());
+  return serialized;
+}
+
+bool State::operator==(const State& other) const noexcept {
+  return google::protobuf::util::MessageDifferencer::Equals(proto_,
+                                                            other.proto_);
+}
+
+bool State::operator!=(const State& other) const noexcept {
+  return !(*this == other);
+}
+}  // namespace mjx
