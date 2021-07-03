f7bab02b31a1985c526fbdc7e863b9ff6bd04d20
n0n0a <n0n0a6262@gmail.com>
2021-07-03 14:50:46 +0900

    feat: action mask environment

--- pymjx/scripts/env_rl.py
+++ pymjx/scripts/env_rl.py
@@ -14,71 +14,90 @@ from ray.tune.registry import register_env
 
 from ray.rllib.examples.env.rock_paper_scissors import RockPaperScissors
 from ray.rllib.examples.policy.rock_paper_scissors_dummies import AlwaysSameHeuristic
-from ray.rllib.policy.view_requirement import ViewRequirement
+from ray.rllib.examples.env.parametric_actions_cartpole import ParametricActionsCartPole
+from ray.rllib.examples.models.parametric_actions_model import TorchParametricActionsModel
+from ray.rllib.agents.dqn.dqn_torch_model import DQNTorchModel
 
 ray.init()
 
-
 # original MultiAgentEnv code (ver. encoder-decoder)
-def TrainEncoderDecoderEnv():
-    class StrJanken(MultiAgentEnv):
-        Guu = "Guu"
-        Choki = "Choki"
-        Paa = "Paa"
-        HandMap = {"Guu": 0, "Choki": 1, "Paa": 2}
-        ReverseHandMap = {0: "Guu", 1: "Choki", 2: "Paa"}
+def TrainMaskEnv():
+    class MaskJanken(MultiAgentEnv):
+        Guu = 0
+        Choki = 1
+        Paa = 2
+        max_avail_actions = 3
+        AGENT_OBS_SPACE = gym.spaces.Dict({
+            "action_mask": Box(0, 1, shape=(max_avail_actions,)),
+            "avail_actions": Box(-10, 10, shape=(max_avail_actions, 2)),
+            "cart": Discrete(3),
+        })
+        AGENT_ACTION_SPACE = Discrete(max_avail_actions)
 
         def __init__(self, config):
             self.player1 = "player1"
             self.player2 = "player2"
-            self.observation_space = Discrete(3)
-            self.action_space = Discrete(3)
+            self.observation_space = self.AGENT_OBS_SPACE
+            self.action_space = self.AGENT_ACTION_SPACE
             self.last_moves = None
             self.game_count = None
             self.action_assignments = None
             self.action_mask = None
-            self.left_action_embed = np.random.randn(2)
-            self.right_action_embed = np.random.randn(2)
+            self.selection1 = self.selection2 = None
+            self.action_embeds = [np.random.randn(2) for _ in range(3)]
 
         def update_avail_actions(self):
-            self.action_assignments = np.array([[0., 0.]] * 3)
-            self.action_mask = np.array([0.] * 3)
-            selection1, selection2 = random.sample(
-                range(3), 2)
-            self.action_assignments[selection1] = self.left_action_embed
-            self.action_assignments[selection2] = self.right_action_embed
-            self.action_mask[selection1] = 1
-            self.action_mask[selection2] = 1
+            self.action_assignments = np.array([[0., 0.]] * self.action_space.n)
+            self.action_mask = np.array([0.] * self.action_space.n)
+            self.selection1, self.selection2 = random.sample(
+                range(self.action_space.n), 2)
+            self.action_assignments[self.selection1] = self.action_embeds[self.selection1]
+            self.action_assignments[self.selection2] = self.action_embeds[self.selection2]
+            self.action_mask[self.selection1] = 1
+            self.action_mask[self.selection2] = 1
 
         def reset(self):
+            obs_dict = self._internal_reset()
             self.update_avail_actions()
             return {
-                "player1": {
+                self.player1: {
                     "action_mask": self.action_mask,
                     "avail_actions": self.action_assignments,
-                    "cart": self._internal_reset()
+                    "cart": obs_dict[self.player1]
+                },
+                self.player2: {
+                    "action_mask": self.action_mask,
+                    "avail_actions": self.action_assignments,
+                    "cart": obs_dict[self.player2]
                 }
             }
 
-        def step(self, action):
+        def step(self, act_dict: Dict[str, int]):
+            orig_obs, rew, done, info = self._internal_step(act_dict)
             self.update_avail_actions()
-
-            obs = {
-                "action_mask": self.action_mask,
-                "avail_actions": self.action_assignments,
-                "cart": self._internal_reset()
+            obs_dict = {
+                self.player1: {
+                    "action_mask": self.action_mask,
+                    "avail_actions": self.action_assignments,
+                    "cart": orig_obs[self.player1]
+                },
+                self.player2: {
+                    "action_mask": self.action_mask,
+                    "avail_actions": self.action_assignments,
+                    "cart": orig_obs[self.player2]
+                }
             }
+            return obs_dict, rew, done, info
 
         def _internal_reset(self):
             self.game_count = 0
-            self.last_moves = {self.player1: StrJanken.Guu, self.player2: StrJanken.Guu}
+            self.last_moves = {self.player1: self.Guu, self.player2: self.Guu}
+            return {self.player1: self.Guu, self.player2: self.Guu}
 
         def _judge_winner(self, action_dict: Dict[str, int]) -> Dict[str, int]:
-            action1 = self.HandMap[action_dict[self.player1]]
-            action2 = self.HandMap[action_dict[self.player2]]
-            if (action1 - action2 + 1) % 3 == 0:
+            if (action_dict[self.player1] - action_dict[self.player2] + 1) % 3 == 0:
                 return {self.player1: 1, self.player2: -1}
-            elif action1 == action2:
+            elif action_dict[self.player1] == action_dict[self.player2]:
                 return {self.player1: 0, self.player2: 0}
             else:
                 return {self.player1: -1, self.player2: 1}
@@ -94,29 +113,30 @@ def TrainEncoderDecoderEnv():
             self.game_count += 1
             return observations, rewards, dones, {}
 
-    class EncodeDecodeWrapper(MultiAgentEnv):
-        def __init__(self, env):
-            self.env = env
+    class OriginalTorchParametricActionsModel(DQNTorchModel):
+        """PyTorch version of above ParametricActionsModel."""
+        def __init__(self,
+                     obs_space,
+                     action_space,
+                     num_outputs,
+                     model_config,
+                     name,
+                     **kw):
+            DQNTorchModel.__init__(self, obs_space, action_space, num_outputs,
+                                   model_config, name, **kw)
+            self.wrapped = TorchParametricActionsModel(obs_space=obs_space,
+                                                       action_space=action_space,
+                                                       num_outputs=num_outputs,
+                                                       model_config=model_config,
+                                                       name=name,
+                                                       **kw,
+                                                       true_obs_shape=(MaskJanken.AGENT_ACTION_SPACE.n, ))
 
-        def reset(self, **kwargs):
-            observations = self.env.reset(**kwargs)
-            return self.encode(observations)
+        def forward(self, input_dict, state, seq_lens):
+            return self.wrapped.forward(input_dict, state, seq_lens)
 
-        def step(self, action):
-            observations, rewards, dones, info = self.env.step(self.decode(action))
-            return self.encode(observations), rewards, dones, info
-
-        def encode(self, observations):
-            encoded = {}
-            for key, val in observations.items():
-                encoded[key] = StrJanken.HandMap[val]
-            return encoded
-
-        def decode(self, action):
-            decoded = {}
-            for key, val in action.items():
-                decoded[key] = StrJanken.ReverseHandMap[val]
-            return decoded
+        def value_function(self):
+            return self.wrapped.value_function()
 
     class ChangeInOrder(Policy):
         def get_initial_state(self):
@@ -137,34 +157,52 @@ def TrainEncoderDecoderEnv():
             # return np.random.randint(0, 3, state_batches[0].shape), state_batches, {}
             return (state_batches[0] + 1) % 3, state_batches, {}
 
-    register_env("StrJanken", lambda _: EncodeDecodeWrapper(env=StrJanken(config={})))
-    config = {
-        "env": "StrJanken",
-        "gamma": 0.9,
-        # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-        "num_gpus": 0,
-        "num_workers": 0,
-        "num_envs_per_worker": 4,
-        "train_batch_size": 200,
-        "multiagent": {
-            "policies_to_train": ["learned"],
-            "policies": {
-                "change_in_order": (ChangeInOrder, Discrete(3), Discrete(3),
-                                    {}),
-                "learned": (None, Discrete(3), Discrete(3), {
-                    "framework": "torch",
-                }),
+    config = dict(
+        {
+            "env": MaskJanken,
+            "model": {
+                "custom_model": OriginalTorchParametricActionsModel,
             },
-            "policy_mapping_fn":
-                lambda agent_id: "learned" if agent_id == "player1" else "change_in_order",
-        },
-        "framework": "torch",
-    }
+            # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+            "num_gpus": 0,
+            "num_workers": 0,
+            "multiagent": {
+                "policies_to_train": ["learned"],
+                "policies": {
+                    "change_in_order": (ChangeInOrder, MaskJanken.AGENT_OBS_SPACE, MaskJanken.AGENT_ACTION_SPACE,
+                                        {}),
+                    "learned": (None, MaskJanken.AGENT_OBS_SPACE, MaskJanken.AGENT_ACTION_SPACE, {
+                        "framework": "torch",
+                    }),
+                },
+                "policy_mapping_fn":
+                    lambda agent_id: "learned" if agent_id == "player1" else "change_in_order",
+            },
+            "framework": "torch"
+        })
     trainer_obj = ppo.PPOTrainer(config=config)
     for _ in range(100):
         results = trainer_obj.train()
         print(pretty_print(results))
 
+
+def TrainExistingMaskEnv():
+    register_env("pa_cartpole", lambda _: ParametricActionsCartPole(10))
+    config = dict(
+        {
+            "env": "pa_cartpole",
+            "model": {
+                "custom_model": TorchParametricActionsModel,
+            },
+            # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+            "num_gpus": 0,
+            "num_workers": 0,
+            "framework": "torch"
+        })
+    trainer_obj = ppo.PPOTrainer(config=config)
+    for _ in range(100):
+        results = trainer_obj.train()
+        print(pretty_print(results))
 
 
 # original MultiAgentEnv code (ver. encoder-decoder)
@@ -456,4 +494,10 @@ def TrainExistingGymEnv():
 
 
 if __name__ == '__main__':
-    TrainEncoderDecoderEnv()
+    # TrainExistingGymEnv()
+    # TrainExistingMultiAgentEnv()
+    # TrainOriginalGymEnv()
+    # TrainOriginalMultiAgentEnv()
+    # TrainEncoderDecoderEnv()
+    # TrainExistingMaskEnv()
+    TrainMaskEnv()
