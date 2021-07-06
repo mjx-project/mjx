91d8652896533f2b1a6c25685a8cb2423b032f86
Okano Shinri <n.salt2000@gmail.com>
2021-07-06 20:17:26 +0900

    unittest->pytest

--- pymjx/mjx/tests/test_visualizer.py
+++ pymjx/mjx/tests/test_visualizer.py
@@ -1,24 +1,18 @@
 import glob
-import unittest
 
 from mjx.visualizer.visualizer import GameBoardVisualizer, GameVisualConfig, MahjongTable
 
 
-class VisualizerTest(unittest.TestCase):
-    def test_visualizer(self):
-        mode = "obs"
-        show = False
-        files = glob.glob("json/*.json")
-        for file in files:
-            game_data = MahjongTable.load_data(file, mode)
-            self.assertEqual(list, type(game_data))
-            self.assertEqual(MahjongTable, type(game_data[0]))
-            self.assertEqual(4, len(game_data[0].players))
+def test_visualizer():
+    mode = "obs"
+    show = False
+    files = glob.glob("resources/observation/*.json")
+    for file in files:
+        game_data = MahjongTable.load_data(file, mode)
+        assert list == type(game_data)
+        assert MahjongTable == type(game_data[0])
+        assert 4 == len(game_data[0].players)
 
-            if show:
-                board_visualizer = GameBoardVisualizer(GameVisualConfig())
-                board_visualizer.print(game_data[0])
-
-
-if __name__ == "__main__":
-    unittest.main()
+        if show:
+            board_visualizer = GameBoardVisualizer(GameVisualConfig())
+            board_visualizer.print(game_data[0])
