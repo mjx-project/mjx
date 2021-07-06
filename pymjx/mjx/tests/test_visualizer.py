d46e6bcf9c419da88c411a3ccdd8acf73a00962d
Okano Shinri <n.salt2000@gmail.com>
2021-07-06 20:28:02 +0900

    black

--- pymjx/mjx/tests/test_visualizer.py
+++ pymjx/mjx/tests/test_visualizer.py
@@ -9,8 +9,8 @@ def test_visualizer():
     files = glob.glob("resources/observation/*.json")
     for file in files:
         game_data = MahjongTable.load_data(file, mode)
-        assert isinstance(game_data,list)
-        assert isinstance(game_data[0],MahjongTable)
+        assert isinstance(game_data, list)
+        assert isinstance(game_data[0], MahjongTable)
         assert 4 == len(game_data[0].players)
 
         if show:
