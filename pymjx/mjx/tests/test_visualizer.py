45036415a951f84c40e14ca3518876c095ee60e8
Okano Shinri <n.salt2000@gmail.com>
2021-07-06 19:51:20 +0900

    複数のjsonファイルをまとめてテスト

--- pymjx/mjx/tests/test_visualizer.py
+++ pymjx/mjx/tests/test_visualizer.py
@@ -1,22 +1,23 @@
 import unittest
+import glob
+
 from mjx.visualizer.visualizer import GameVisualConfig, GameBoardVisualizer, MahjongTable
 
 
 class VisualizerTest(unittest.TestCase):
     def test_visualizer(self):
-        path = "observations.json"
         mode = "obs"
-        print = False
+        show = False
+        files = glob.glob("json/*.json")
+        for file in files:
+            game_data = MahjongTable.load_data(file, mode)
+            self.assertEqual(list, type(game_data))
+            self.assertEqual(MahjongTable, type(game_data[0]))
+            self.assertEqual(4, len(game_data[0].players))
 
-        game_data = MahjongTable.load_data(path, mode)
-
-        self.assertEqual(list, type(game_data))
-        self.assertEqual(MahjongTable, type(game_data[0]))
-        self.assertEqual(4, len(game_data[0].players))
-
-        if print:
-            board_visualizer = GameBoardVisualizer(GameVisualConfig())
-            board_visualizer.print(game_data)
+            if show:
+                board_visualizer = GameBoardVisualizer(GameVisualConfig())
+                board_visualizer.print(game_data[0])
 
 
 if __name__ == "__main__":
