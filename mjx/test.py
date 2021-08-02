c90b28289d57cf11f9e69e62d01e2be8292180d6
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-08-02 17:12:12 +0900

    remove pybind from this repo

--- mjx/test.py
+++
@@ -1,9 +0,0 @@
-import mjxcore
-
-
-def test_RLlibMahjongEnv():
-    env = mjxcore.RLlibMahjongEnv()
-    env.seed(1234)
-    obs = env.reset()
-    assert len(obs.keys()) == 1
-    assert 'player_2' in obs.keys()
