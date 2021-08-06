dd0e3ebf02808f1284b0a7678d8732ed53712855
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-08-06 09:19:29 +0900

    implement test for mjx.env.RLlivMahjongEnv

--- pymjx/mjx/env.py
+++ pymjx/mjx/env.py
@@ -1,36 +1,13 @@
-# from typing import Dict, Tuple
-#
-# from google.protobuf import json_format
-#
-# import mjxcore
-# import mjxproto
-#
-#
-# class RLlibMahjongEnv:
-#     def __init__(self):
-#         self.json_env = mjxcore.RLlibMahjongEnv()
-#
-#     def step(
-#         self, actions: Dict[str, mjxproto.Action]
-#     ) -> Tuple[Dict[str, mjxproto.Observation], Dict[str, int], Dict[str, bool], Dict[str, str]]:
-#         json_observation, rewards, dones, info = self.json_env.step(
-#             {id: json_format.MessageToJson(act, False) for id, act in actions.items()}
-#         )
-#         return (
-#             {
-#                 id: json_format.Parse(obs, mjxproto.Observation())
-#                 for id, obs in json_observation.items()
-#             },
-#             rewards,
-#             dones,
-#             info,
-#         )
-#
-#     def reset(self) -> Dict[str, mjxproto.Observation]:
-#         return {
-#             id: json_format.Parse(obs, mjxproto.Observation())
-#             for id, obs in self.json_env.reset().items()
-#         }
-#
-#     def seed(self, seed: int):
-#         self.json_env.seed(seed)
+class RLlibMahjongEnv:
+    def __init__(self):
+        import mjx._mjx as _mjx
+        self.env = _mjx.RLlibMahjongEnv()
+
+    def step(self, action_dict):
+        return self.env.step(action_dict)
+
+    def reset(self):
+        return self.env.reset()
+
+    def seed(self, seed: int):
+        self.env.seed(seed)
--- test.py
+++ test.py
@@ -1,9 +1,23 @@
-import mjx._mjx as m
+import mjx.env
+import random
 
 
 def test_RLlibMahjongEnv():
-    env = m.RLlibMahjongEnv()
+    random.seed(1234)
+    env = mjx.env.RLlibMahjongEnv()
     env.seed(1234)
-    obs = env.reset()
-    assert len(obs.keys()) == 1
-    assert 'player_2' in obs.keys()
+    obs_dict = env.reset()
+    dones = {"__all__": False}
+    while not dones["__all__"]:
+        action_dict = {agent: random.choice(obs.legal_actions())
+                       for agent, obs in obs_dict.items()}
+        obs_dict, rewards, dones, info = env.step(action_dict)
+    assert len(rewards) == 4
+    assert rewards['player_0'] == 0
+    assert rewards['player_1'] == 45
+    assert rewards['player_2'] == 90
+    assert rewards['player_3'] == -135
+
+
+if __name__ == '__main__':
+    test_RLlibMahjongEnv()
