acdd192713b5d8c5ed29e1aedfabc89cdfd6dbae
n0n0a <n0n0a6262@gmail.com>
2021-08-13 12:27:15 +0900

    add: pettingzoo env test

--- test.py
+++ test.py
@@ -21,5 +21,27 @@ def test_RLlibMahjongEnv():
     assert rewards['player_3'] == -135
 
 
+def test_PettingZooMahjongEnv():
+    random.seed(1234)
+    env = mjx.env.PettingZooMahjongEnv()
+    env.seed(1234)
+    env.reset()
+    rewards = {}
+    for agent in env.agent_iter():
+        observation, reward, done, info = env.last(True)
+        rewards[agent] = reward
+        legal_actions = [i for i, b in enumerate(observation["action_mask"]) if b]
+        action = random.choice(legal_actions)
+        if reward == 90:
+            break
+        env.step(action)
+    assert len(rewards) == 4
+    assert rewards['player_0'] == 0
+    assert rewards['player_1'] == 45
+    assert rewards['player_2'] == 90
+    assert rewards['player_3'] == -135
+
+
 if __name__ == '__main__':
     test_RLlibMahjongEnv()
+    test_PettingZooMahjongEnv()
