0aa50bf543f41bcc02d6fe582be44be3403abb4f
n0n0a <n0n0a6262@gmail.com>
2021-08-31 15:27:48 +0900

    add: observation methods

--- pymjx/mjx/observation.py
+++ pymjx/mjx/observation.py
@@ -1,3 +1,12 @@
 class Observation:
     def __init__(self, cpp_obj):
         self._cpp_obj = cpp_obj
+
+    def to_feature(self, version):
+        return self._cpp_obj.to_feature(version)
+
+    def legal_actions(self):
+        return self._cpp_obj.legal_actions()
+
+    def action_mask(self):
+        return self._cpp_obj.action_mask()
