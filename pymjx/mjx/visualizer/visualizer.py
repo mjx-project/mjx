4e82976a7b3371ef194189185126e8c9f312ada0
Okano Shinri <n.salt2000@gmail.com>
2021-09-14 20:19:35 +0900

    import修正

--- pymjx/mjx/visualizer/visualizer.py
+++ pymjx/mjx/visualizer/visualizer.py
@@ -3,6 +3,7 @@ import os
 import sys
 from dataclasses import dataclass
 
+sys.path.append(os.path.dirname(__file__))
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 import mjxproto
 from google.protobuf import json_format
