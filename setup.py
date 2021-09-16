bbe070f0ad3b9fa914771681dbbeb58d91fb43cd
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-09-16 23:24:14 +0900

    fix

--- setup.py
+++ setup.py
@@ -161,10 +161,11 @@ setup(
         "grpcio==1.39.0",
         "grpcio-tools==1.39.0",
         "rich==10.9.0",
-        "gym==0.19.0",
-        "tenhou_wall_reproducer",
+        "pillow",
+        "gym",
         "pettingzoo",
         "svgwrite",
+        "tenhou_wall_reproducer",
     ],
     extras_require={"test": ["pytest"]},
     include_package_data=True
