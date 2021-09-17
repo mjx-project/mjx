982348869f65e5a55f592f3ee5449fbc82f5a9fe
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-09-17 19:26:58 +0900

    apply formats

--- pymjx/mjx/agent.py
+++ pymjx/mjx/agent.py
@@ -1,9 +1,10 @@
 import mjx
 
-class RandomDebugAgent():
 
+class RandomDebugAgent:
     def __init__(self) -> None:
         import mjx._mjx as _mjx
+
         self._agent = _mjx.RandomDebugAgent()
 
     def act(self, observation):
