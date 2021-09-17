c1e773eb6f1f8c0420f88218a8f2e8584452097e
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-09-18 00:18:50 +0900

    apply formats

--- pymjx/mjx/__init__.py
+++ pymjx/mjx/__init__.py
@@ -1,7 +1,7 @@
 from mjx.action import Action
 from mjx.diff import check_equality  # noqa: F401
+from mjx.env import MjxEnv
 from mjx.observation import Observation
 from mjx.state import State
-from mjx.env import MjxEnv
 
 __all__ = ["MjxEnv", "Action", "Observation", "State"]
