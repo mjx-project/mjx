3afd15a9f99e7f6519841bce499bee72db1b9d25
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-09-20 16:40:05 +0900

    add train example

--- examples/rllib_example.py
+++ examples/rllib_example.py
@@ -79,5 +79,157 @@ def random_run():
     assert list(sorted(rewards.values())) == [-135, 0, 45, 90]
 
 
+def train():
+    import ray
+    import mjx.env
+    import ray.rllib.agents.pg as pg
+    import re
+    import numpy as np
+    from gym.spaces import Discrete, Box, Dict
+    from ray.rllib.agents.dqn.dqn_torch_model import DQNTorchModel
+    from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
+    from ray.rllib.utils.framework import try_import_tf, try_import_torch
+    from ray.rllib.utils.torch_ops import FLOAT_MIN, FLOAT_MAX
+    from ray.tune.registry import register_env
+    from ray.tune.logger import pretty_print
+    from ray.rllib.policy.policy import Policy
+    
+    tf1, tf, tfv = try_import_tf()
+    torch, nn = try_import_torch()
+    
+    env = RLlibMahjongEnv()
+    
+    
+    class TorchRLlibMahjongEnvModel(DQNTorchModel):
+        """PyTorch version of above ParametricActionsModel."""
+    
+        def __init__(self,
+                     obs_space,
+                     action_space,
+                     num_outputs,
+                     model_config,
+                     name,
+                     true_obs_shape=(env.num_features,),
+                     action_embed_size=1,
+                     **kw):
+            DQNTorchModel.__init__(self, obs_space, action_space, num_outputs,
+                                   model_config, name, **kw)
+    
+            self.action_embed_model = TorchFC(
+                Box(-1, 1, shape=true_obs_shape), action_space, action_embed_size,
+                model_config, name + "_action_embed")
+    
+        def forward(self, input_dict, state, seq_lens):
+            # Extract the available actions tensor from the observation.
+            action_mask = input_dict["obs"]["action_mask"]
+    
+            # Compute the predicted action embedding
+            action_embed, _ = self.action_embed_model({
+                "obs": input_dict["obs"]["real_obs"]
+            })
+    
+            # Expand the model output to [BATCH, 1, EMBED_SIZE]. Note that the
+            # avail actions tensor is of shape [BATCH, MAX_ACTIONS, EMBED_SIZE].
+            intent_vector = torch.unsqueeze(action_embed, 1)
+            action_mask_plus = torch.unsqueeze(action_mask, 2)
+    
+            # Batch dot product => shape of logits is [BATCH, MAX_ACTIONS].
+            action_logits = torch.sum(action_mask_plus * intent_vector, dim=2)
+    
+            # Mask out invalid actions (use -inf to tag invalid).
+            # These are then recognized by the EpsilonGreedy exploration component
+            # as invalid actions that are not to be chosen.
+            inf_mask = torch.clamp(torch.log(action_mask), FLOAT_MIN, FLOAT_MAX)
+    
+            return action_logits + inf_mask, state
+    
+        def value_function(self):
+            return self.action_embed_model.value_function()
+    
+    
+    class RuleBased(Policy):
+        def get_initial_state(self):
+            return [
+                np.random.choice(
+                    range(env.num_actions)
+                )
+            ]
+    
+        def compute_actions(self,
+                            obs_batch,
+                            state_batches=None,
+                            prev_action_batch=None,
+                            prev_reward_batch=None,
+                            info_batch=None,
+                            episodes=None,
+                            **kwargs):
+            legal_actions = [idx for idx in range(env.num_actions) if obs_batch[0][idx]]
+            if 179 in legal_actions:
+                legal_actions.remove(179)
+            action = np.random.choice(legal_actions) if legal_actions else 0
+            return np.array([action]), state_batches, {}
+    
+    
+    def select_policy(agent_id):
+        num = re.sub(r"\D", "", agent_id)
+        return f"random{num}" if num != "0" else "learned"
+    
+    register_env("rllibmahjong", lambda _: RLlibMahjongEnv())
+    config = dict(
+        {
+            "env": "rllibmahjong",
+            "model": {
+                "custom_model": TorchRLlibMahjongEnvModel,
+            },
+            # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+            "num_gpus": 0,
+            "num_workers": 0,
+            "output": "batch/onpolicy",
+            "explore": True,
+            # "exploration_config": {
+            #     "type": "EpsilonGreedy",
+            #     "initial_epsilon": 0.3,
+            #     "final_epsilon": 0.01,
+            # },
+            "exploration_config": {
+                "type": "SoftQ",
+                "temperature": 1,
+            },
+            # "gamma": 0.99,
+            # "train_batch_size": 2000,
+            # "batch_mode": "complete_episodes",
+            "multiagent": {
+                "policies_to_train": ["learned"],
+                "policies": {
+                    "learned": (None, env.observation_space, env.action_space, {
+                        "framework": "torch",
+                    }),
+                    "random1": (RuleBased,
+                                env.observation_space,
+                                env.action_space,
+                                {}),
+                    "random2": (RuleBased,
+                                env.observation_space,
+                                env.action_space,
+                                {}),
+                    "random3": (RuleBased,
+                                env.observation_space,
+                                env.action_space,
+                                {})
+                },
+                "policy_mapping_fn": select_policy
+            },
+            "framework": "torch"
+        })
+
+    ray.init() 
+    trainer_obj = pg.PGTrainer(config=config)
+    results = []
+    for _ in range(10):
+        result = trainer_obj.train()
+        print(pretty_print(result))
+
+
 if __name__ == '__main__':
-	random_run()
+    random_run()
+    train()
