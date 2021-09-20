f9f935ddd75c6c3aa60d9ed9f878804806c2bbc1
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-09-20 19:18:32 +0900

    add test_action.py

--- pymjx/tests/test_action.py
+++ pymjx/tests/test_action.py
@@ -2,7 +2,11 @@ import mjx
 
 
 def test_Action():
-    json_str = ""
+    json_str = '{"gameId":"b2a9fa48-44f1-460b-8b0a-e75d336c2bb1","tile":3}'
     action = mjx.Action(json_str)
-    restored = action.to_json()
+    restored = action._cpp_obj.to_json()
     assert json_str == restored
+
+
+if __name__ == '__main__':
+    test_Action()
