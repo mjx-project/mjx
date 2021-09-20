c2da14c5bf5d5e83fa8d2e219f71b65a33f1e80d
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-09-20 16:09:11 +0900

    [py] Remove rllib and pettingzoo env from python for simplicity (#937)
    * remove rllib and pettingzoo env from python for simplicity
    
    * fix

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import mjx.env
 import mjx.agent
 import random
-from pettingzoo.test import api_test
 
 
 def test_EnvRunner():
@@ -37,80 +36,7 @@ def test_MjxEnv():
     assert rewards['player_3'] == -135
 
 
-def test_RLlibMahjongEnv():
-    random.seed(1234)
-    env = mjx.env.RLlibMahjongEnv()
-    env.seed(1234)
-    obs_dict = env.reset()
-    dones = {"__all__": False}
-    while not dones["__all__"]:
-        action_dict = {}
-        for agent, obs in obs_dict.items():
-            legal_actions = [i for i, b in enumerate(obs["action_mask"]) if b]
-            action_dict[agent] = random.choice(legal_actions)
-        obs_dict, rewards, dones, info = env.step(action_dict)
-    assert len(rewards) == 4
-    assert list(sorted(rewards.values())) == [-135, 0, 45, 90]
-
-
-def test_PettingZooMahjongEnv():
-    random.seed(1234)
-    env = mjx.env.PettingZooMahjongEnv()
-    env.seed(1234)
-    env.reset()
-    results = []
-    for agent in env.agent_iter():
-        observation, reward, done, info = env.last(True)
-        results.append((agent, observation, reward, done, info))
-        legal_actions = [i for i, b in enumerate(
-            observation["action_mask"]) if b]
-        action = random.choice(legal_actions)
-        env.step(action if not done else None)
-
-    # PettingZoo Test
-    # https://github.com/mjx-project/mjx/pull/887
-    # Last iteration before done
-    agent, observation, reward, done, info = results[-5]
-    assert observation["action_mask"][180] == 0
-    assert reward == 0
-    assert not done
-    assert info == {}
-
-    # After done
-    final_rewards = set([])
-
-    agent, observation, reward, done, info = results[-4]
-    assert observation["action_mask"][180] == 1
-    final_rewards.add(reward)
-    assert done
-    assert info == {}
-
-    agent, observation, reward, done, info = results[-3]
-    assert observation["action_mask"][180] == 1
-    final_rewards.add(reward)
-    assert done
-    assert info == {}
-
-    agent, observation, reward, done, info = results[-2]
-    assert observation["action_mask"][180] == 1
-    final_rewards.add(reward)
-    assert done
-    assert info == {}
-
-    agent, observation, reward, done, info = results[-1]
-    assert observation["action_mask"][180] == 1
-    final_rewards.add(reward)
-    assert done
-    assert info == {}
-
-    assert final_rewards == {-135, 0, 45, 90}
-
-    # API test
-    api_test(env, num_cycles=100000, verbose_progress=False)
-
 
 if __name__ == '__main__':
     test_EnvRunner()
     test_MjxEnv()
-    test_RLlibMahjongEnv()
-    test_PettingZooMahjongEnv()
