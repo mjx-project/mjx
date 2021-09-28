60c6b0470c6c898b09fd41ca08a25660a30b2dd4
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-09-28 20:44:54 +0900

    update

--- mjx/agent.py
+++ mjx/agent.py
@@ -5,9 +5,17 @@ import _mjx  # type: ignore
 import mjx
 
 
-class RandomAgent(_mjx.Agent):  # type: ignore
+class Agent(_mjx.Agent):  # type: ignore
     def __init__(self) -> None:
         _mjx.Agent.__init__(self)  # type: ignore
+
+    def act(self, observation: _mjx.Observation) -> _mjx.Action:  # type: ignore
+        pass
+
+
+class RandomAgent(Agent):  # type: ignore
+    def __init__(self) -> None:
+        super().__init__()
 
     def act(self, observation: _mjx.Observation) -> _mjx.Action:  # type: ignore
         return random.choice(observation.legal_actions())
