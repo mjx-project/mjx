7f4c3c7ce4a0b878442ce5018b6e1e02ea6bbf49
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-09-28 20:53:29 +0900

    fix

--- mjx/agent.py
+++ mjx/agent.py
@@ -9,13 +9,16 @@ class Agent(_mjx.Agent):  # type: ignore
     def __init__(self) -> None:
         _mjx.Agent.__init__(self)  # type: ignore
 
+    def act_impl(self, observation: mjx.Observation) -> mjx.Action:
+        raise NotImplementedError
+
     def act(self, observation: _mjx.Observation) -> _mjx.Action:  # type: ignore
-        pass
+        return self.act_impl(mjx.Observation._from_cpp_obj(observation))._cpp_obj
 
 
 class RandomAgent(Agent):  # type: ignore
     def __init__(self) -> None:
         super().__init__()
 
-    def act(self, observation: _mjx.Observation) -> _mjx.Action:  # type: ignore
+    def act_impl(self, observation: mjx.Observation) -> mjx.Action:  # type: ignore
         return random.choice(observation.legal_actions())
