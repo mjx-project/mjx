b84982fc3fe98b761d37ec5d69999c314b4e1784
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-09-30 01:53:39 +0900

    add default arg

--- include/mjx/agent.h
+++ include/mjx/agent.h
@@ -24,7 +24,7 @@ class Agent {
       const Observation& observation) const noexcept = 0;
   [[nodiscard]] virtual std::vector<mjx::Action> ActBatch(
       const std::vector<mjx::Observation>& observations) const noexcept;
-  void Serve(const std::string& socket_address, int batch_size, int wait_limit_ms, int sleep_ms) const noexcept;
+  void Serve(const std::string& socket_address, int batch_size = 64, int wait_limit_ms = 100, int sleep_ms = 10) const noexcept;
 };
 
 class AgentServer {
