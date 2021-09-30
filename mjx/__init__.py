227707578b0882e8b416588029b9fb03c2dc8b95
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-09-30 13:04:22 +0900

    fix __init__.py

--- mjx/__init__.py
+++ mjx/__init__.py
@@ -1,5 +1,7 @@
 from mjx.action import Action
+from mjx.agent import Agent
+from mjx.env import MjxEnv
 from mjx.observation import Observation
 from mjx.state import State
 
-__all__ = ["Action", "Observation", "State"]
+__all__ = ["Action", "Observation", "State", "MjxEnv", "Agent"]
