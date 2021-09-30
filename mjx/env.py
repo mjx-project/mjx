e0c154566339650427589d38ac29c1c657bff804
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-09-30 12:27:52 +0900

    clean up type-hinting

--- mjx/env.py
+++ mjx/env.py
@@ -10,18 +10,18 @@ class MjxEnv:
         self,
         player_ids: List[str] = ["player_0", "player_1", "player_2", "player_3"],
     ):
-        self._env = _mjx.MjxEnv(player_ids)  # type: ignore
+        self._env: _mjx.MjxEnv = _mjx.MjxEnv(player_ids)  # type: ignore
 
     def seed(self, seed) -> None:
         self._env.seed(seed)
 
     def reset(self) -> Dict[str, mjx.Observation]:
-        cpp_obs_dict = self._env.reset()
+        cpp_obs_dict: Dict[str, _mjx.Observation] = self._env.reset()  # type: ignore
         return {k: mjx.Observation._from_cpp_obj(v) for k, v in cpp_obs_dict.items()}
 
     def step(self, aciton_dict: Dict[str, mjx.Action]) -> Dict[str, mjx.Observation]:
-        cpp_action_dict = {k: v._cpp_obj for k, v in aciton_dict.items()}
-        cpp_obs_dict = self._env.step(cpp_action_dict)
+        cpp_action_dict: Dict[str, _mjx.Action] = {k: v._cpp_obj for k, v in aciton_dict.items()}  # type: ignore
+        cpp_obs_dict: Dict[str, _mjx.Observation] = self._env.step(cpp_action_dict)  # type: ignore
         return {k: mjx.Observation._from_cpp_obj(v) for k, v in cpp_obs_dict.items()}
 
     def done(self) -> bool:
