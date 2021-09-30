161ff6a9c1628fe728fdd6e984a6fb4db5c54739
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-09-30 12:46:52 +0900

    move diff.py to utils/diff.py

--- mjx/__init__.py
+++ mjx/__init__.py
@@ -1,6 +1,5 @@
 from mjx.action import Action
 from mjx.agent import Agent
-from mjx.diff import check_equality  # noqa: F401
 from mjx.env import MjxEnv
 from mjx.observation import Observation
 from mjx.state import State
