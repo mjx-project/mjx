233afe2f71d86c9a28cb7f91add8d5f00f81da98
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-10-05 16:26:03 +0900

    fix script

--- scripts/run_games.cpp
+++ scripts/run_games.cpp
@@ -9,9 +9,5 @@ int main() {
       {"player_2", agent.get()},
       {"player_3", agent.get()},
   };
-  auto runner = mjx::EnvRunner(agents, 1, 1, true);
-  std::optional<std::string> state_json;
-  while ((state_json = runner.pop_state())) {
-    std::cerr << state_json.value() << std::endl;
-  }
+  auto runner = mjx::EnvRunner(agents, 1, 1);
 }
