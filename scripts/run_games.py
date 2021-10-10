2af85c6c7c9715df652ae49a561747fc4703f71f
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-10-10 18:43:49 +0900

    apply fmt

--- scripts/run_games.py
+++ scripts/run_games.py
@@ -5,6 +5,6 @@ agents = {
     "player_0": "127.0.0.1:9090",
     "player_1": "127.0.0.1:9090",
     "player_2": "127.0.0.1:9090",
-    "player_3": "127.0.0.1:9090"
+    "player_3": "127.0.0.1:9090",
 }
 mjx.run(agents, 100, 8, 10, seed_type=SeedType.DUPLICATE)
