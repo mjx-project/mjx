17a8cd2f98966b7729587f0c89577b66019a45c1
Okano Shinri <n.salt2000@gmail.com>
2021-10-12 12:24:14 +0000

    move

--- mjx/visualizer/serve2.py
+++ mjx/visualizer/serve2.py
@@ -1,4 +1,4 @@
 import mjx.agent
 
-human_control_agent = mjx.agent.HumanControlAgent(unicode=True, ja=True)
+human_control_agent = mjx.agent.HumanControlAgent()
 human_control_agent.serve("127.0.0.1:9091", 1, 0, 0)
