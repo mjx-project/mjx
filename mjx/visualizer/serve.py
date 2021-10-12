176ae6813404ca80bc1038c7f03042be13829e10
Okano Shinri <n.salt2000@gmail.com>
2021-10-12 12:39:25 +0000

    remove files

--- mjx/visualizer/serve.py
+++
@@ -1,4 +0,0 @@
-import mjx.agent
-
-rulebased_agent = mjx.agent.RuleBasedAgent()
-rulebased_agent.serve("127.0.0.1:9090", 1, 0, 0)
--- mjx/visualizer/serve2.py
+++
@@ -1,4 +0,0 @@
-import mjx.agent
-
-human_control_agent = mjx.agent.HumanControlAgent()
-human_control_agent.serve("127.0.0.1:9091", 1, 0, 0)
