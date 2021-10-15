575b1f78aade7068b8483f52449c751773dfdbc7
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-10-15 19:32:40 +0900

    fix triple ron

--- include/mjx/internal/state.cpp
+++ include/mjx/internal/state.cpp
@@ -312,7 +312,23 @@ State::State(const mjxproto::State &state) {
 
   // Update by events
   std::queue<mjxproto::Action> actions;
+  int last_ron_target = -1;
+  int last_ron_target_tile = -1;
   for (const auto &event : state.public_observation().events()) {
+    if (Any(event.type(), {mjxproto::EVENT_TYPE_DISCARD, mjxproto::EVENT_TYPE_TSUMOGIRI, mjxproto::EVENT_TYPE_ADDED_KAN})) {
+      last_ron_target = event.who();
+      last_ron_target_tile = event.tile();
+    }
+    if (event.type() == mjxproto::EVENT_TYPE_ABORTIVE_DRAW_THREE_RONS) {
+      assert(last_ron_target != -1);
+      assert(last_ron_target_tile != -1);
+      for (int i = 0; i < 4; ++i) {
+        if (i == last_ron_target) continue;
+        mjxproto::Action ron = Action::CreateRon(AbsolutePos(i), Tile(last_ron_target_tile), state_.public_observation().game_id());
+        actions.push(ron);
+      }
+      continue;
+    }
     std::optional<mjxproto::Action> action = Action::FromEvent(event);
     if (action) actions.push(action.value());
   }
