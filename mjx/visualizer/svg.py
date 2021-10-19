3393c07f77d954dd2596140cdf7b0b750ee09008
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-10-19 22:44:48 +0900

    fix make_svg => _make_svg

--- mjx/visualizer/svg.py
+++ mjx/visualizer/svg.py
@@ -888,7 +888,7 @@ def _make_svg(
 
 def svg_from_json(json_data: str, target_idx: int) -> str:
     proto_data = MahjongTable.json_to_proto(json_data=json_data)
-    dwg = make_svg(proto_data, ".svg", target_idx)
+    dwg = _make_svg(proto_data, ".svg", target_idx)
     return '<?xml version="1.0" encoding="utf-8" ?>\n' + dwg.tostring()
 
 
