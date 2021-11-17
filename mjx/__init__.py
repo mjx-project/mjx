c946f2a1fe142e0eed569ec3789a70cd2bef581b
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-11-17 15:24:50 +0900

    `mjx.Action.type` を追加 (#1028)
    * add action.type
    
    * make mjx.ActionType available

--- mjx/__init__.py
+++ mjx/__init__.py
@@ -1,8 +1,9 @@
 from mjx.action import Action
 from mjx.agent import Agent
+from mjx.const import ActionType
 from mjx.env import MjxEnv, run
 from mjx.observation import Observation
 from mjx.open import Open
 from mjx.state import State
 
-__all__ = ["Action", "Observation", "State", "MjxEnv", "Agent", "Open", "run"]
+__all__ = ["Action", "Observation", "State", "MjxEnv", "Agent", "Open", "run", "ActionType"]
