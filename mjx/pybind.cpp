be9b5ac50cb4efa9428aa7dfb96e51f0dab1d247
Keigo Habara <k.habara.aa@gmail.com>
2021-11-17 17:38:24 +0900

    Remove proceeding tile types

--- mjx/pybind.cpp
+++ mjx/pybind.cpp
@@ -105,7 +105,5 @@ PYBIND11_MODULE(_mjx, m) {
       .def("state", &mjx::MjxEnv::state);
 
   py::class_<mjx::ShantenCalculator>(m, "ShantenCalculator")
-      .def_static("shanten_number", &mjx::ShantenCalculator::ShantenNumber)
-      .def_static("proceeding_tile_types",
-                  &mjx::ShantenCalculator::ProceedingTileTypes);
+      .def_static("shanten_number", &mjx::ShantenCalculator::ShantenNumber);
 }
--- mjx/shanten_calculator.py
+++ mjx/shanten_calculator.py
@@ -17,13 +17,3 @@ class ShantenCalculator:
             tile_type_cnt[tile_type] += 1
 
         return _mjx.ShantenCalculator.shanten_number(tile_type_cnt, len(hand.opens))
-
-    @classmethod
-    def proceeding_tile_types(cls, hand: mjxproto.Hand) -> int:
-        closed_tiles = hand.closed_tiles
-        tile_type_cnt: List[int] = [0] * 34
-        for tile_id in closed_tiles:
-            tile_type = tile_id // 4
-            tile_type_cnt[tile_type] += 1
-
-        return _mjx.ShantenCalculator.proceeding_tile_types(tile_type_cnt, len(hand.opens))
