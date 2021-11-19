1ae7607fb45a0dbce734aa5af8d80d620e4ab8ae
Keigo Habara <k.habara.aa@gmail.com>
2021-11-19 17:49:33 +0900

    Fix

--- mjx/__init__.py
+++ mjx/__init__.py
@@ -1,11 +1,12 @@
 from mjx.action import Action
 from mjx.agent import Agent
-from mjx.const import ActionType
+from mjx.const import ActionType, TileType
 from mjx.env import MjxEnv, run
 from mjx.observation import Observation
 from mjx.open import Open
-from mjx.shanten_calculator import ShantenCalculator
 from mjx.state import State
+from mjx.tile import Tile
+from mjx.hand import Hand
 
 __all__ = [
     "Action",
@@ -14,7 +15,8 @@ __all__ = [
     "MjxEnv",
     "Agent",
     "Open",
+    "Hand",
     "run",
     "ActionType",
-    "ShantenCalculator",
+    "TileType",
 ]
--- mjx/tile.py
+++ mjx/tile.py
@@ -9,3 +9,7 @@ class Tile:
 
     def type(self) -> TileType:
         return TileType(self.tid // 4)
+
+    def is_red(self) -> bool:
+        # m5(red), p5(red), s5(red)
+        return self.tid in [16, 52, 88]
