07e943650a933b4446fb2ef05426aa8df346bfc3
keigo habara <34413567+habara-k@users.noreply.github.com>
2021-11-21 18:52:48 +0900

    Serve `mjx.Hand` and `mjx.Tile` (#1035)
    * Add mjx.Hand and mjx.Tile
    
    * Apply formatter
    
    * Fix
    
    * Apply formatter
    
    * Add Tile to __init__.py
    
    * fix imports
    
    Co-authored-by: GitHub Actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>

--- mjx/__init__.py
+++ mjx/__init__.py
@@ -1,11 +1,12 @@
 from mjx.action import Action
 from mjx.agent import Agent
-from mjx.const import ActionType
+from mjx.const import ActionType, TileType
 from mjx.env import MjxEnv, run
+from mjx.hand import Hand
 from mjx.observation import Observation
 from mjx.open import Open
-from mjx.shanten_calculator import ShantenCalculator
 from mjx.state import State
+from mjx.tile import Tile
 
 __all__ = [
     "Action",
@@ -14,7 +15,9 @@ __all__ = [
     "MjxEnv",
     "Agent",
     "Open",
+    "Hand",
+    "Tile",
     "run",
     "ActionType",
-    "ShantenCalculator",
+    "TileType",
 ]
---
+++ mjx/hand.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from typing import List
+
+import _mjx  # type: ignore
+from google.protobuf import json_format
+
+import mjxproto
+from mjx.open import Open
+from mjx.tile import Tile
+
+
+class Hand:
+    def closed_tiles(self) -> List[Tile]:
+        assert self._cpp_obj is not None
+        return [Tile(tid) for tid in self.to_proto().closed_tiles]  # type: ignore
+
+    def opens(self) -> List[Open]:
+        assert self._cpp_obj is not None
+        return [Open(bit) for bit in self.to_proto().opens]  # type: ignore
+
+    def shanten_number(self) -> int:
+        closed_tiles = self.closed_tiles()
+        tile_type_cnt: List[int] = [0] * 34
+        for tile in closed_tiles:
+            tile_type_cnt[tile.type()] += 1
+
+        return _mjx.ShantenCalculator.shanten_number(tile_type_cnt, len(self.opens()))
+
+    def to_json(self) -> str:
+        assert self._cpp_obj is not None
+        return self._cpp_obj.to_json()  # type: ignore
+
+    def to_proto(self) -> mjxproto.Hand:
+        assert self._cpp_obj is not None
+        return json_format.Parse(self.to_json(), mjxproto.Hand())
+
+    @classmethod
+    def _from_cpp_obj(cls, cpp_obj: _mjx.Hand) -> Hand:  # type: ignore
+        hand = cls()
+        hand._cpp_obj = cpp_obj
+        return hand
