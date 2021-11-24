d51701d198549ff2fa9814d5aa398dc81c3ff500
Okano Shinri <n.salt2000@gmail.com>
2021-11-24 12:09:27 +0000

    red_hai

--- mjx/agent.py
+++ mjx/agent.py
@@ -2,14 +2,17 @@ import random
 import threading
 import time
 from queue import Queue
-from typing import List, Optional
+from typing import Dict, List, Optional, Tuple
 
 import _mjx  # type: ignore
 from flask import Flask, render_template, request
 from flask.views import View
 
+import mjxproto
 from mjx.action import Action
 from mjx.observation import Observation
+from mjx.visualizer.converter import action_type_en, action_type_ja, get_tile_char
+from mjx.visualizer.open_utils import open_tile_ids
 from mjx.visualizer.selector import Selector
 from mjx.visualizer.svg import to_svg
 
@@ -123,32 +126,63 @@ class ShowPages(View):
     def make_choices(self):
         assert ShowPages.observation is not None
         legal_actions_proto_ = [
-            [action.to_proto(), i, action.to_proto().tile in [16, 52, 88]]
+            (action.to_proto(), i)
             for i, action in enumerate(ShowPages.observation.legal_actions())
         ]
-        legal_actions_proto = sorted(legal_actions_proto_, key=lambda x: x[0].type)
-        choices_ = Selector.make_choices(
-            [x[0] for x in legal_actions_proto], unicode=True, ja=True
-        )
-        choices = [
-            (
-                {
-                    "type": choice_str.split("-")[0].split(":")[1],
-                    "char": choice_str.split("-")[1],
-                    "index": legal_actions_proto[i][1],
-                    "red": legal_actions_proto[i][2],
-                }
-                if len(choice_str.split("-")) > 1
-                else {
-                    "type": choice_str.split("-")[0].split(":")[1],
-                    "char": "",
-                    "index": legal_actions_proto[i][1],
-                    "red": legal_actions_proto[i][2],
-                }
-            )
-            for i, choice_str in enumerate(choices_)
-        ]
+        legal_actions_proto = self.sort_legal_actions(legal_actions_proto_)
+        choices = [self.make_choice(choice_[0], choice_[1]) for choice_ in legal_actions_proto]
         return choices
+
+    def make_choice(self, action_proto: mjxproto.Action, i: int):
+        red_hai = [16, 52, 88]
+        if action_proto.type == mjxproto.ActionType.ACTION_TYPE_DUMMY:
+            return {"text": "<span>次へ</span>", "index": 0}
+        elif action_proto.type == mjxproto.ActionType.ACTION_TYPE_NO:
+            return {"text": "<span>パス</span>", "index": 0}
+        elif action_proto.type in [
+            mjxproto.ActionType.ACTION_TYPE_PON,
+            mjxproto.ActionType.ACTION_TYPE_CHI,
+            mjxproto.ActionType.ACTION_TYPE_CLOSED_KAN,
+            mjxproto.ActionType.ACTION_TYPE_OPEN_KAN,
+            mjxproto.ActionType.ACTION_TYPE_ADDED_KAN,
+            mjxproto.ActionType.ACTION_TYPE_RON,
+        ]:
+            text = "<span>" + action_type_ja[action_proto.type] + "</span>"
+            open_ids = [id for id in open_tile_ids(action_proto.open)]
+            for hai_id in open_ids:
+                if hai_id in red_hai:
+                    text_ = '<span class="MJFontR">' + get_tile_char(hai_id, True) + "</span>"
+                    text += text_
+                else:
+                    text_ = '<span class="MJFont">' + get_tile_char(hai_id, True) + "</span>"
+                    text += text_
+            return {"text": text, "index": i}
+        else:
+            text = "<span>" + action_type_ja[action_proto.type] + "</span>"
+            if action_proto.tile in red_hai:
+                text_ = (
+                    '<span class="MJFontR">' + get_tile_char(action_proto.tile, True) + "</span>"
+                )
+                text += text_
+            else:
+                text_ = (
+                    '<span class="MJFont">' + get_tile_char(action_proto.tile, True) + "</span>"
+                )
+                text += text_
+            return {"text": text, "index": i}
+
+    def sort_legal_actions(self, legal_actions_proto: List[Tuple[mjxproto.Action, int]]):
+        """
+        ツモ切りを先頭に持ってくるための専用ソート
+        """
+        legal_actions_proto_ = sorted(
+            [a for a in legal_actions_proto if a[0].type != mjxproto.ACTION_TYPE_TSUMOGIRI],
+            key=lambda x: x[0].type,
+        )
+        for action_ in legal_actions_proto:
+            if action_[0].type == mjxproto.ACTION_TYPE_TSUMOGIRI:
+                legal_actions_proto_ = [action_] + legal_actions_proto_
+        return legal_actions_proto_
 
 
 class HumanControlAgentOnBrowser(Agent):  # type: ignore
