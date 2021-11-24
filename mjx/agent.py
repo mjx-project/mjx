9ba8af7abcb0cebad1a89693cd5d0b0cb33f1caa
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-11-24 15:38:43 +0900

    add ShantenAgent

--- mjx/agent.py
+++ mjx/agent.py
@@ -60,6 +60,43 @@ class TsumogiriAgent(Agent):
         assert False
 
 
+class ShantenAgent(Agent):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def act(self, observation: Observation) -> Action:
+        legal_actions = observation.legal_actions()
+        if len(legal_actions) == 1:
+            return legal_actions[0]
+
+        # if it can win, just win
+        win_actions = [a for a in legal_actions if a.type in [ActionType.TUSMO, ActionType.RON]]
+        if len(win_actions) >= 1:
+            return win_actions[0]
+
+        # if it can declare riichi, just declar
+        riichi_actions = [a for a in legal_actions if a.type == ActionType.RIICHI]
+        if len(riichi_actions) >= 1:
+            return riichi_actions[0]
+
+        # if it can apply ClosedKan/AddedKan, choose randomly
+        kan_actions = [
+            a for a in legal_actions if a.type in [ActionType.CLOSED_KAN, ActionType.ADDED_KAN]
+        ]
+        if len(kan_actions) >= 1:
+            return random.choice(kan_actions)
+
+        # discard an effective tile randomly
+        effective_tile_types = observation.curr_hand().effective_tile_types()
+        effective_discard_actions = [
+            a
+            for a in legal_actions
+            if a.type in [ActionType.DISCARD, ActionType.TSUMOGIRI]
+            and a.tile().type() in effective_tile_types
+        ]
+        return random.choice(effective_discard_actions)
+
+
 class RandomDebugAgent(Agent):
     def __init__(self) -> None:
         super().__init__()
