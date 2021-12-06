e7740ee8606ddcae451497a8cf8709980953ffbc
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-12-06 15:13:59 +0900

    fix test

--- tests_py/test_env.py
+++ tests_py/test_env.py
@@ -72,7 +72,10 @@ def test_MjxEnv():
             action_dict[agent] = random_agent.act(obs)
         obs_dict = env.step(action_dict)
     assert len(action_dict) == 4  # four dummies
-    assert obs_dict == {}
+    # four obs with empty legal actions
+    assert len(obs_dict) == 4
+    for _, obs in obs_dict.items():
+        assert len(obs.legal_actions()) == 0
     rewards = env.rewards()
 
     assert len(rewards) == 4
