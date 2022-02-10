6db2b3113f4eb97572e836ad143d1df15995221b
Keigo Habara <k.habara.aa@gmail.com>
2022-02-10 14:53:16 +0900

    Fix shanten-pinfu.rs

--- include/mjx/internal/shanten-pinfu.rs
+++ include/mjx/internal/shanten-pinfu.rs
@@ -3,16 +3,16 @@ use std::fs;
 use std::io::Write;
 use std::time::Instant;
 
-fn encode(hand: &Vec<usize>, draw: usize) -> usize {
+fn encode(hand: &Vec<usize>) -> usize {
     let mut code: usize = 0;
     for h in hand.iter() {
         code = 5 * code + h;
     }
-    code * 10 + draw
+    code
 }
 
-// x面子y雀頭z両面完成の形を列挙
-fn complete_hands(x: usize, y: usize, z: usize) -> BTreeSet<(Vec<usize>,usize)> {
+// x面子y雀頭z両面の形を列挙
+fn complete_hands(x: usize, y: usize, z: usize) -> BTreeSet<Vec<usize>> {
     assert!(x <= 4);
     assert!(y <= 1);
     assert!(z <= 1);
@@ -31,22 +31,22 @@ fn complete_hands(x: usize, y: usize, z: usize) -> BTreeSet<(Vec<usize>,usize)> 
     // 両面待ちのリスト
     let open_ends = if z == 1 {
         vec![
-            (vec![0,1,1,0,0,0,0,0,0], 0),
-            (vec![0,1,1,0,0,0,0,0,0], 3),
-            (vec![0,0,1,1,0,0,0,0,0], 1),
-            (vec![0,0,1,1,0,0,0,0,0], 4),
-            (vec![0,0,0,1,1,0,0,0,0], 2),
-            (vec![0,0,0,1,1,0,0,0,0], 5),
-            (vec![0,0,0,0,1,1,0,0,0], 3),
-            (vec![0,0,0,0,1,1,0,0,0], 6),
-            (vec![0,0,0,0,0,1,1,0,0], 4),
-            (vec![0,0,0,0,0,1,1,0,0], 7),
-            (vec![0,0,0,0,0,0,1,1,0], 5),
-            (vec![0,0,0,0,0,0,1,1,0], 8),
+            vec![0,1,1,0,0,0,0,0,0],
+            vec![0,1,1,0,0,0,0,0,0],
+            vec![0,0,1,1,0,0,0,0,0],
+            vec![0,0,1,1,0,0,0,0,0],
+            vec![0,0,0,1,1,0,0,0,0],
+            vec![0,0,0,1,1,0,0,0,0],
+            vec![0,0,0,0,1,1,0,0,0],
+            vec![0,0,0,0,1,1,0,0,0],
+            vec![0,0,0,0,0,1,1,0,0],
+            vec![0,0,0,0,0,1,1,0,0],
+            vec![0,0,0,0,0,0,1,1,0],
+            vec![0,0,0,0,0,0,1,1,0],
         ]
     } else {
         vec![
-            (vec![0,0,0,0,0,0,0,0,0], 9),
+            vec![0,0,0,0,0,0,0,0,0],
         ]
     };
 
@@ -69,7 +69,7 @@ fn complete_hands(x: usize, y: usize, z: usize) -> BTreeSet<(Vec<usize>,usize)> 
         ]
     };
 
-    let mut ret: BTreeSet<(Vec<usize>,usize)> = BTreeSet::new();
+    let mut ret: BTreeSet<Vec<usize>> = BTreeSet::new();
 
     let mut sid = vec![0; x+1];
 
@@ -77,12 +77,11 @@ fn complete_hands(x: usize, y: usize, z: usize) -> BTreeSet<(Vec<usize>,usize)> 
         for head in heads.iter() {
             for open_end in open_ends.iter() {
                 let hand: Vec<usize> = (0..9).map(|i| {
-                    (0..x).map(|j| sets[sid[j]][i]).sum::<usize>() + head[i] + open_end.0[i]
+                    (0..x).map(|j| sets[sid[j]][i]).sum::<usize>() + head[i] + open_end[i]
                 }).collect();
-                let draw: usize = open_end.1;
 
-                if (0..9).all(|i| hand[i] + if draw == i { 1 } else { 0 } <= 4) {
-                    ret.insert((hand, draw));
+                if (0..9).all(|i| hand[i] <= 4) {
+                    ret.insert(hand);
                 }
             }
         }
@@ -100,57 +99,39 @@ fn complete_hands(x: usize, y: usize, z: usize) -> BTreeSet<(Vec<usize>,usize)> 
 }
 
 // 01BFSを用いてシャンテン数を計算
-fn bfs(ws: BTreeSet<(Vec<usize>,usize)>) -> Vec<i32> {
-    let mut dist = vec![i32::MAX; 19531250];
-    let mut deq: VecDeque<(i32,Vec<usize>,usize)> = VecDeque::new();
+fn bfs(ws: BTreeSet<Vec<usize>>) -> Vec<i32> {
+    let mut dist = vec![i32::MAX; 1953125];
+    let mut deq: VecDeque<(i32,Vec<usize>)> = VecDeque::new();
 
-    for (hand, draw) in ws {
-        dist[encode(&hand, draw)] = 0;
-        deq.push_back((0, hand, draw));
+    for hand in ws {
+        dist[encode(&hand)] = 0;
+        deq.push_back((0, hand));
     }
 
     while !deq.is_empty() {
-        let (d, hand, draw) = deq.pop_front().unwrap();
-        if d > dist[encode(&hand, draw)] {
+        let (d, hand) = deq.pop_front().unwrap();
+        if d > dist[encode(&hand)] {
             continue;
         }
 
-        // (hand, k) -> (hand + k, None)
-        if draw == 9 {
-            for k in 0..9 {
-                if hand[k] > 0 {
-                    let mut hand_tmp = hand.clone();
-                    hand_tmp[k] -= 1;
-                    let code = encode(&hand_tmp, k);
-                    if dist[code] > d {
-                        dist[code] = d;
-                        deq.push_front((d, hand_tmp, k));
-                    }
+        for k in 0..9 {
+            if hand[k] < 4 {
+                let mut hand_add = hand.clone();
+                hand_add[k] += 1;
+                let code_add = encode(&hand_add);
+                if dist[code_add] > d {
+                    dist[code_add] = d;
+                    deq.push_front((d, hand_add));
                 }
             }
-        }
-
-        // (hand, None) -> (hand - k, None)
-        if draw == 9 {
-            for k in 0..9 {
-                if hand[k] < 4 {
-                    let mut hand_tmp = hand.clone();
-                    hand_tmp[k] += 1;
-                    let code = encode(&hand_tmp, 9); 
-                    if dist[code] > d {
-                        dist[code] = d;
-                        deq.push_front((d, hand_tmp, 9));
-                    }
+            if hand[k] > 0 {
+                let mut hand_sub = hand.clone();
+                hand_sub[k] -= 1;
+                let code_sub = encode(&hand_sub);
+                if dist[code_sub] > d + 1 {
+                    dist[code_sub] = d + 1;
+                    deq.push_back((d + 1, hand_sub));
                 }
-            }
-        }
-
-        // (hand, None) -> (hand, draw)
-        if draw != 9 {
-            let code = encode(&hand, 0);
-            if dist[code] > d + 1 {
-                dist[code] = d + 1;
-                deq.push_back((d + 1, hand.clone(), 9));
             }
         }
     }
@@ -169,14 +150,11 @@ fn main() {
         }).collect()
     }).collect();
     dbg!(shanten[0][0][0].len());
-    // assert!(shanten[0][0][0].iter().all(|d| *d == 0));
-    for code in 0..1953125 {
-        assert!(shanten[0][0][0][code * 10 + 9] == 0);
-    }
+    assert!(shanten[0][0][0].iter().all(|d| *d == 0));
 
     // 計算結果を保存
     let mut f = fs::File::create("shanten-pinfu.txt").unwrap();
-    for code in 0..19531250 {
+    for code in 0..1953125 {
         f.write_all(format!("{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}\n",
             shanten[0][0][0][code],
             shanten[1][0][0][code],
