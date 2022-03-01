4281dbab27b453bdc6babf21c1fa0ad943842e5b
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2022-03-01 15:51:08 +0900

    add internal::Observation::GenerateLegalActions

--- include/mjx/internal/observation.cpp
+++ include/mjx/internal/observation.cpp
@@ -2,6 +2,7 @@
 
 #include "mjx/internal/mjx.grpc.pb.h"
 #include "mjx/internal/utils.h"
+#include "mjx/internal/yaku_evaluator.h"
 
 namespace mjx::internal {
 Observation::Observation(const mjxproto::Observation& proto) : proto_(proto) {}
@@ -225,5 +226,432 @@ std::vector<float> Observation::small_v0() const {
   }
 
   return feature;
+}
+
+std::vector<mjxproto::Action> Observation::GenerateLegalActions(
+    const mjxproto::Observation &observation) {
+  auto obs = Observation(observation);
+  auto who = AbsolutePos(observation.who());
+  Assert(observation.public_observation().events_size() > 0,
+         "Events must not be empty.");
+  const auto &last_event = *observation.public_observation().events().rbegin();
+  auto last_event_type = last_event.type();
+  auto hand = obs.current_hand();
+  const auto &game_id = observation.public_observation().game_id();
+
+  // Dummy action at the round terminal
+  if (IsRoundOver(observation.public_observation())) {
+    obs.add_legal_action(Action::CreateDummy(who, game_id));
+    return obs.legal_actions();
+  }
+
+  if (who == AbsolutePos(last_event.who())) {
+    switch (last_event_type) {
+      case mjxproto::EVENT_TYPE_DRAW: {
+        // NineTiles
+        if (IsFirstTurnWithoutOpen(observation.public_observation()) &&
+            hand.CanNineTiles())
+          obs.add_legal_action(Action::CreateNineTiles(who, game_id));
+
+        // Tsumo
+        Tile drawn_tile = Tile(hand.LastTileAdded().value());
+        if (hand.IsCompleted() && CanTsumo(who, observation))
+          obs.add_legal_action(Action::CreateTsumo(who, drawn_tile, game_id));
+
+        // Kan
+        if (auto possible_kans = hand.PossibleOpensAfterDraw();
+            !possible_kans.empty() &&
+            !IsFourKanNoWinner(observation.public_observation())) {  // TODO:
+          // 四槓散了かのチェックは5回目のカンをできないようにするためだが、正しいのか確認
+          // #701
+          for (const auto possible_kan : possible_kans) {
+            obs.add_legal_action(
+                Action::CreateOpen(who, possible_kan, game_id));
+          }
+        }
+
+        // Riichi
+        if (CanRiichi(who, observation)) {
+          obs.add_legal_action(Action::CreateRiichi(who, game_id));
+        }
+
+        // Discard and tsumogiri
+        obs.add_legal_actions(Action::CreateDiscardsAndTsumogiri(
+            who, hand.PossibleDiscards(), game_id));
+        break;
+      }
+      case mjxproto::EVENT_TYPE_CHI:
+      case mjxproto::EVENT_TYPE_PON: {
+        obs.add_legal_actions(Action::CreateDiscardsAndTsumogiri(
+            who, hand.PossibleDiscards(), game_id));
+        break;
+      }
+      case mjxproto::EVENT_TYPE_RIICHI: {
+        obs.add_legal_actions(Action::CreateDiscardsAndTsumogiri(
+            who, hand.PossibleDiscardsJustAfterRiichi(), game_id));
+        break;
+      }
+      case mjxproto::EVENT_TYPE_DISCARD:
+      case mjxproto::EVENT_TYPE_TSUMOGIRI:
+      case mjxproto::EVENT_TYPE_CLOSED_KAN:
+      case mjxproto::EVENT_TYPE_ADDED_KAN:
+      case mjxproto::EVENT_TYPE_TSUMO:
+      case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_NINE_TERMINALS:
+      case mjxproto::EVENT_TYPE_OPEN_KAN:
+      case mjxproto::EVENT_TYPE_RON:
+      case mjxproto::EVENT_TYPE_RIICHI_SCORE_CHANGE:
+      case mjxproto::EVENT_TYPE_NEW_DORA:
+      case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_FOUR_RIICHIS:
+      case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_THREE_RONS:
+      case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_FOUR_KANS:
+      case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_FOUR_WINDS:
+      case mjxproto::EVENT_TYPE_EXHAUSTIVE_DRAW_NORMAL:
+      case mjxproto::EVENT_TYPE_EXHAUSTIVE_DRAW_NAGASHI_MANGAN:
+        break;
+    }
+  } else {
+    switch (last_event_type) {
+      case mjxproto::EVENT_TYPE_DISCARD:
+      case mjxproto::EVENT_TYPE_TSUMOGIRI: {
+        auto tile = Tile(last_event.tile());
+        // Ron
+        if (CanRon(who, observation)) {
+          obs.add_legal_action(Action::CreateRon(who, tile, game_id));
+        }
+        // Chi, Pon, and AddedKan
+        if (!HasDrawLeft(observation.public_observation())) break;
+        if (IsFourKanNoWinner(observation.public_observation()))
+          break;  // if 四槓散了直前の捨て牌, only ron
+        auto relative_pos = ToRelativePos(who, AbsolutePos(last_event.who()));
+        auto possible_opens =
+            hand.PossibleOpensAfterOthersDiscard(tile, relative_pos);
+        for (const auto &possible_open : possible_opens)
+          obs.add_legal_action(Action::CreateOpen(who, possible_open, game_id));
+        break;
+      }
+      case mjxproto::EVENT_TYPE_ADDED_KAN: {
+        // 槍槓
+        auto tile = Open(last_event.open()).LastTile();
+        if (CanRon(who, observation)) {
+          obs.add_legal_action(Action::CreateRon(who, tile, game_id));
+        }
+        break;
+      }
+      case mjxproto::EVENT_TYPE_DRAW:
+      case mjxproto::EVENT_TYPE_RIICHI:
+      case mjxproto::EVENT_TYPE_CLOSED_KAN:
+      case mjxproto::EVENT_TYPE_TSUMO:
+      case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_NINE_TERMINALS:
+      case mjxproto::EVENT_TYPE_CHI:
+      case mjxproto::EVENT_TYPE_PON:
+      case mjxproto::EVENT_TYPE_OPEN_KAN:
+      case mjxproto::EVENT_TYPE_RON:
+      case mjxproto::EVENT_TYPE_RIICHI_SCORE_CHANGE:
+      case mjxproto::EVENT_TYPE_NEW_DORA:
+      case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_FOUR_RIICHIS:
+      case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_THREE_RONS:
+      case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_FOUR_KANS:
+      case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_FOUR_WINDS:
+      case mjxproto::EVENT_TYPE_EXHAUSTIVE_DRAW_NORMAL:
+      case mjxproto::EVENT_TYPE_EXHAUSTIVE_DRAW_NAGASHI_MANGAN:
+        break;
+    }
+    if (obs.has_legal_action())
+      obs.add_legal_action(Action::CreateNo(who, game_id));
+  }
+  return obs.legal_actions();
+}
+
+bool Observation::HasDrawLeft(const mjxproto::PublicObservation &public_observation) {
+  const auto &events = public_observation.events();
+  int num_draw = 0;
+  for (const auto &e : events) {
+    if (e.type() == mjxproto::EVENT_TYPE_DRAW) num_draw++;
+  }
+  return 52 + num_draw < 122;
+}
+
+bool Observation::HasNextDrawLeft(
+    const mjxproto::PublicObservation &public_observation) {
+  const auto &events = public_observation.events();
+  int num_draws = 0;
+  for (const auto &e : events) {
+    if (e.type() == mjxproto::EVENT_TYPE_DRAW) num_draws++;
+  }
+  return 52 + num_draws <= 118;
+}
+
+bool Observation::RequireKanDraw(
+    const mjxproto::PublicObservation &public_observation) {
+  for (auto it = public_observation.events().rbegin();
+       it != public_observation.events().rend(); ++it) {
+    const auto &event = *it;
+    switch (event.type()) {
+      case mjxproto::EventType::EVENT_TYPE_DRAW:
+        return false;
+      case mjxproto::EventType::EVENT_TYPE_ADDED_KAN:
+      case mjxproto::EventType::EVENT_TYPE_CLOSED_KAN:
+      case mjxproto::EventType::EVENT_TYPE_OPEN_KAN:
+        return true;
+    }
+  }
+  return false;
+}
+
+bool Observation::CanRon(AbsolutePos who, const mjxproto::Observation &observation) {
+  auto obs = Observation(observation);
+  auto hand = obs.current_hand();
+  const auto &events = observation.public_observation().events();
+  const auto &last_event = *observation.public_observation().events().rbegin();
+
+  const auto target_tile = TargetTile(observation.public_observation());
+  if (!target_tile.has_value()) return false;
+  if (!hand.IsTenpai()) return false;
+
+  // set machi
+  std::bitset<34> machi;
+  for (auto tile_type :
+       WinHandCache::instance().Machi(hand.ClosedTileTypes())) {
+    machi.set(ToUType(tile_type));
+  }
+
+  // set missed_tiles and discards
+  std::bitset<34> missed_tiles;
+  std::bitset<34> discards;
+  for (auto it = events.begin(); it != events.end(); ++it) {
+    const auto &e = *it;
+    auto last_it = events.end();
+    --last_it;
+    bool is_under_riichi = false;
+    switch (e.type()) {
+      case mjxproto::EVENT_TYPE_DISCARD:
+      case mjxproto::EVENT_TYPE_TSUMOGIRI: {
+        auto tile = Tile(e.tile());
+        if (who == AbsolutePos(e.who())) discards.set(tile.TypeUint());
+        // フリテン設定 ロン牌はフリテン判定しない
+        if (it != last_it) missed_tiles.set(tile.TypeUint());
+        break;
+      }
+      case mjxproto::EVENT_TYPE_DRAW: {
+        // フリテン解除
+        if (who == AbsolutePos(e.who()) && !is_under_riichi)
+          missed_tiles.reset();
+        break;
+      }
+      case mjxproto::EVENT_TYPE_CHI:
+      case mjxproto::EVENT_TYPE_PON:
+      case mjxproto::EVENT_TYPE_CLOSED_KAN:
+      case mjxproto::EVENT_TYPE_ADDED_KAN:
+      case mjxproto::EVENT_TYPE_OPEN_KAN: {
+        if (who == AbsolutePos(e.who())) missed_tiles.reset();  // フリテン解除
+        break;
+      }
+      case mjxproto::EVENT_TYPE_RIICHI: {
+        if (who == AbsolutePos(e.who())) {
+          is_under_riichi = true;
+        }
+        break;
+      }
+    }
+  }
+
+  // フリテン
+  if ((machi & discards).any()) return false;
+  if ((machi & missed_tiles).any()) return false;
+
+  auto seat_wind = ToSeatWind(who, dealer(observation.public_observation()));
+  auto win_state_info = WinStateInfo(
+      seat_wind, prevalent_wind(observation.public_observation()),
+      !HasDrawLeft(observation.public_observation()),
+      IsIppatsu(who, observation.public_observation()),
+      IsFirstTurnWithoutOpen(observation.public_observation()) &&
+          AbsolutePos(last_event.who()) == who &&
+          (Any(last_event.type(),
+               {mjxproto::EVENT_TYPE_DRAW, mjxproto::EVENT_TYPE_TSUMO})),
+      seat_wind == Wind::kEast, IsRobbingKan(observation.public_observation()),
+      {},  // dora type count 和了れるかどうかだけなのでドラは関係ない
+      {}  // ura dora type count 和了れるかどうかだけなのでドラは関係ない
+  );
+  return YakuEvaluator::CanWin(
+      WinInfo(std::move(win_state_info), hand.win_info())
+          .Ron(target_tile.value()));
+}
+
+std::optional<Tile> Observation::TargetTile(
+    const mjxproto::PublicObservation &public_observation) {
+  for (auto it = public_observation.events().rbegin();
+       it != public_observation.events().rend(); ++it) {
+    const auto &event = *it;
+
+    if (event.type() == mjxproto::EventType::EVENT_TYPE_DISCARD or
+        event.type() == mjxproto::EventType::EVENT_TYPE_TSUMOGIRI) {
+      return Tile(event.tile());
+    }
+    if (event.type() == mjxproto::EventType::EVENT_TYPE_ADDED_KAN) {
+      return Open(event.open()).LastTile();
+    }
+  }
+  return std::nullopt;
+}
+
+AbsolutePos Observation::dealer(
+    const mjxproto::PublicObservation &public_observation) {
+  return AbsolutePos(public_observation.init_score().round() % 4);
+}
+
+Wind Observation::prevalent_wind(
+    const mjxproto::PublicObservation &public_observation) {
+  return Wind(public_observation.init_score().round() / 4);
+  ;
+}
+
+bool Observation::IsIppatsu(AbsolutePos who,
+                      const mjxproto::PublicObservation &public_observation) {
+  std::vector<bool> is_ippatsu_ = {false, false, false, false};
+  const auto &events = public_observation.events();
+  std::optional<mjxproto::EventType> prev_event_type = std::nullopt;
+  for (const auto &e : events) {
+    switch (e.type()) {
+      case mjxproto::EVENT_TYPE_RIICHI_SCORE_CHANGE: {
+        is_ippatsu_[e.who()] = true;
+      }
+      case mjxproto::EVENT_TYPE_DISCARD:
+      case mjxproto::EVENT_TYPE_TSUMOGIRI: {
+        is_ippatsu_[e.who()] = false;
+        break;
+      }
+      case mjxproto::EVENT_TYPE_ADDED_KAN: {
+        is_ippatsu_[e.who()] = false;
+        break;
+      }
+      case mjxproto::EVENT_TYPE_CHI:
+      case mjxproto::EVENT_TYPE_PON:
+      case mjxproto::EVENT_TYPE_CLOSED_KAN:
+      case mjxproto::EVENT_TYPE_OPEN_KAN: {
+        for (int i = 0; i < 4; ++i) is_ippatsu_[i] = false;
+        break;
+      }
+      // 加槓=>槍槓=>Noのときの一発消し
+      case mjxproto::EVENT_TYPE_DRAW: {
+        if (prev_event_type.has_value() &&
+            prev_event_type.value() == mjxproto::EVENT_TYPE_ADDED_KAN) {
+          for (int i = 0; i < 4; ++i) is_ippatsu_[i] = false;
+        }
+        break;
+      }
+    }
+    prev_event_type = e.type();
+  }
+  return is_ippatsu_[ToUType(who)];
+}
+
+bool Observation::IsRobbingKan(
+    const mjxproto::PublicObservation &public_observation) {
+  for (auto it = public_observation.events().rbegin();
+       it != public_observation.events().rend(); ++it) {
+    const auto &event = *it;
+    if (event.type() == mjxproto::EventType::EVENT_TYPE_DRAW) {
+      return false;
+    }
+    if (event.type() == mjxproto::EventType::EVENT_TYPE_ADDED_KAN) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool Observation::IsFirstTurnWithoutOpen(
+    const mjxproto::PublicObservation &public_observation) {
+  const auto &events = public_observation.events();
+  for (const auto &event : events) {
+    switch (event.type()) {
+      case mjxproto::EVENT_TYPE_CHI:
+      case mjxproto::EVENT_TYPE_PON:
+      case mjxproto::EVENT_TYPE_CLOSED_KAN:
+      case mjxproto::EVENT_TYPE_OPEN_KAN:
+      case mjxproto::EVENT_TYPE_ADDED_KAN:
+        return false;
+      case mjxproto::EVENT_TYPE_DISCARD:
+      case mjxproto::EVENT_TYPE_TSUMOGIRI:
+        if (ToSeatWind(static_cast<AbsolutePos>(event.who()),
+                       dealer(public_observation)) == Wind::kNorth) {
+          return false;
+        }
+    }
+  }
+  return true;
+}
+
+bool Observation::IsFourKanNoWinner(
+    const mjxproto::PublicObservation &public_observation) {
+  const auto &events = public_observation.events();
+  int num_total_kans = 0;
+  std::unordered_set<int> kan_players;
+  for (const auto &e : events) {
+    if (Any(e.type(),
+            {mjxproto::EVENT_TYPE_ADDED_KAN, mjxproto::EVENT_TYPE_CLOSED_KAN,
+             mjxproto::EVENT_TYPE_OPEN_KAN})) {
+      num_total_kans++;
+      kan_players.insert(e.who());
+    }
+  }
+  return num_total_kans == 4 && kan_players.size() > 1;
+}
+
+bool Observation::CanRiichi(AbsolutePos who,
+                      const mjxproto::Observation &observation) {
+  auto obs = Observation(observation);
+  auto hand = obs.current_hand();
+  if (hand.IsUnderRiichi()) return false;
+  if (!HasNextDrawLeft(observation.public_observation())) return false;
+  auto ten =
+      observation.public_observation().init_score().tens(ToUType(obs.who()));
+  return hand.CanRiichi(ten);
+}
+
+bool Observation::CanTsumo(AbsolutePos who,
+                     const mjxproto::Observation &observation) {
+  auto obs = Observation(observation);
+  auto hand = obs.current_hand();
+  const auto &events = observation.public_observation().events();
+  const auto &last_event = *observation.public_observation().events().rbegin();
+  auto seat_wind = ToSeatWind(who, dealer(observation.public_observation()));
+
+  // TODO: duplicated. see CanRon
+  auto win_state_info = WinStateInfo(
+      seat_wind, prevalent_wind(observation.public_observation()),
+      !HasDrawLeft(observation.public_observation()),
+      IsIppatsu(who, observation.public_observation()),
+      IsFirstTurnWithoutOpen(observation.public_observation()) &&
+          AbsolutePos(last_event.who()) == who &&
+          (Any(last_event.type(),
+               {mjxproto::EVENT_TYPE_DRAW, mjxproto::EVENT_TYPE_TSUMO})),
+      seat_wind == Wind::kEast, IsRobbingKan(observation.public_observation()),
+      {},  // dora type count 和了れるかどうかだけなのでドラは関係ない
+      {}  // ura dora type count 和了れるかどうかだけなのでドラは関係ない
+  );
+  return YakuEvaluator::CanWin(
+      WinInfo(std::move(win_state_info), hand.win_info()));
+}
+
+bool Observation::IsRoundOver(
+    const mjxproto::PublicObservation &public_observation) {
+  bool has_last_event = !public_observation.events().empty();
+  if (!has_last_event) return false;
+  auto last_event_type = public_observation.events().rbegin()->type();
+  switch (last_event_type) {
+    case mjxproto::EVENT_TYPE_TSUMO:
+    case mjxproto::EVENT_TYPE_RON:
+    case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_NINE_TERMINALS:
+    case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_FOUR_RIICHIS:
+    case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_THREE_RONS:
+    case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_FOUR_KANS:
+    case mjxproto::EVENT_TYPE_ABORTIVE_DRAW_FOUR_WINDS:
+    case mjxproto::EVENT_TYPE_EXHAUSTIVE_DRAW_NORMAL:
+    case mjxproto::EVENT_TYPE_EXHAUSTIVE_DRAW_NAGASHI_MANGAN:
+      return true;
+    default:
+      return false;
+  }
 }
 }  // namespace mjx::internal
