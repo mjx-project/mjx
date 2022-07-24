3fb1b04143fc9eb9d42037877989a089b67ba9ed
OkanoShinri <n.salt2000@gmail.com>
2022-07-24 12:57:49 +0000

    fix

--- tests_py/test_features.py
+++ tests_py/test_features.py
@@ -1,5 +1,3 @@
-from matplotlib.pyplot import close
-
 from mjx import Observation
 
 json_str = '{"publicObservation":{"playerIds":["player_2","player_1","player_0","player_3"],"initScore":{"tens":[25000,25000,25000,25000]},"doraIndicators":[101],"events":[{"type":"EVENT_TYPE_DRAW"}]},"privateObservation":{"initHand":{"closedTiles":[24,3,87,124,37,42,58,134,92,82,122,18,117]},"drawHistory":[79],"currHand":{"closedTiles":[3,18,24,37,42,58,79,82,87,92,117,122,124,134]}},"legalActions":[{"tile":3},{"tile":18},{"tile":24},{"tile":37},{"tile":42},{"tile":58},{"type":"ACTION_TYPE_TSUMOGIRI","tile":79},{"tile":82},{"tile":87},{"tile":92},{"tile":117},{"tile":122},{"tile":124},{"tile":134}]}'
